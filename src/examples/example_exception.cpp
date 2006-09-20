78dea584aacca3eb060314c36694b06454b350d1
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2006-09-20 09:28:20 +0000

    + Added example for exceptions.
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@13 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ src/examples/example_exception.cpp
@@ -0,0 +1,102 @@
+
+/***************************************************************************
+ *  example_exception.cpp - Example for using exceptions
+ *
+ *  Generated: Sun Sep 17 14:00:26 2006 (German Medical Library)
+ *  Copyright  2006  Tim Niemueller [www.niemueller.de]
+ *
+ *  $Id$
+ *
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+// Do not mention in API doc
+/// @cond EXAMPLES
+
+#include <core/exception.h>
+
+#include <stdlib.h>
+#include <iostream>
+
+class ExampleSmallException : public Exception
+{
+ public:
+  ExampleSmallException() : Exception("Small Exception") {}
+};
+
+class ExampleBigException : public Exception
+{
+ public:
+  ExampleBigException() : Exception("Big Exception") {}
+};
+
+class ExampleUnhandledException : public Exception
+{
+ public:
+  ExampleUnhandledException() : Exception("Exception not handled") {}
+};
+
+
+void
+throw_some_exception()
+{
+  int r = rand();
+  if ( r < (RAND_MAX / 3)) {
+    throw ExampleSmallException();
+  } else if ( r < (RAND_MAX / 3 * 2)) {
+    // printf("Throwing boom\n");
+    // throw ExampleUnhandledException();
+  } else {
+    throw ExampleBigException();
+  }
+}
+
+void
+indirect_throw_some_exception()
+{
+  try {
+    throw_some_exception();
+  } catch (Exception &e) {
+    e.append("More info");
+    throw;
+  }
+}
+
+
+int
+main(int argc, char **argv)
+{
+  srand(42);
+
+  while (1) {
+    try {
+      indirect_throw_some_exception();
+    } catch (ExampleSmallException &se) {
+      std::cout << "Message: " <<  se.c_str() << std::endl;
+      std::cout << "Trace:" << std::endl;
+      se.printTrace();
+    } catch (ExampleBigException &be) {
+      std::cout << "Message: " << be.c_str() << std::endl;
+      std::cout << "Trace:" << std::endl;
+      be.printTrace();
+    }
+  }
+}
+
+
+/// @endcond
