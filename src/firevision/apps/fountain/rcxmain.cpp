faeda1d36fec05c8c9dc92469a371b3b1bcbda35
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2007-11-22 15:19:28 +0000

    Fountain for RCSoftX
    + Implemented Fountain for RCSoftX, as assumed this is close to trivial.
    . Starts a FuseServer and announces the services on the network. Note that
    . a RCSoftX Fountain will show Fawkes images and vice versa.
    + Binary is now named fvfountain, not fountain anymore. This is to make
    . FireVision apps more consistent in their names.
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@469 fc82b16a-671c-0410-8732-9903a8d2dd0d

--- src/firevision/apps/fountain/rcxmain.cpp
+++ src/firevision/apps/fountain/rcxmain.cpp
@@ -1,9 +1,9 @@
 
 /***************************************************************************
- *  main.cpp - main application for front camera computer vision pipeline
+ *  rcxmain.cpp - Main application for Fountain in RCSoftX
  *
- *  Generated: Fri May 20 14:25:28 2005
- *  Copyright  2005  Tim Niemueller [www.niemueller.de]
+ *  Created: Fri May 20 14:25:28 2005
+ *  Copyright  2005-2007  Tim Niemueller [www.niemueller.de]
  *
  *  $Id$
  *
@@ -27,77 +27,134 @@
 
 /// @cond RCSoftX
 
-#include <iostream>
-
 #include <apps/fountain/config.h>
-#include <apps/fountain/fountain.h>
+#include <fvutils/net/fuse_server.h>
 
 #include <utils/system/argparser.h>
 #include <utils/system/console_colors.h>
+#include <utils/system/signal.h>
 #include <utils/config_reader/config_reader.h>
 #include <utils/logging/console.h>
 
+#ifdef HAVE_AVAHI
+#include <netcomm/dns-sd/avahi_thread.h>
+#include <netcomm/dns-sd/avahi_service_publisher.h>
+#include <netcomm/dns-sd/avahi_browser.h>
+#include <netcomm/service_discovery/service.h>
+#include <netcomm/utils/resolver.h>
+#endif
+
+#include <iostream>
+
 using namespace std;
+
+class FountainRCSoftX : public SignalHandler
+{
+ public:
+  FountainRCSoftX(int argc, char **argv)
+  {
+    __argp   = new ArgumentParser(argc, argv, "Hx:");
+    __logger = new ConsoleLogger();
+
+#ifdef HAVE_AVAHI
+    __avahi_thread          = new AvahiThread();
+    __avahi_thread->start();
+    __nnresolver = new NetworkNameResolver(__avahi_thread);
+#endif
+
+    __cr = NULL;
+    __config = NULL;
+    __fuse_server = NULL;
+  }
+
+  ~FountainRCSoftX()
+  {
+#ifdef HAVE_AVAHI
+    __avahi_thread->cancel();
+    __avahi_thread->join();
+    delete __nnresolver;
+    delete __avahi_thread;
+#endif
+
+    delete __argp;
+    delete __logger;
+    delete __cr;
+    delete __config;
+    delete __fuse_server;
+  }
+
+  virtual void handle_signal(int signal)
+  {
+    if ( __fuse_server ) {
+      __fuse_server->cancel();
+    }
+  }
+
+  void
+  print_usage()
+  {
+    printf("Usage: %s [-x config] [-H]\n"
+	   "  -H             Show this help message\n"
+	   "  -x config      Load given RCSoftX config file\n",
+	   __argp->program_name());
+  }
+
+  void run()
+  {
+    if ( __argp->has_arg("H") ) {
+      print_usage();
+      return;
+    }
+
+    __cr = new CConfigReader();
+
+    const char *config_filename;
+    if ( (config_filename = __argp->arg("x")) == NULL ) {
+      config_filename = "../cfg/config.xml";
+      cout << cblue << "FireVision Fountain" << cnormal
+	   << ": No config file given, using default ("
+	   << config_filename << ")" << endl;
+    }
+    __cr->LoadConfFile( config_filename );
+    __config = new RCSoftXFountainConfig( __cr );
+
+    __fuse_server = new FuseServer(__config->FountainPort);
+    __fuse_server->start();
+
+#ifdef HAVE_AVAHI
+    char *fountain_service_name;
+    asprintf(&fountain_service_name, "Fountain on %s", __nnresolver->short_hostname());
+    NetworkService *fountain_service = new NetworkService(fountain_service_name,
+							  "_fountain._tcp",
+							  __config->FountainPort);
+    free(fountain_service_name);
+    __avahi_thread->publish(fountain_service);
+#endif
+
+    __fuse_server->join();
+  }
+
+
+ private:
+  ArgumentParser        *__argp;
+  ConsoleLogger         *__logger;
+  CConfigReader         *__cr;
+  RCSoftXFountainConfig *__config;
+  FuseServer            *__fuse_server;
+#ifdef HAVE_AVAHI
+  AvahiThread           *__avahi_thread;
+  NetworkNameResolver   *__nnresolver;
+#endif
+};
+
 
 int
 main( int argc, char **argv )
 {
-
-  // see help output below for the meaning of the parameters
-  ArgumentParser *argp = new ArgumentParser(argc, argv, "d:x:Hb");
-  ConsoleLogger *logger = new ConsoleLogger();
-
-  if (argp->has_arg("H")) {
-
-    cout << endl << cblue << "FireFountain Help" << cnormal << endl << endl
-	 << " -d delay  Delay in ms between image sends" << endl
-	 << " -x file   use named XML config file" << endl
-	 << " -H        Shows this help" << endl
-	 << endl;
-
-  } else {
-
-    cout << cblue << "FireFountain" << cnormal << ": Running pipeline directly" << endl;
-
-    CConfigReader *cr = new CConfigReader();
-
-    const char *config_filename;
-    if ( (config_filename = argp->arg("x")) == NULL ) {
-      config_filename = "../cfg/config.xml";
-      cout << cblue << "FireFountain" << cnormal
-	   << ": No config file given, using default ("
-	   << config_filename << ")" << endl;
-    }
-    cr->LoadConfFile( config_filename );
-    RCSoftXFountainConfig *c = new RCSoftXFountainConfig( cr );
-
-    const char *tmp;
-    int delay = 0;
-    if ( (tmp = argp->arg("d")) != NULL ) {
-      delay = atoi(tmp);
-      if (delay < 0) {
-	cout << cblue << "FireFountain: " << cred << "Invalid delay. Resetting to 0" << cnormal << endl;
-	delay = 0;
-      }
-    }
-
-    cout << cblue << "FireFountain" << cnormal << ": Delay is " << delay << " ms" << endl;
-
-    Fountain *fountain = new Fountain(argp, logger, c->ImageDelay, c->FountainPort);
-
-    fountain->init();
-    fountain->run();
-    fountain->finalize();
-
-    // causes segfault, already deleted otherwise, probably by signal
-    // delete fountain;
-    delete cr;
-    delete c;
-
-  }
-
-  delete argp;
-
+  FountainRCSoftX f(argc, argv);
+  SignalManager::register_handler(SIGINT, &f);
+  f.run();
+  SignalManager::finalize();
 }
 
 /// @endcond
