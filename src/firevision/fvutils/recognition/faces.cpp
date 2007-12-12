a40112e668b84213fb8cd6a5fd6c6bd809a950af
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2007-12-12 12:25:55 +0000

    Added FaceRecognized skeleton
    + Added basic skeleton of the FaceRecognizer class. To be filled. The added
    . recognition directory should also be a nice place for other recognizers
    . like a generic object recognizer.
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@578 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ src/firevision/fvutils/recognition/faces.cpp
@@ -0,0 +1,63 @@
+
+/***************************************************************************
+ *  faces.cpp - Face Recognizer
+ *
+ *  Created: Wed Dec 12 13:08:42 2007
+ *  Copyright  2007  Tim Niemueller [www.niemueller.de]
+ *
+ *  $Id$
+ *
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <fvutils/recognition/faces.h>
+
+/** @class FaceRecognizer <fvutils/recognition/faces.h>
+ * Face Recognizer.
+ * This class takes an image and a number of ROIs as input. The ROIs are then
+ * scanned and a map with labeled ROIs is returned.
+ * @author Tim Niemueller
+ */
+
+/** Constructor. */
+FaceRecognizer::FaceRecognizer()
+{
+}
+
+
+/** Destructor. */
+FaceRecognizer::~FaceRecognizer()
+{
+}
+
+
+/** Recognize faces.
+ * Scans the given ROIs in the image for faces and returns a map of labeled
+ * faces.
+ * @param buffer image buffer
+ * @param rois reference to list of ROIs, can be the list of ROIs returned by a
+ * classifier like the FacesClassifier.
+ * @return map of ROIs with strings as labels for the ROI.
+ */
+FaceRecognizer::FaceRoiMap *
+FaceRecognizer::recognize(unsigned char *buffer, std::list<ROI> &rois)
+{
+  FaceRoiMap *rv = new FaceRoiMap();
+
+  return rv;
+}
---
+++ src/firevision/fvutils/recognition/faces.h
@@ -0,0 +1,51 @@
+
+/***************************************************************************
+ *  faces.h - Face Recognizer
+ *
+ *  Created: Wed Dec 12 13:04:12 2007
+ *  Copyright  2007  Tim Niemueller [www.niemueller.de]
+ *
+ *  $Id$
+ *
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __FIREVISION_FVUTILS_RECOGNITION_FACES_H_
+#define __FIREVISION_FVUTILS_RECOGNITION_FACES_H
+
+#include <fvutils/base/roi.h>
+
+#include <map>
+#include <list>
+#include <string>
+
+class FaceRecognizer
+{
+ public:
+  FaceRecognizer();
+  ~FaceRecognizer();
+
+  /** Map of labeled ROIs. */
+  typedef std::map<ROI, std::string> FaceRoiMap ;
+
+  FaceRoiMap *  recognize(unsigned char *buffer, std::list<ROI> &rois);
+
+};
+
+
+#endif
