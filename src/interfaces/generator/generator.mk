adc71d5aa84836a5488affced1f7d88e2b75f5bb
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2008-02-01 13:28:41 +0000

    Fixed nasty buildsys bug, change interfaces Makefile, adapted
    + There was a nasty bug in the build system: if you wanted to have PRESUBDIRS
    . in a file that would include base.mk instead of rules.mk (so there are subdirs
    . *and* targets) it could print dependency notes twice and wrongly formatted.
    . This was because the SRCDIR was not set appropriately in that case. This has
    . been fixed by setting the SRCDIR explicitly for submakes. This breaks our
    . dependency output protection, SRCMAKE will never be . and thus it would not
    . work. To cover that a new special variable OBJSSUBMAKE has been introduced
    . that is passed to objsdir submakes.
    + Interfaces and interface generator makefiles have been heavily modified to
    . update to current patterns and to be able to have a "make" in the generator
    . sub-directory.
    + Fixed the navigator makefile to have ordering constraints such that in a
    . parallel build the presubdirs are really built before navigator is linked.
    . Unfortunately my first approach of having this in rules.mk did not work out
    . since the interfaces are dependent of an ordering different from most others:
    . the interface_generator (bin) has to be built before libinterfaces (lib)
    . while usually you want to have the libs before the bin or plugin. Add
    . ordering constraints in your Makefile as necessary.
    
    Tim
    
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@656 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ src/interfaces/generator/generator.mk
@@ -0,0 +1,21 @@
+#*****************************************************************************
+#            Makefile Build System for Fawkes : Interfaces Generator
+#                            -------------------
+#   Created on Fri Feb 01 11:37:25 2008
+#   copyright (C) 2006-2008 by Tim Niemueller, AllemaniACs RoboCup Team
+#
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifneq ($(PKGCONFIG),)
+  HAVE_LIBXMLPP = $(if $(shell $(PKGCONFIG) --exists 'libxml++-2.6'; echo $${?/1/}),1,0)
+endif
+
+BUILD_INTERFACE_GENERATOR=$(HAVE_LIBXMLPP)
+
---
+++ src/interfaces/generator/generator_rules.mk
@@ -0,0 +1,51 @@
+#*****************************************************************************
+#            Makefile Build System for Fawkes : Interfaces Generator
+#                            -------------------
+#   Created on Fri Feb 01 11:37:25 2008
+#   copyright (C) 2006-2008 by Tim Niemueller, AllemaniACs RoboCup Team
+#
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifeq ($(BUILD_INTERFACE_GENERATOR),1)
+  CFLAGS += -DINTERFACEDIR=\"$(realpath $(BASEDIR)/src/interfaces)\" \
+	    $(shell $(PKGCONFIG) --cflags libxml++-2.6)
+
+  ifneq ($(notdir $(SRCDIR)),generator)
+    GENDIR=generator/
+  endif
+
+  LIBS_interface_generator = core utils
+  LDFLAGS_interface_generator = $(shell $(PKGCONFIG) --libs libxml++-2.6)
+  OBJS_interface_generator = $(GENDIR)constant.o		\
+			     $(GENDIR)enum_constant.o	\
+			     $(GENDIR)field.o		\
+			     $(GENDIR)generator.o	\
+			     $(GENDIR)main.o		\
+			     $(GENDIR)message.o		\
+			     $(GENDIR)parser.o		\
+			     $(GENDIR)type_checker.o
+
+  OBJS_all = $(OBJS_interface_generator)
+  BINS_all = $(BINDIR)/interface_generator
+
+else
+  WARN_TARGETS += warning_libxmlpp
+endif
+
+ifeq ($(OBJSSUBMAKE),1)
+  ifneq ($(WARN_TARGETS),)
+all: $(WARN_TARGETS)
+  endif
+.PHONY: warning_libxmlpp
+warning_libxmlpp:
+	$(SILENT)echo -e "$(INDENT_PRINT)--> $(TRED)Omitting interface generator$(TNORMAL) (libxml++[-devel] not installed) +$(INDENT_PRINT)+ $(INDENT)"
+	$(SILENT)echo -e "$(INDENT_PRINT)--> $(TYELLOW)Interfaces cannot be generated$(TNORMAL)"
+endif
+
