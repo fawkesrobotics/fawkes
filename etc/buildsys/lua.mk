b6b38d46692536c70fc8664e1ab77523dd86ce86
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2008-03-12 23:59:33 +0000

    Added initial support for Lua wrapper generation
    + To interface with Lua we need to export some of our C++ classes and methods.
    . After some evaluation of Swig, luabind and tolua++ I have decided to use the
    . latter for now as it is easy to use and supports the features that we need.
    + Added lua subdir in lib, where C/C++ packages for lua will reside, without
    . a lib prefix for the so name.
    + Added lua.mk buildsys support. By including this file new targets and macros
    . become available needed to create interfaces.
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@785 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ etc/buildsys/lua.mk
@@ -0,0 +1,38 @@
+#*****************************************************************************
+#                 Makefile Build System for Fawkes: Lua bits
+#                            -------------------
+#   Created on Wed Mar 12 19:15:54 2008
+#   Copyright (C) 2006-2008 by Tim Niemueller, AllemaniACs RoboCup Team
+#
+#   $Id$
+#
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifndef __buildsys_lua_mk_
+__buildsys_lua_mk := 1
+
+ifneq ($(wildcard /usr/include/tolua++.h),)
+  HAVE_TOLUA = 1
+endif
+
+ifeq ($(HAVE_TOLUA),1)
+TOLUAPP=tolua++
+
+.SECONDEXPANSION:
+%_tolua.cpp: $$(TOLUA_$$(subst /,_,$$*))
+	$(SILENT) echo "$(INDENT_PRINT)--- Generating Lua package C++ file $(@F)"
+	$(SILENT)cat $(filter %.tolua,$^) | $(TOLUAPP) -n $(notdir $*) | \
+	sed -e 's/^\(.*Generated automatically .*\) on/\1/' | \
+	awk '/^#if defined/ { f=1 }; f { t = t "\n" $$0 }; !f {print}; f && /^#endif/ {print "extern \"C\" {" t "\n}\n"; f=0}' \
+	> $@
+endif # HAVE_TOLUA is 1
+
+endif # __buildsys_lua_mk_
+
