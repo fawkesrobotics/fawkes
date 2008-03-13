308956a5e21dd9b750532f78a16fc661be1a6e01
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2008-03-13 12:17:12 +0000

    Added Lua macros and standard warning target for wrappers
    + Lua related macros have been moved here from skiller.mk. tolua++ is only
    . marked available if Lua is available.
    + Added standard warning_tolua_wrapper target that can be used if a Lua
    . wrapper cannot be generated/built because tolua++ is not available.
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@789 fc82b16a-671c-0410-8732-9903a8d2dd0d

--- etc/buildsys/lua.mk
+++ etc/buildsys/lua.mk
@@ -18,12 +18,21 @@
 ifndef __buildsys_lua_mk_
 __buildsys_lua_mk := 1
 
-ifneq ($(wildcard /usr/include/tolua++.h),)
-  HAVE_TOLUA = 1
+include $(BASEDIR)/etc/buildsys/config.mk
+
+LUA_MINVERSION = 5.1
+
+# Check for Lua (Fedora packages lua and lua-devel)
+ifneq ($(PKGCONFIG),)
+  HAVE_LUA = $(if $(shell $(PKGCONFIG) --atleast-version $(LUA_MINVERSION) 'lua'; echo $${?/1/}),1,0)
 endif
 
-ifeq ($(HAVE_TOLUA),1)
-TOLUAPP=tolua++
+ifeq ($(HAVE_LUA),1)
+  CFLAGS_LUA = $(shell $(PKGCONFIG) --cflags 'lua')
+  LDFLAGS_LUA = $(shell $(PKGCONFIG) --libs 'lua')
+  ifneq ($(wildcard /usr/include/tolua++.h),)
+    HAVE_TOLUA = 1
+    TOLUAPP=tolua++
 
 .SECONDEXPANSION:
 %_tolua.cpp: $$(TOLUA_$$(subst /,_,$$*))
@@ -32,7 +41,15 @@ TOLUAPP=tolua++
 	sed -e 's/^\(.*Generated automatically .*\) on/\1/' | \
 	awk '/^#if defined/ { f=1 }; f { t = t "\n" $$0 }; !f {print}; f && /^#endif/ {print "extern \"C\" {" t "\n}\n"; f=0}' \
 	> $@
-endif # HAVE_TOLUA is 1
+
+  endif # HAVE_TOLUA is 1
+endif # HAVE_LUA is 1
+
+ifeq ($(OBJSSUBMAKE),1)
+.PHONY: warning_tolua_wrapper
+warning_tolua_wrapper:
+	$(SILENT)echo -e "$(INDENT_PRINT)--> $(TRED)Omitting Lua compatibility wrapper$(TNORMAL) (tolua++[-devel] not installed)"
+endif # OBJSSUBMAKE is 1
 
 endif # __buildsys_lua_mk_
 
