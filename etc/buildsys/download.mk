e17f74404b0d76f3f53e90c0d009b1ec88608193
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2008-04-06 22:17:29 +0000

    Download helper for build system
    + By using the macros from this buildsys add-on one can easily generate
    . download targets to get stuff off the web. See FawkesBuildSystemPatterns
    . wiki page for usage instructions.
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@870 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ etc/buildsys/download.mk
@@ -0,0 +1,47 @@
+#*****************************************************************************
+#              Makefile Build System for Fawkes: Download Support
+#                            -------------------
+#   Created on Sat Apr 05 00:11:59 2008
+#   Copyright (C) 2006-2008 by Tim Niemueller, AllemaniACs RoboCup Team
+#
+#   $Id$
+#
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifndef __buildsys_download_mk_
+__buildsys_download_mk := 1
+
+include $(BASEDIR)/etc/buildsys/config.mk
+
+DOWNLOAD_APPS=curl wget fetch
+DOWNLOAD_APP=$(firstword $(foreach a,$(DOWNLOAD_APPS),$(shell which $a 2>/dev/null)))
+
+DOWNLOAD_APP_PARAMS_curl=-O
+
+define download-files
+if [ -z "$(DOWNLOAD_APP)" ]; then \
+	echo -e "$(INDENT_PRINT)--> $(TRED)Cannot download files$(TNORMAL) (no download application found)"; \
+else \
+	if [ -z "$(URLS_$(patsubst download-%,%,$@))" ]; then \
+		echo -e "$(INDENT_PRINT)--> $(TRED)Cannot download files$(TNORMAL) (no URLs given)"; \
+	else \
+		for u in "$(URLS_$(patsubst download-%,%,$@))"; do \
+			echo -e "$(INDENT_PRINT)--> Downloading $$u using $(notdir $(DOWNLOAD_APP))"; \
+			if ! $(DOWNLOAD_APP) $(DOWNLOAD_APP_PARAMS_$(notdir $(DOWNLOAD_APP))) "$$u"; then \
+				echo -e "$(INDENT_PRINT)--> $(TRED)Download of $$u failed$(TNORMAL)"; \
+				exit 4; \
+			fi \
+		done \
+	fi \
+fi
+endef
+
+endif # __buildsys_download_mk_
+
