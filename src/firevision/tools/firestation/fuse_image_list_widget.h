35772b970bc4f30c7c8a54b43b2fb3e40df50290
Daniel Beck <beck@kbsg.rwth-aachen.de>
2008-04-12 18:15:23 +0000

    * Fixed header.
    db
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@911 fc82b16a-671c-0410-8732-9903a8d2dd0d

--- src/firevision/tools/firestation/fuse_image_list_widget.h
+++ src/firevision/tools/firestation/fuse_image_list_widget.h
@@ -9,8 +9,7 @@
  *
  ****************************************************************************/
 
-/*
- *  This program is free software; you can redistribute it and/or modify
+/*  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
@@ -20,9 +19,7 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software Foundation,
- *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
 #ifndef __FIREVISION_TOOLS_FIRESTATION_FUSE_IMAGE_LIST_WIDGET_H_
