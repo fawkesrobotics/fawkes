5546f8df30484dc76a5edf8e15ed054c360cb2b2
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2008-04-20 10:42:25 +0000

    Added C++ signature mangler
    + Added simple mangler for C++ signatures. It was taken from BlackBoard and
    . put here as a more appropriate place and to resolve some problems with
    . RCSoftX and a Fawkes RemoteBlackBoard...
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@997 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ src/libs/utils/misc/sigmangler.cpp
@@ -0,0 +1,52 @@
+
+/***************************************************************************
+ *  sigmangler.cpp - C++ Signature Mangler
+ *
+ *  Created: Sun Apr 20 11:03:57 2008 (GO2008, day 2)
+ *  Copyright  2008  Tim Niemueller [www.niemueller.de]
+ *
+ *  $Id$
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#include <utils/misc/sigmangler.h>
+
+#include <string>
+#include <cstring>
+
+/** @class CppSignatureMangler <utils/misc/sigmangler.h>
+ * C++ signature mangling.
+ * Simple methods to process C++ signatures. It is not true signature handling but
+ * rather hacking it to useful output for current GCC (4.x).
+ * @author Tim Niemueller
+ */
+
+/** Strip numbers at the beginning of the class type.
+ * This has been implemented by observations of C++ class names as returned by GCC's
+ * typeid operator.
+ * @param type type name to strip
+ * @return stripped class type, use delete to free it after you are done
+ */
+char *
+CppSignatureMangler::strip_class_type(const char *type)
+{
+  std::string t = type;
+  t = t.substr( t.find_first_not_of("0123456789") );
+  char *rv = new char[t.length() + 1];
+  strcpy(rv, t.c_str());
+  return rv;
+}
---
+++ src/libs/utils/misc/sigmangler.h
@@ -0,0 +1,36 @@
+
+/***************************************************************************
+ *  sigmangler.h - C++ Signature Mangler
+ *
+ *  Created: Sun Apr 20 11:01:55 2008 (GO2008, day 2)
+ *  Copyright  2008  Tim Niemueller [www.niemueller.de]
+ *
+ *  $Id$
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#ifndef __FVUTILS_MISC_SIGMANGLER_H_
+#define __FVUTILS_MISC_SIGMANGLER_H_
+
+class CppSignatureMangler
+{
+ public:
+  static char * strip_class_type(const char *type);
+};
+
+
+#endif
