0e21257bd9f0709eec99e49a59965b93f69a867c
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2008-08-11 13:52:15 +0000

    New InterfaceFieldPointer for a persistent field pointer
    + The new class InterfaceFieldPointer can be used to create a persistent
    . pointer to a value from an iterator. This can be useful for integration
    . with other software that needs a static assignment and cannot deal with
    . our BlackBoard directly.
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@1355 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ src/libs/interface/field_pointer.h
@@ -0,0 +1,96 @@
+
+/***************************************************************************
+ *  field_pointer.h - BlackBoard Interface Field Pointer
+ *
+ *  Created: Mon Oct 09 18:34:11 2006
+ *  Copyright  2006-2008  Tim Niemueller [www.niemueller.de]
+ *
+ *  $Id$
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#ifndef __INTERFACE_FIELD_POINTER_H_
+#define __INTERFACE_FIELD_POINTER_H_
+
+#include <interface/interface.h>
+
+namespace fawkes {
+
+/** Direct pointer to an interface field.
+ * This class allows for keeping a pointer to an interface value which is
+ * valid for the whole lifetime of the interface.
+ * @author Tim Niemueller
+ */
+template <typename FieldType>
+  class InterfaceFieldPointer
+{
+ public:
+  /** Constructor.
+   * @param type value type of the field
+   * @param name name of the field
+   * @param value pointer to the value of the field
+   */
+  InterfaceFieldPointer(Interface::interface_fieldtype_t type,
+			const char *name,
+			FieldType *value)
+  {
+    __type       = type;
+    __name       = name;
+    __value      = value;
+  }
+
+  /** Get the type of the field.
+   * @return type of the field
+   */
+  Interface::interface_fieldtype_t get_type() const
+  {
+    return __type;
+  }
+
+  /** Get name of the field.
+   * @return name of the field.
+   */
+  const char * get_name() const
+  {
+    return __name;
+  }
+
+  /** Get current value of the field.
+   * @return current vlaue of the field.
+   */
+  FieldType get_value() const
+  {
+    return *__value;
+  }
+
+  /** Set value of the field.
+   * @param value new value to set for the field.
+   */
+  void set_value(FieldType value)
+  {
+    *__value = value;
+  }
+
+ private:
+  Interface::interface_fieldtype_t  __type;
+  const char                       *__name;
+  volatile FieldType               *__value;
+};
+
+} // end namespace fawkes
+
+#endif
