528ad6f47c59c1490d7f081c377759acdeb5ff60
Daniel Beck <beck@kbsg.rwth-aachen.de>
2008-10-28 10:54:13 +0000

    Reanimated src/libs/gui_utils/utils.cpp.
    db
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@1472 fc82b16a-671c-0410-8732-9903a8d2dd0d

--- src/libs/gui_utils/utils.cpp
+++ src/libs/gui_utils/utils.cpp
@@ -1,8 +1,8 @@
 
 /***************************************************************************
- *  utils.h - General purpose GUI utility functions
+ *  utils.cpp - General purpose GUI utility functions
  *
- *  Created: Tue Sep 23 13:38:20 2008
+ *  Created: Tue Sep 23 13:41:30 2008
  *  Copyright  2008  Daniel Beck
  *
  *  $Id$
@@ -23,18 +23,26 @@
  *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
  */
 
-#ifndef __GUIUTILS_UTILS_H_
-#define __GUIUTILS_UTILS_H_
+#include <gui_utils/utils.h>
 
-#include <libglademm/xml.h>
-#include <gtkmm.h>
+/** Obtain a widget from a Glade XML file by its name.
+ * Use a dynamic cast to cast it to the expected widget-type.
+ * @param ref_xml Glade XML file
+ * @param widget_name name of the widget
+ */
+Gtk::Widget*
+fawkes::get_widget( Glib::RefPtr<Gnome::Glade::Xml> ref_xml,
+	    const char* widget_name )
+{
+  Gtk::Widget* widget;
+  ref_xml->get_widget(widget_name, widget);
+  if ( !widget )
+    { 
+      std::string err_str = "Couldn't find widget ";
+      err_str += std::string(widget_name);
+      err_str += ".";
+      throw std::runtime_error(err_str);
+    }
 
-namespace fawkes {
-
-Gtk::Widget* get_widget( Glib::RefPtr<Gnome::Glade::Xml> ref_xml,
-			 const char* widget_name );
-
+  return widget;
 }
-
-
-#endif /* __GUIUTILS_GET_WIDGET_H_ */
