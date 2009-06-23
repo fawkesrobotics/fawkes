91ae68378a038915d6df4d7eba6150d5317c465b
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2009-06-23 21:09:32 +0000

    Added Laser720Interface
    + Added laser interface that transports 720 beams
    
    Tim
    
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@2599 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ src/interfaces/Laser720Interface.cpp
@@ -0,0 +1,168 @@
+
+/***************************************************************************
+ *  Laser720Interface.cpp - Fawkes BlackBoard Interface - Laser720Interface
+ *
+ *  Templated created:   Thu Oct 12 10:49:19 2006
+ *  Copyright  2008  Tim Niemueller
+ *
+ *  $Id$
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#include <interfaces/Laser720Interface.h>
+
+#include <core/exceptions/software.h>
+
+#include <cstring>
+#include <cstdlib>
+
+namespace fawkes {
+
+/** @class Laser720Interface <interfaces/Laser720Interface.h>
+ * Laser720Interface Fawkes BlackBoard Interface.
+ * 
+      This interface provides access to data of a laser scanner that produces
+      720 beams per scan.
+    
+ * @ingroup FawkesInterfaces
+ */
+
+
+
+/** Constructor */
+Laser720Interface::Laser720Interface() : Interface()
+{
+  data_size = sizeof(Laser720Interface_data_t);
+  data_ptr  = malloc(data_size);
+  data      = (Laser720Interface_data_t *)data_ptr;
+  memset(data_ptr, 0, data_size);
+  add_fieldinfo(Interface::IFT_FLOAT, "distances", 720, &data->distances);
+  unsigned char tmp_hash[] = {0xc6, 0x5c, 0xe2, 0xcd, 0x6, 0x6c, 0xdb, 0x3f, 0x8c, 0x81, 0x78, 0xe1, 0xba, 0xf4, 0xc5, 0x17};
+  set_hash(tmp_hash);
+}
+
+/** Destructor */
+Laser720Interface::~Laser720Interface()
+{
+  free(data_ptr);
+}
+/* Methods */
+/** Get distances value.
+ * 
+      The distances in meter of the beams.
+    
+ * @return distances value
+ */
+float *
+Laser720Interface::distances() const
+{
+  return data->distances;
+}
+
+/** Get distances value at given index.
+ * 
+      The distances in meter of the beams.
+    
+ * @param index index of value
+ * @return distances value
+ * @exception Exception thrown if index is out of bounds
+ */
+float
+Laser720Interface::distances(unsigned int index) const
+{
+  if (index > 720) {
+    throw Exception("Index value %u out of bounds (0..720)", index);
+  }
+  return data->distances[index];
+}
+
+/** Get maximum length of distances value.
+ * @return length of distances value, can be length of the array or number of 
+ * maximum number of characters for a string
+ */
+size_t
+Laser720Interface::maxlenof_distances() const
+{
+  return 720;
+}
+
+/** Set distances value.
+ * 
+      The distances in meter of the beams.
+    
+ * @param new_distances new distances value
+ */
+void
+Laser720Interface::set_distances(const float * new_distances)
+{
+  memcpy(data->distances, new_distances, sizeof(float) * 720);
+}
+
+/** Set distances value at given index.
+ * 
+      The distances in meter of the beams.
+    
+ * @param new_distances new distances value
+ * @param index index for of the value
+ */
+void
+Laser720Interface::set_distances(unsigned int index, const float new_distances)
+{
+  if (index > 720) {
+    throw Exception("Index value %u out of bounds (0..720)", index);
+  }
+  data->distances[index] = new_distances;
+}
+/* =========== message create =========== */
+Message *
+Laser720Interface::create_message(const char *type) const
+{
+  throw UnknownTypeException("The given type '%s' does not match any known "
+                             "message type for this interface type.", type);
+}
+
+
+/** Copy values from other interface.
+ * @param other other interface to copy values from
+ */
+void
+Laser720Interface::copy_values(const Interface *other)
+{
+  const Laser720Interface *oi = dynamic_cast<const Laser720Interface *>(other);
+  if (oi == NULL) {
+    throw TypeMismatchException("Can only copy values from interface of same type (%s vs. %s)",
+                                type(), other->type());
+  }
+  memcpy(data, oi->data, sizeof(Laser720Interface_data_t));
+}
+
+/* =========== messages =========== */
+/** Check if message is valid and can be enqueued.
+ * @param message Message to check
+ */
+bool
+Laser720Interface::message_valid(const Message *message) const
+{
+  return false;
+}
+
+/// @cond INTERNALS
+EXPORT_INTERFACE(Laser720Interface)
+/// @endcond
+
+
+} // end namespace fawkes
