45c63028777743ac4919cfe41559ba94bff2c6c3
Daniel Beck <beck@kbsg.rwth-aachen.de>
2009-07-29 12:03:03 +0000

    First version of the readylogagent plugin.
    This thing is not really usable, yet, but here is what it can do:
    
    * Data can be read from/written to blackboard interfaces.
    * Message can be sent and received.
    * The central logger can be accessed from within the ECLiPSe context.
    * In each Fawkes loop a signal is posted to the ECLiPSe context.
    * The dummy interpreter shows how the interaction with the blackboard
    . and the logger works.
    
    db
    
    git-svn-id: https://lechuck.informatik.rwth-aachen.de:2342/svn/fawkes/trunk@2883 fc82b16a-671c-0410-8732-9903a8d2dd0d

---
+++ src/plugins/readylogagent/eclipse.mk
@@ -0,0 +1,33 @@
+#*****************************************************************************
+#              Makefile Build System for Fawkes: ECLiPSe Config
+#                            -------------------
+#   Created on Wed Jul 15 15:40:56 2009
+#   copyright (C) 2009 by Daniel Beck, AllemaniACs RoboCup Team
+#
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+include $(BASEDIR)/etc/buildsys/config.mk
+
+ECLIPSE_BINARY = eclipse
+
+HAVE_ECLIPSE = $(if $(shell which $(ECLIPSE_BINARY) > /dev/null; echo $${?/1/}),1,0)
+
+ifeq ($(HAVE_ECLIPSE),1)
+  HOSTARCH = $(shell $(ECLIPSE_BINARY) -e "get_flag(hostarch,A),printf(\"%p\", [A])")
+  ifeq ($(findstring $(patsubst i%86,i386,$(ARCH)),$(HOSTARCH)),)
+    HAVE_ECLIPSE = 0
+  endif
+
+  ECLIPSE_LIBDIR = $(shell $(ECLIPSE_BINARY) -e "get_flag(installation_directory,D),printf(\"%p\", [D])")/lib/$(HOSTARCH)
+  ECLIPSE_INCDIR = $(shell $(ECLIPSE_BINARY) -e "get_flag(installation_directory,D),printf(\"%p\", [D])")/include/$(HOSTARCH)
+
+  ECLIPSE_CFLAGS = -I$(ECLIPSE_INCDIR)
+  ECLIPSE_LDFLAGS = -L$(ECLIPSE_LIBDIR) -Wl,-R$(ECLIPSE_LIBDIR)
+endif
