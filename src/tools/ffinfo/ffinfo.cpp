8e0f1de2257c5cec2387794e31981d9f6f94f2fc
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2009-08-07 23:49:50 +0200

    Added ffinfo tool to get info about the Fawkes Framework
    Currently always prints the version number.
    
    Tim

---
+++ src/tools/ffinfo/ffinfo.cpp
@@ -0,0 +1,42 @@
+
+/***************************************************************************
+ *  ffinfo.cpp - Fawkes info tool
+ *
+ *  Created: Fri Aug 07 23:37:57 2009
+ *  Copyright  2006-2009  Tim Niemueller [www.niemueller.de]
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <core/version.h>
+
+#include <cstdio>
+
+
+/** Info tool main.
+ * @param argc argument count
+ * @param argv arguments
+ */
+int
+main(int argc, char **argv)
+{
+  printf("Fawkes %u.%u", FAWKES_VERSION_MAJOR, FAWKES_VERSION_MINOR);
+  if (FAWKES_VERSION_MICRO > 0) {
+    printf(".%u", FAWKES_VERSION_MICRO);
+  }
+  printf("\n");
+
+  return 0;
+}
