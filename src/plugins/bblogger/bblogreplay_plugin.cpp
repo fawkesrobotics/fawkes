799dbae1a3bc90f6ee45c64df87f4afc69e45fdb
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-02-22 00:58:41 +0100

    bblogreplay: support blocked timing replay integrated into main loop
    There will be scenarios where you want to have the same timing guarantees
    for log replay that you have for, e.g., real sensors. This can now be done
    by defining a hook at which the log data should be written. For now, one
    data item per time will be written, the loop maybe slowed down to replay
    time. A future enhancement can be to check at a certain loop if new data
    is available and only write if it is, not slowing down that loop. But there
    must be something for tomorrow...

--- src/plugins/bblogger/bblogreplay_plugin.cpp
+++ src/plugins/bblogger/bblogreplay_plugin.cpp
@@ -23,6 +23,7 @@
 
 #include "bblogreplay_plugin.h"
 #include "logreplay_thread.h"
+#include "logreplay_bt_thread.h"
 
 #include <utils/time/time.h>
 
@@ -63,8 +64,9 @@ BlackBoardLogReplayPlugin::BlackBoardLogReplayPlugin(Configuration *config)
   }
 
   std::string scenario_prefix = prefix + scenario + "/";
-  std::string log_prefix   = scenario_prefix + "log/";
-  std::string loop_prefix   = scenario_prefix + "loop/";
+  std::string log_prefix      = scenario_prefix + "log/";
+  std::string loop_prefix     = scenario_prefix + "loop/";
+  std::string hook_prefix     = scenario_prefix + "hook/";
 
   std::string logdir = LOGDIR;
   try {
@@ -85,24 +87,66 @@ BlackBoardLogReplayPlugin::BlackBoardLogReplayPlugin(Configuration *config)
   try {
     loop_replay = config->get_bool((scenario_prefix + std::string("loop")).c_str());
   } catch (Exception &e) { /* ignored, use default set above */ }
-  
-
+    
   Configuration::ValueIterator *i = config->search(log_prefix.c_str());
   while (i->next()) {
     bool loop_replay_i = loop_replay;
-    try {
-      char * log_reference = (char*) strrchr(i->path(),'/');
-      if(log_reference != NULL){
-	log_reference++;
+    std::string hook_str = "";
+    char * log_reference = (char*) strrchr(i->path(),'/');
+    if(log_reference != NULL){
+      log_reference++;
 	
+      try {
 	loop_replay_i = config->get_bool((loop_prefix + std::string(log_reference)).c_str());
+      } catch (Exception &e) { /* ignored, use default set above */ }
+      try {
+	hook_str = config->get_string((hook_prefix + std::string(log_reference)).c_str());
+	printf("%s = %s\n", (hook_prefix + std::string(log_reference)).c_str(), hook_str.c_str());
+      } catch (Exception &e) { /* ignored, use default set above */ }
+    }
+
+    if (hook_str != "") {
+      BlockedTimingAspect::WakeupHook hook;
+      hook = BlockedTimingAspect::WAKEUP_HOOK_PRE_LOOP;
+
+      if (hook_str == "pre_loop") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_PRE_LOOP;
+      } else if (hook_str == "sensor") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_SENSOR;
+      } else if (hook_str == "sensor_process") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_SENSOR_PROCESS;
+      } else if (hook_str == "worldstate") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_WORLDSTATE;
+      } else if (hook_str == "think") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_THINK;
+      } else if (hook_str == "skill") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_SKILL;
+      } else if (hook_str == "act") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_ACT;
+      } else if (hook_str == "act_exec") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_ACT_EXEC;
+      } else if (hook_str == "post_loop") {
+	hook = BlockedTimingAspect::WAKEUP_HOOK_POST_LOOP;
+      } else {
+	throw Exception("Invalid hook '%s' for %s",
+			hook_str.c_str(), i->get_string().c_str());
       }
-    } catch (Exception &e) { /* ignored, use default set above */ }
-    BBLogReplayThread *log_replay_thread = new BBLogReplayThread(i->get_string().c_str(),
-								 logdir.c_str(),
-								 scenario.c_str(),
-								 loop_replay_i);
-    thread_list.push_back(log_replay_thread);
+
+      BBLogReplayBlockedTimingThread *lrbt_thread;
+	lrbt_thread = new BBLogReplayBlockedTimingThread(hook,
+							 i->get_string().c_str(),
+							 logdir.c_str(),
+							 scenario.c_str(),
+							 loop_replay_i);
+      thread_list.push_back(lrbt_thread);
+    } else {
+      BBLogReplayThread *lr_thread = new BBLogReplayThread(i->get_string().c_str(),
+							   logdir.c_str(),
+							   scenario.c_str(),
+							   loop_replay_i);
+      thread_list.push_back(lr_thread);
+    }
+
   }
   delete i;
 
