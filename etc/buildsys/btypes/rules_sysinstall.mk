f40e4e498f66420bf1611e2255bb97e3569f8ca6
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-11-04 15:31:20 -0400

    BuildSys/install: support uncolored-install

--- etc/buildsys/btypes/rules_sysinstall.mk
+++ etc/buildsys/btypes/rules_sysinstall.mk
@@ -29,7 +29,8 @@ $(foreach L,$(LIBS_all:$(LIBDIR)/lua/%.so=%),$(if $L,$(eval INST_LIB_SUBDIR_lua_
 endif
 
 # Main install target
-.PHONY: install install_test_basedir install_config install_buildsys install_lua install_apidoc
+.PHONY: install install_test_basedir install_config install_buildsys install_lua install_apidoc uncolored-install
+uncolored-install: install
 install: install_test_basedir presubdirs $(subst $(LIBDIR),$(DESTDIR)$(EXEC_LIBDIR),$(LIBS_all) $(LIBS_gui)) $(subst $(PLUGINDIR),$(DESTDIR)$(EXEC_PLUGINDIR),$(PLUGINS_all)) $(subst $(BINDIR),$(DESTDIR)$(EXEC_BINDIR),$(BINS_all) $(BINS_gui)) resdirs subdirs install_buildsys install_config install_lua install_apidoc
 
 # Only allow "make install" from basedir
