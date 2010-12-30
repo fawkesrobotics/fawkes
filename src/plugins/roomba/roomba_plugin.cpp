b8618eddd74660bcdc4d34d2b235413df4d8b927
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-12-31 00:05:44 +0100

    roomba: initial plugin skeleton

---
+++ src/plugins/roomba/roomba_plugin.cpp
@@ -0,0 +1,43 @@
+
+/***************************************************************************
+ *  roomba_plugin.cpp - Plugin to interface with a Roomba
+ *
+ *  Created: Thu Dec 30 22:05:07 2010
+ *  Copyright  2006-2010  Tim Niemueller [www.niemueller.de]
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "roomba_plugin.h"
+
+using namespace fawkes;
+
+/** @class RoombaPlugin "roomba_plugin.h"
+ * Plugin to interface with a Roomba robot.
+ * @author Tim Niemueller
+ */
+
+/** Constructor.
+ * @param config Fawkes configuration
+ */
+RoombaPlugin::RoombaPlugin(Configuration *config)
+  : Plugin(config)
+{
+  //thread_list.push_back();
+}
+
+
+PLUGIN_DESCRIPTION("Roomba vacuum robot plugin.")
+EXPORT_PLUGIN(RoombaPlugin)
