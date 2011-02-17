0da9bf96baec367c6e718b163beea5962db3d338
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2011-02-17 10:53:58 +0100

    openni: plugin skeleton

---
+++ src/plugins/openni/openni_plugin.cpp
@@ -0,0 +1,44 @@
+
+/***************************************************************************
+ *  openni_plugin.cpp - Plugin to access OpenNI features
+ *
+ *  Created: Thu Feb 17 10:23:03 2011
+ *  Copyright  2006-2011  Tim Niemueller [www.niemueller.de]
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "openni_plugin.h"
+
+using namespace fawkes;
+
+/** @class OpenNiPlugin "openni_plugin.h"
+ * Plugin to access OpenNI from Fawkes.
+ * This plugin integrates OpenNI and provides access to the OpenNI context
+ * to other plugins.
+ * @author Tim Niemueller
+ */
+
+/** Constructor.
+ * @param config Fawkes configuration
+ */
+OpenNiPlugin::OpenNiPlugin(Configuration *config)
+  : Plugin(config)
+{
+}
+
+
+PLUGIN_DESCRIPTION("OpenNI integration base plugin")
+EXPORT_PLUGIN(OpenNiPlugin)
