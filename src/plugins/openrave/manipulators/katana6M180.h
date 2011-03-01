853abd58540ff5155c847a9a391075e57902edab
Bahram Maleki-Fard <maleki-fard@kbsg.rwth-aachen.de>
2011-03-01 13:36:26 +0100

    openrave: Included manipulators folder from libs/openrave
    * Folder is still needed, as our model of the katana arm
    . differs from the original one. With COLLUDA support in
    . OpenRAVE this can be reduced to one file though. Hopefully
    . this will be done in the near future.

--- src/plugins/openrave/manipulators/katana6M180.h
+++ src/plugins/openrave/manipulators/katana6M180.h
@@ -20,8 +20,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef __OPENRAVE_MANIPULATORS_KATANA6M180_H_
-#define __OPENRAVE_MANIPULATORS_KATANA6M180_H_
+#ifndef __PLUGINS_OPENRAVE_MANIPULATORS_KATANA6M180_H_
+#define __PLUGINS_OPENRAVE_MANIPULATORS_KATANA6M180_H_
 
 #include "../manipulator.h"
 
