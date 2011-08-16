279372c3de3491f8b0c172523f48e9260aafa767
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2011-08-16 15:58:53 +0200

    fvconf: bring back OpenCV 2.1 support
    Yeah, we even have 2.1 somewhere...

--- etc/buildsys/fvconf.mk
+++ etc/buildsys/fvconf.mk
@@ -87,7 +87,7 @@ ifeq ($(HAVE_OPENCV),1)
   VERSION_MAJOR_OPENCV    = $(word 1,$(VERSION_SPLITTED_OPENCV))
   VERSION_MINOR_OPENCV    = $(word 2,$(VERSION_SPLITTED_OPENCV))
   CFLAGS_OPENCV      = -DHAVE_OPENCV $(shell $(PKGCONFIG) --cflags 'opencv')
-  ifeq ($(VERSION_MAJOR_OPENCV).$(VERSION_MINOR_OPENCV),2.0)
+  ifeq ($(filter-out 2.0 2.1,$(VERSION_MAJOR_OPENCV).$(VERSION_MINOR_OPENCV)),)
     LDFLAGS_OPENCV     = -lcxcore
     LDFLAGS_OPENCV_GUI = -lhighgui
   else
