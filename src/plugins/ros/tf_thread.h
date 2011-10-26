00949aa1bedc5d5adca79a0231fb9bf74afd0e4f
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2011-10-26 16:32:03 +0200

    ros-tf: plugin to exchange transforms between Fawkes and ROS
    This plugin hooks into the Fawkes and ROS tf systems and forwards the
    transforms in both directions. It tries to be clever enough as to not
    re-post information it just forwarded. Tested by visualizing transforms
    published with the static-transforms Fawkes plugin with ROS' rviz.

---
+++ src/plugins/ros/tf_thread.h
@@ -0,0 +1,99 @@
+
+/***************************************************************************
+ *  tf_thread.h - Thread to exchange transforms
+ *
+ *  Created: Wed Oct 26 00:50:12 2011
+ *  Copyright  2011  Tim Niemueller [www.niemueller.de]
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_ROS_TF_THREAD_H_
+#define __PLUGINS_ROS_TF_THREAD_H_
+
+#include <core/threading/thread.h>
+#include <aspect/blocked_timing.h>
+#include <aspect/clock.h>
+#include <aspect/configurable.h>
+#include <aspect/logging.h>
+#include <aspect/blackboard.h>
+#include <plugins/ros/aspect/ros.h>
+#include <blackboard/interface_listener.h>
+#include <blackboard/interface_observer.h>
+#include <interfaces/TransformInterface.h>
+#include <core/threading/mutex.h>
+
+// from Fawkes
+#include <tf/transform_publisher.h>
+
+#include <list>
+#include <queue>
+
+// from ROS
+#include <tf/tfMessage.h>
+
+class RosTfThread
+: public fawkes::Thread,
+  public fawkes::ClockAspect,
+  public fawkes::LoggingAspect,
+  public fawkes::ConfigurableAspect,
+  public fawkes::BlockedTimingAspect,
+  public fawkes::BlackBoardAspect,
+  public fawkes::ROSAspect,
+  public fawkes::BlackBoardInterfaceObserver,
+  public fawkes::BlackBoardInterfaceListener
+{
+ public:
+  RosTfThread();
+  virtual ~RosTfThread();
+
+  virtual void init();
+  virtual void loop();
+  virtual void finalize();
+
+  // for BlackBoardInterfaceObserver
+  virtual void bb_interface_created(const char *type, const char *id) throw();
+
+  // for BlackBoardInterfaceListener
+  virtual void bb_interface_data_changed(fawkes::Interface *interface) throw();
+  virtual void bb_interface_writer_removed(fawkes::Interface *interface,
+                                           unsigned int instance_serial) throw();
+  virtual void bb_interface_reader_removed(fawkes::Interface *interface,
+                                           unsigned int instance_serial) throw();
+
+ private:
+  void tf_message_cb(const tf::tfMessage::ConstPtr &msg);
+  void conditional_close(fawkes::Interface *interface) throw();
+
+ /** Stub to see name in backtrace for easier debugging. @see Thread::run() */
+ protected: virtual void run() { Thread::run(); }
+
+ private:
+  std::list<fawkes::TransformInterface *> __tfifs;
+
+  fawkes::tf::TransformPublisher *__tf_publisher;
+
+  ros::Subscriber __sub_tf;
+  ros::Publisher  __pub_tf;
+
+  fawkes::Mutex *__tf_msg_queue_mutex;
+  unsigned int __active_queue;
+  std::queue<tf::tfMessage::ConstPtr>   __tf_msg_queues[2];
+
+  fawkes::Mutex *__seq_num_mutex;
+  unsigned int   __seq_num;
+
+};
+
+#endif
