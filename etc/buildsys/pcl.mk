186ed9205410c2f1b35fecb0e637f6c565657578
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2011-11-08 17:05:01 +0100

    buildsys: add pcl detection and config file

---
+++ etc/buildsys/pcl.mk
@@ -0,0 +1,72 @@
+#*****************************************************************************
+#                 Makefile Build System for Fawkes: PCL bits
+#                            -------------------
+#   Created on Mon Nov 07 01:06:16 2011
+#   Copyright (C) 2011 by Tim Niemueller, AllemaniACs RoboCup Team
+#
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifndef __buildsys_config_mk_
+$(error config.mk must be included before pcl.mk)
+endif
+
+ifndef __buildsys_pcl_mk_
+__buildsys_pcl_mk_ := 1
+
+PCL_VERSION = 1.2
+
+ifneq ($(PKGCONFIG),)
+  HAVE_PCL = $(if $(shell $(PKGCONFIG) --exists 'pcl_common-$(PCL_VERSION)'; echo $${?/1/}),1,0)
+  HAVE_EIGEN3 = $(if $(shell $(PKGCONFIG) --exists 'eigen3'; echo $${?/1/}),1,0)
+endif
+
+ifeq ($(HAVE_EIGEN3),1)
+  CFLAGS_EIGEN3  = -DHAVE_EIGEN3 $(shell $(PKGCONFIG) --cflags 'eigen3') \
+		   -DEIGEN_USE_NEW_STDVECTOR \
+		   -DEIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
+  LDFLAGS_EIGEN3 = $(shell $(PKGCONFIG) --libs 'eigen3')
+else
+  HAVE_PCL = 0
+endif
+
+ifneq ($(wildcard $(SYSROOT)/usr/include/vtk/vtkVersion.h),)
+  HAVE_VTK = 1
+  CFLAGS_VTK = -I/usr/include/vtk
+endif
+
+
+
+ifeq ($(HAVE_PCL),1)
+  # if we have ROS, by default use std_msgs and sensor_msgs from there, rather
+  # than the types that come with PCL, because it'll crash miserably when pumping
+  # data from Fawkes to ROS, it might cause problems the other way around
+  # ifeq ($(HAVE_ROS),1)
+  #   CFLAGS_PCL  = $(call ros-pkg-cflags,sensor_msgs) \
+  # 		  $(call ros-pkg-cflags,std_msgs) \
+  # 		  -DHAVE_ROS_STD_MSGS
+  #   LDFLAGS_PCL = $(call ros-pkg-lflags,sensor_msgs) \
+  # 		  $(call ros-pkg-lflags,std_msgs) \
+  # 		  -DHAVE_ROS_SENSOR_MSGS
+  # endif
+
+  CFLAGS_PCL  += -DHAVE_PCL $(CFLAGS_EIGEN3) \
+		 $(shell $(PKGCONFIG) --cflags 'pcl_common-$(PCL_VERSION)')
+  LDFLAGS_PCL += $(foreach L,common features filters kdtree keypoints octree \
+			range_image range_image_border_extractor registration \
+			sample_consensus segmentation surface, \
+		   $(shell $(PKGCONFIG) --libs 'pcl_$L-$(PCL_VERSION)') ) \
+		 $(LDFLAGS_EIGEN3)
+  # need to fix PCL's pkg-config files first
+  #LDFLAGS_PCL_VIS = $(shell $(PKGCONFIG) --libs 'pcl_visualization-$(PCL_VERSION)')
+  #LDFLAGS_PCL_IO = $(shell $(PKGCONFIG) --libs 'pcl_io-$(PCL_VERSION)')
+endif
+
+endif # __buildsys_pcl_mk_
+
