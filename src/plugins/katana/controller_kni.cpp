8dbec5be50bbe7221f1f3c6b408d7bd5a1292e56
Bahram Maleki-Fard <maleki-fard@kbsg.rwth-aachen.de>
2012-01-04 18:03:50 +0100

    katana: Add controller class to encapsulate katana control
    * Encapsulation katana control from the general integration
    . of katana functionality (e.g. message processing) allows
    . usage of different controllers. This is a first step to
    . allow simulation of the katana arm (e.g. by an OpenRAVE
    . controller) without loosing any of the current functionality
    . for skills and the like.
    * It is also possible to implement a "dummy" controller which
    . could come in handy if we just want to test new KatanaInterface
    . messages withough actually needing to run a real katana arm
    . or power consuming simulations.
    
    * Current version contains only an abstract and empty controller
    . class, and a deriving class for a KNI-controller.

---
+++ src/plugins/katana/controller_kni.cpp
@@ -0,0 +1,40 @@
+
+/***************************************************************************
+ *  controller_kni.cpp - KNI Controller class for katana arm
+ *
+ *  Created: Tue Jan 03 11:40:31 2012
+ *  Copyright  2012  Bahram Maleki-Fard, AllemaniACs RoboCup Team
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "controller_kni.h"
+
+namespace fawkes {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+/** Constructor. */
+KatanaControllerKni::KatanaControllerKni()
+{
+}
+
+/** Destructor. */
+KatanaControllerKni::~KatanaControllerKni()
+{
+}
+
+} // end of namespace fawkes
---
+++ src/plugins/katana/controller_kni.h
@@ -0,0 +1,47 @@
+
+/***************************************************************************
+ *  controller_kni.h - KNI Controller class for katana arm
+ *
+ *  Created: Tue Jan 03 11:40:31 2012
+ *  Copyright  2012  Bahram Maleki-Fard, AllemaniACs RoboCup Team
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_KATANA_CONTROLLER_KNI_H_
+#define __PLUGINS_KATANA_CONTROLLER_KNI_H_
+
+#include "controller.h"
+
+namespace fawkes {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+
+class KatanaControllerKni : public KatanaController
+{
+ public:
+  KatanaControllerKni();
+  virtual ~KatanaControllerKni();
+
+ private:
+
+};
+
+
+} // end of namespace fawkes
+
+#endif
