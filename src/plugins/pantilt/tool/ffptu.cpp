6e025f9f38454f8ae8c96dddfa0723b8c9bb7e07
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2012-04-02 17:35:01 +0200

    ffptu: support RX28 commands, especially for setting ID
    ffptu now takes commands on the command line. It can be moved by command
    line arguments (but the interactive mode is still available).
    Additionally it can now directly interact with Robotis RX28 PTUs, for
    eaxmple to set servo IDs.

--- src/plugins/pantilt/tool/ffptu.cpp
+++ src/plugins/pantilt/tool/ffptu.cpp
@@ -20,6 +20,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
+#include "../robotis/rx28.h"
+
 #include <core/exceptions/system.h>
 #include <utils/system/argparser.h>
 #include <logging/console.h>
@@ -30,6 +32,8 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
+#include <unistd.h>
 
 #include <interfaces/PanTiltInterface.h>
 
@@ -38,12 +42,23 @@ using namespace fawkes;
 void
 print_usage(const char *program_name)
 {
-  printf("Usage: %s [-h] [-r host[:port]]\n"
+  printf("Usage: %s [-h] [-r host[:port]] <command>\n"
+         " Options:\n"
 	 " -h              This help message\n"
 	 " -r host[:port]  Remote host (and optionally port) to connect to\n"
          " -p <ptu>        PTU, interface must have id 'PanTilt <ptu>'\n"
          " -l              List available PTUs\n"
-         " -i              Invert tilt control buttons\n",
+         " -i              Invert tilt control buttons\n\n"
+         " Commands:\n"
+         "move [pan P] [tilt T]\n"
+         "  Move PTU, if either pan or tilt are supplied, send the\n"
+         "  command and wait for angle to be reached. Otherwise enter\n"
+         "  interactive mode\n"
+         "rx28-set-id <old-id> <new-id>\n"
+         "  Send message to set a new servo ID.\n"
+         "  WARNING: use this with only a single servo connected!\n"
+         "rx28-discover\n"
+         "  Discover and print servos on the BUS.\n",
 	 program_name);
 }
 
@@ -61,8 +76,10 @@ class PTUJoystickControl
   PTUJoystickControl(int argc, char **argv)
     : __argp(argc, argv, "hr:p:li")
   {
+    __rx28 = NULL;
     __bb = NULL;
     __ptu_if = NULL;
+    __resolution = 0.1f;
 
     if ( __argp.has_arg("h") ) {
       print_usage(argv[0]);
@@ -77,11 +94,12 @@ class PTUJoystickControl
       __bb->close(__ptu_if);
       delete __bb;
     }
+    delete __rx28;
   }
 
 
   /** Initialize BB connection. */
-  void init()
+  void init_bb()
   {
     char *host = (char *)"localhost";
     unsigned short int port = 1910;
@@ -128,14 +146,49 @@ class PTUJoystickControl
       throw Exception("No suitable PanTiltInterface found");
     } 
 
-    __resolution = 0.1f;
+  }
+
+  /** Initialize Robotis RX28 raw servo access. */
+  void init_rx28()
+  {
+    __rx28 = new RobotisRX28("/dev/ttyUSB0");
+    RobotisRX28::DeviceList devl = __rx28->discover();
+
+    if (devl.empty()) {
+      throw Exception("No devices found\n");
+      
+    }
+
   }
 
 
   /** Run control loop. */
   void run()
   {
-    if (!__ptu_if)  return;
+    if (__argp.num_items() == 0) {
+      interactive_move();
+    } else if (strcmp(__argp.items()[0], "move") == 0) {
+      if (__argp.num_items() == 1) {
+        interactive_move();
+      } else {
+        exec_move();
+      }
+    } else if (strcmp(__argp.items()[0], "rx28-set-id") == 0) {
+      rx28_set_id();
+
+    } else if (strcmp(__argp.items()[0], "rx28-discover") == 0) {
+      rx28_discover();
+
+    } else {
+      printf("Unknown command '%s'\n", __argp.items()[0]);
+    }
+  }
+
+ private:
+
+  void interactive_move()
+  {
+    init_bb();
 
     Time last, now;
 
@@ -151,33 +204,32 @@ class PTUJoystickControl
     tilt = new_tilt = __ptu_if->tilt();
 
     last.stamp();
-
     char key = 0;
     int wait_time = 5;
     while (key != 'q') {
       key = getkey(wait_time);
       //printf("Key: %u = %u\n", key, key);
       if (key != 0) {
-	now.stamp();
-	if ( (now - &last) < 0.5) {
-	  wait_time = 1;
-	}
-	last.stamp();
+        now.stamp();
+        if ( (now - &last) < 0.5) {
+          wait_time = 1;
+        }
+        last.stamp();
       }
       if (key == 0) {
-	wait_time = 5;
-
+        wait_time = 5;
+        
       } else if (key == 27) {
-	key = getkey();
-	if (key == 0) {
-	  // Escape key
-	  new_pan = pan;
+        key = getkey();
+        if (key == 0) {
+          // Escape key
+          new_pan = pan;
           new_tilt = tilt;
-	} else {
-	  if (key != 91) continue;
+        } else {
+          if (key != 91) continue;
 
-	  key = getkey();
-	  if (key == 0) continue;
+          key = getkey();
+          if (key == 0) continue;
 
           if (key == tilt_up) {
             new_tilt = std::min(tilt + __resolution, __ptu_if->max_tilt());
@@ -189,7 +241,7 @@ class PTUJoystickControl
             new_pan = std::min(pan + __resolution, __ptu_if->max_pan());
           } else continue;
 
-	}
+        }
       } else if (key == '0') {
         new_pan = new_tilt = 0.f;
       } else if (key == '9') {
@@ -231,11 +283,107 @@ class PTUJoystickControl
     }
   }
 
+
+  void exec_move()
+  {
+    init_bb();
+
+    float pan, tilt, new_pan, new_tilt;
+
+    __ptu_if->read();
+    pan  = new_pan  = __ptu_if->pan();
+    tilt = new_tilt = __ptu_if->tilt();
+
+    const std::vector< const char * > &items = __argp.items();
+    for (unsigned int i = 1; i < items.size(); ++i) {
+      if (strcmp(items[i], "pan") == 0) {
+        if (items.size() > i+1) {
+          new_pan = __argp.parse_item_float(++i);
+        } else {
+          printf("No pan value supplied, aborting.\n");
+          return;
+        }
+      } else if (strcmp(items[i], "tilt") == 0) {
+        if (items.size() > i+1) {
+          new_tilt = __argp.parse_item_float(++i);
+        } else {
+          printf("No tilt value supplied, aborting.\n");
+          return;
+        }
+      } else {
+        printf("Unknown parameter '%s', aborting.\n", items[i]);
+        return;
+      }
+    }
+
+    if ((pan != new_pan) || (tilt != new_tilt)) {
+      printf("Goto pan %f and tilt %f\n", new_pan, new_tilt);
+
+      PanTiltInterface::SetVelocityMessage *svm =
+        new PanTiltInterface::SetVelocityMessage(__ptu_if->max_pan_velocity() / 2.,
+                                                 __ptu_if->max_tilt_velocity() / 2.);
+      __ptu_if->msgq_enqueue(svm);
+
+      PanTiltInterface::GotoMessage *gm =
+        new PanTiltInterface::GotoMessage(new_pan, new_tilt);
+      __ptu_if->msgq_enqueue(gm);
+      usleep(5e5);
+    }
+  }
+
+
+  void rx28_set_id()
+  {
+    init_rx28();
+
+    int old_id = __argp.parse_item_int(1);
+    int new_id = __argp.parse_item_int(2);
+
+    printf("Servo IDs *before* setting the ID:\n");
+    RobotisRX28::DeviceList devl = __rx28->discover();
+    for (RobotisRX28::DeviceList::iterator i = devl.begin(); i != devl.end(); ++i) {
+      printf("  %d\n", *i);
+    }
+
+    if (old_id < 0 || old_id >= RobotisRX28::BROADCAST_ID) {
+      printf("Invalid old ID %i, must be in range [0..%u]\n",
+             old_id, RobotisRX28::BROADCAST_ID);
+      return;
+    }
+
+    if (new_id < 0 || new_id >= RobotisRX28::BROADCAST_ID) {
+      printf("Invalid new ID %i, must be in range [0..%u]\n",
+             new_id, RobotisRX28::BROADCAST_ID);
+      return;
+    }
+
+    __rx28->set_id(old_id, new_id);
+
+    printf("Servo IDs *after* setting the ID:\n");
+    devl = __rx28->discover();
+    for (RobotisRX28::DeviceList::iterator i = devl.begin(); i != devl.end(); ++i) {
+      printf("  %d\n", *i);
+    }
+  }
+
+  void rx28_discover()
+  {
+    init_rx28();
+
+    printf("Servo IDs on the bus:\n");
+    RobotisRX28::DeviceList devl = __rx28->discover();
+    for (RobotisRX28::DeviceList::iterator i = devl.begin(); i != devl.end(); ++i) {
+      printf("  %d\n", *i);
+    }
+  }
+
+
  private:
   ArgumentParser __argp;
   BlackBoard *__bb;
   PanTiltInterface *__ptu_if;
   float __resolution;
+  RobotisRX28 *__rx28;
 };
 
 
@@ -248,7 +396,6 @@ main(int argc, char **argv)
 {
   try {
     PTUJoystickControl ptuctrl(argc, argv);
-    ptuctrl.init();
     ptuctrl.run();
   } catch (Exception e) {
     printf("Running failed: %s\n\n", e.what());
