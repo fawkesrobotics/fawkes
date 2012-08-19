2ba55bc3e30597dba0ca4b86dbc1bee764e15b6f
Victor Mataré <matare@lih.rwth-aachen.de>
2012-08-19 21:54:06 +0200

    make start.lua patch-compatible with ROS skiller
    start.lua from fawkes goes to fawkes/ subdir, while a ROS-specific
    start.lua fell from the sky in src/lua/skiller.

---
+++ src/lua/skiller/fawkes/start.lua
@@ -0,0 +1,62 @@
+
+----------------------------------------------------------------------------
+--  start.lua - skiller Lua start code
+--              executed when exec thread is running, but before skills are
+--              executed. Only run if initialization was successful.
+--
+--  Created: Thu Mar 13 11:24:40 2008
+--  Copyright  2008-2009  Tim Niemueller [www.niemueller.de]
+--
+----------------------------------------------------------------------------
+
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Library General Public License for more details.
+--
+--  Read the full text in the LICENSE.GPL file in the doc directory.
+
+-- lists whole config
+--[[
+v = config:iterator()
+while ( v:next() ) do
+   if ( v:is_float() ) then
+      print(v:path(), "[float]", v:get_float())
+   elseif ( v:is_uint() ) then
+      print(v:path(), "[uint]", v:get_uint())
+   elseif ( v:is_int() ) then
+      print(v:path(), "[int]", v:get_int())
+   elseif ( v:is_bool() ) then
+      print(v:path(), "[bool]", v:get_bool())
+   elseif ( v:is_string() ) then
+      print(v:path(), "[string]", v:get_string())
+   end
+end
+--]]
+-- prints all interfaces
+--[[
+for k,v in pairs(interfaces) do
+   for k2,v2 in pairs(v) do
+      print(k, k2)
+   end
+end
+--]]
+
+require("fawkes.logprint")
+fawkes.logprint.init(logger)
+
+require("fawkes.mathext")
+
+skillenv = require("skiller.skillenv")
+require("skiller.skillhsm")
+
+skillenv.init(SKILLSPACE)
+
+--skiller.skillhsm.SkillHSM:set_debug(true)
+
+logger:log_debug("Lua startup completed")
--- src/lua/skiller/start.lua
+++ src/lua/skiller/start.lua
@@ -1,11 +1,9 @@
 
 ----------------------------------------------------------------------------
---  start.lua - skiller Lua start code
---              executed when exec thread is running, but before skills are
---              executed. Only run if initialization was successful.
+--  start.lua - Skiller startup script -- ROS version
 --
---  Created: Thu Mar 13 11:24:40 2008
---  Copyright  2008-2009  Tim Niemueller [www.niemueller.de]
+--  Created: Tue Aug  3 15:29:28 2010 (at Intel Research, Pittsburgh)
+--  Copyright  2010  Tim Niemueller [www.niemueller.de]
 --
 ----------------------------------------------------------------------------
 
@@ -21,42 +19,25 @@
 --
 --  Read the full text in the LICENSE.GPL file in the doc directory.
 
--- lists whole config
---[[
-v = config:iterator()
-while ( v:next() ) do
-   if ( v:is_float() ) then
-      print(v:path(), "[float]", v:get_float())
-   elseif ( v:is_uint() ) then
-      print(v:path(), "[uint]", v:get_uint())
-   elseif ( v:is_int() ) then
-      print(v:path(), "[int]", v:get_int())
-   elseif ( v:is_bool() ) then
-      print(v:path(), "[bool]", v:get_bool())
-   elseif ( v:is_string() ) then
-      print(v:path(), "[string]", v:get_string())
-   end
-end
---]]
--- prints all interfaces
---[[
-for k,v in pairs(interfaces) do
-   for k2,v2 in pairs(v) do
-      print(k, k2)
-   end
-end
---]]
-
-require("fawkes.logprint")
-fawkes.logprint.init(logger)
+require("actionlib")
 
 require("fawkes.mathext")
 
-skillenv = require("skiller.skillenv")
-require("skiller.skillhsm")
+local acl = actionlib.action_client("/roundtrip", "actionlib_benchmark/Roundtrip")
+local sent_goal = false
+local goal_handle = nil
 
-skillenv.init(SKILLSPACE)
-
---skiller.skillhsm.SkillHSM:set_debug(true)
-
-logger:log_debug("Lua startup completed")
+roslua.add_spinner(
+   function ()
+      if acl:has_server() and not sent_goal then
+	 sent_goal = true
+	 printf("Send goal")
+	 local goal = acl.actspec.goal_spec:instantiate()
+	 goal.values.start = roslua.Time.now()
+	 goal_handle = acl:send_goal(goal)
+      elseif goal_handle then
+	 if goal_handle:terminal() then
+	    printf("Goal finished")
+	 end
+      end
+   end)
