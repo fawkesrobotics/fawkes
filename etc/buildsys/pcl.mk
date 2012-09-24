8b10c79542bdb4ac6a797783ebc4ca95cb7015eb
Daniel Ewert <daniel.ewert@ima-zlw-ifu.rwth-aachen.de>
2012-09-24 11:33:45 +0200

    buildsys: Makefile and flag fixes required for Ubuntu 12.04
    Make Fawkes compile on Ubuntu 12.04 and in the presence of multiple PCL
    versions.

--- etc/buildsys/pcl.mk
+++ etc/buildsys/pcl.mk
@@ -50,7 +50,7 @@ ifneq ($(HAVE_PCL),1)
   # Give it another shot, name might contain version
   _PCL_ALTERNATE_NAME=$(shell $(PKGCONFIG) pkg-config --list-all | grep pcl_common | awk '{ print $$1 }')
   ifneq ($(_PCL_ALTERNATE_NAME),)
-    PCL_VERSION_SUFFIX=$(patsubst pcl_common%,%,$(_PCL_ALTERNATE_NAME))
+    PCL_VERSION_SUFFIX=$(patsubst pcl_common%,%,$(lastword $(_PCL_ALTERNATE_NAME)))
     HAVE_PCL=1
   endif
 endif
