0c62f729c3c2410927dd0c5f6b3e9b9fe91e6dd2
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2012-12-10 14:09:15 +0100

    clips-agent: skeleton of CLIPS-based agent framework
    Based on robotino-agent by the Carologistics RoboCup team. Generalizing
    to be applicable in multiple domains.

---
+++ src/plugins/clips-agent/clips_agent_thread.cpp
@@ -0,0 +1,212 @@
+
+/***************************************************************************
+ *  clips_agent_thread.cpp -  RobotinoClipsAgent environment providing Thread
+ *
+ *  Created: Sat Jun 16 14:40:56 2012 (Mexico City)
+ *  Copyright  2006-2012  Tim Niemueller [www.niemueller.de]
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "clips_agent_thread.h"
+
+#include <interfaces/SkillerInterface.h>
+#include <interfaces/SwitchInterface.h>
+
+using namespace fawkes;
+
+/** @class ClipsAgentThread "clips_agent_thread.h"
+ * Main thread of CLIPS-based agent.
+ *
+ * @author Tim Niemueller
+ */
+
+/** Constructor. */
+ClipsAgentThread::ClipsAgentThread()
+  : Thread("ClipsAgentThread", Thread::OPMODE_WAITFORWAKEUP),
+    BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_THINK)
+{
+}
+
+
+/** Destructor. */
+ClipsAgentThread::~ClipsAgentThread()
+{
+}
+
+
+void
+ClipsAgentThread::init()
+{
+  cfg_clips_debug_ = false;
+  cfg_skill_sim_time_ = 2.0;
+  cfg_use_sim_ = false;
+
+  try {
+    cfg_clips_debug_ = config->get_bool("/plugins/clips-agent/clips-debug");
+  } catch (Exception &e) {} // ignore, use default
+  try {
+    cfg_use_sim_ = config->get_bool("/plugins/clips-agent/use-sim");
+  } catch (Exception &e) {} // ignore, use default
+  try {
+    cfg_skill_sim_time_ =
+      config->get_float("/plugins/clips-agent/skill-sim-time");
+  } catch (Exception &e) {} // ignore, use default
+
+  cfg_clips_dir_ = std::string(SRCDIR) + "/clips/";
+
+  skiller_if_ = blackboard->open_for_reading<SkillerInterface>("Skiller");
+
+  if (! skiller_if_->has_writer()) {
+    blackboard->close(skiller_if_);
+    throw Exception("Skiller has no writer, aborting");
+    
+  } else if (skiller_if_->exclusive_controller() != 0) {
+    blackboard->close(skiller_if_);
+    throw Exception("Skiller already has a different exclusive controller");
+  }
+
+  switch_if_ = blackboard->open_for_reading<SwitchInterface>("Clips Agent Start");
+
+  clips->add_function("get-clips-dirs", sigc::slot<CLIPS::Values>(sigc::mem_fun(*this, &ClipsAgentThread::clips_get_clips_dirs)));
+  clips->add_function("now", sigc::slot<CLIPS::Values>(sigc::mem_fun( *this, &ClipsAgentThread::clips_now)));
+  clips->add_function("call-skill", sigc::slot<void, std::string, CLIPS::Values>(sigc::mem_fun( *this, &ClipsAgentThread::clips_call_skill)));
+
+  if (!clips->batch_evaluate(cfg_clips_dir_ + "init.clp")) {
+    logger->log_error(name(), "Failed to initialize CLIPS environment, "
+                      "batch file failed.");
+    blackboard->close(skiller_if_);
+    throw Exception("Failed to initialize CLIPS environment, batch file failed.");
+  }
+
+  if (cfg_clips_debug_) {
+    clips->assert_fact("(enable-debug)");
+  }
+
+  if (cfg_use_sim_) {
+    clips->assert_fact("(enable-sim)");
+  } else {
+    clips->assert_fact("(enable-skills)");
+  }
+
+  clips->refresh_agenda();
+  clips->run();
+
+  ctrl_recheck_ = true;
+
+  started_ = false;
+  skill_started_ = false;
+  skill_start_time_ = new Time(clock);
+}
+
+
+void
+ClipsAgentThread::finalize()
+{
+  delete skill_start_time_;
+
+  if (skiller_if_->has_writer()) {
+    SkillerInterface::ReleaseControlMessage *msg =
+      new SkillerInterface::ReleaseControlMessage();
+    skiller_if_->msgq_enqueue(msg);
+  }
+
+  blackboard->close(skiller_if_);
+  blackboard->close(switch_if_);
+}
+
+
+void
+ClipsAgentThread::loop()
+{
+  skiller_if_->read();
+
+  if ((skiller_if_->exclusive_controller() == 0) && skiller_if_->has_writer())
+  {
+    if (ctrl_recheck_) {
+      logger->log_info(name(), "Acquiring exclusive skiller control");
+      SkillerInterface::AcquireControlMessage *msg =
+        new SkillerInterface::AcquireControlMessage();
+      skiller_if_->msgq_enqueue(msg);
+      ctrl_recheck_ = false;
+    } else {
+      ctrl_recheck_ = true;
+    }
+    return;
+  }
+
+  if (! started_) {
+    switch_if_->read();
+    if (switch_if_->is_enabled()) {
+      clips->assert_fact("(start)");
+      started_ = true;
+    }
+  }
+
+  // might be used to trigger loop events
+  // must be cleaned up each loop from within the CLIPS code
+  //clips->assert_fact("(time (now))");
+
+
+  Time now(clock);
+  if (! active_skills_.empty()) {
+    std::list<std::string> finished_skills;
+    std::map<std::string, SkillExecInfo>::iterator as;
+    for (as = active_skills_.begin(); as != active_skills_.end(); ++as) {
+      const std::string   &as_name = as->first;
+      const SkillExecInfo &as_info = as->second;
+
+      if ((now - as_info.start_time) >= cfg_skill_sim_time_) {
+	logger->log_warn(name(), "Skill '%s' is final", as_name.c_str());
+	clips->assert_fact_f("(skill-finished (name %s) (status FINAL))", as_name.c_str());
+	finished_skills.push_back(as_name);
+      }
+    }
+
+    std::list<std::string>::iterator fs;
+    for (fs = finished_skills.begin(); fs != finished_skills.end(); ++fs) {
+      active_skills_.erase(*fs);
+    }
+  }
+
+  clips->refresh_agenda();
+  clips->run();
+}
+
+
+CLIPS::Values
+ClipsAgentThread::clips_now()
+{
+  CLIPS::Values rv;
+  fawkes::Time now(clock);
+  rv.push_back(now.get_sec());
+  rv.push_back(now.get_usec());
+  return rv;
+}
+
+
+CLIPS::Values
+ClipsAgentThread::clips_get_clips_dirs()
+{
+  CLIPS::Values rv;
+  rv.push_back(cfg_clips_dir_);
+  return rv;
+}
+
+
+void
+ClipsAgentThread::clips_call_skill(std::string skill_name, CLIPS::Values args)
+{
+  logger->log_info(name(), "Call skill %s", skill_name.c_str());
+}
---
+++ src/plugins/clips-agent/clips_agent_thread.h
@@ -0,0 +1,97 @@
+
+/***************************************************************************
+ *  clips_agent_thread.h - CLIPS-based agent plugin
+ *
+ *  Created: Sat Jun 16 14:38:21 2012 (Mexico City)
+ *  Copyright  2006-2011  Tim Niemueller [www.niemueller.de]
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_CLIPS_AGENT_CLIPS_AGENT_THREAD_H_
+#define __PLUGINS_CLIPS_AGENT_CLIPS_AGENT_THREAD_H_
+
+#include <core/threading/thread.h>
+#include <aspect/blackboard.h>
+#include <aspect/blocked_timing.h>
+#include <aspect/clock.h>
+#include <aspect/logging.h>
+#include <aspect/configurable.h>
+#include <plugins/clips/aspect/clips.h>
+#include <utils/time/time.h>
+
+#include <clipsmm.h>
+
+#include <map>
+#include <string>
+
+namespace fawkes {
+  class SkillerInterface;
+  class SwitchInterface;
+}
+
+class ClipsAgentThread
+: public fawkes::Thread,
+  public fawkes::BlockedTimingAspect,
+  public fawkes::LoggingAspect,
+  public fawkes::BlackBoardAspect,
+  public fawkes::ConfigurableAspect,
+  public fawkes::ClockAspect,
+  public fawkes::CLIPSAspect
+{
+ public:
+  ClipsAgentThread();
+  virtual ~ClipsAgentThread();
+
+  virtual void init();
+  virtual void loop();
+  virtual void finalize();
+
+ /** Stub to see name in backtrace for easier debugging. @see Thread::run() */
+ protected: virtual void run() { Thread::run(); }
+
+ private:
+  CLIPS::Values  clips_get_clips_dirs();
+  CLIPS::Values  clips_now();
+  void           clips_call_skill(std::string skill_name, CLIPS::Values args);
+
+ private:
+  std::string cfg_clips_dir_;
+  bool        cfg_clips_debug_;
+  bool        cfg_use_sim_;
+  bool        cfg_sim_randomize_;
+  float       cfg_skill_sim_time_;
+
+  fawkes::SkillerInterface *skiller_if_;
+  fawkes::SwitchInterface *switch_if_;
+
+  bool          ctrl_recheck_;
+
+  typedef struct {
+    fawkes::Time             start_time;
+    std::string              skill_string;
+    std::vector<std::string> args;
+  } SkillExecInfo;
+
+  std::map<std::string, SkillExecInfo> active_skills_;
+  bool          started_;
+  bool          skill_started_;
+  fawkes::Time *skill_start_time_;
+
+  bool          worldmodel_changed_;
+
+};
+
+#endif
