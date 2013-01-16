771a3a63517c3f434f517e026a8739fa2097f626
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2013-01-16 15:11:52 +0100

    flite: fix compile flags for Ubuntu

--- src/plugins/flite/flite.mk
+++ src/plugins/flite/flite.mk
@@ -18,7 +18,14 @@ ifneq ($(wildcard $(SYSROOT)/usr/include/flite/flite.h $(SYSROOT)/usr/local/incl
 endif
 
 ifeq ($(HAVE_FLITE),1)
+  _FLITE_NEEDS_EXTRA_LIBS=
   ifeq ($(DISTRO),arch)
+    _FLITE_NEEDS_EXTRA_LIBS=1
+  endif
+  ifeq ($(DISTRO),ubuntu)
+    _FLITE_NEEDS_EXTRA_LIBS=1
+  endif
+  ifeq ($(_FLITE_NEEDS_EXTRA_LIBS),1)
     CFLAGS_FLITE += -lflite_cmu_us_kal -lflite_usenglish -lflite_cmulex -lflite
     LDFLAGS_FLITE += -lflite_cmu_us_kal -lflite_usenglish -lflite_cmulex -lflite
   endif
