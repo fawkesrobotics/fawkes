1fe746d772d54bb8d2d42253e0e4e5a03c675eb2
Gesche Gierse <gierse@kbsg.rwth-aachen.de>
2013-02-27 12:37:57 +0100

    eclipse-clp: cleanup of unused files
    eclipseclass include guard is no longer needed (not included
    in any .h file)
    external interface interaction is now done by blackboard.ecl

--- src/plugins/eclipse-clp/eclipse_class_include.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PLUGINS_READYLOG_AGENT_ECLIPSE_CLASS_INCLUDE_H_
-#define __PLUGINS_READYLOG_AGENT_ECLIPSE_CLASS_INCLUDE_H_
-
-//since eclipseclass.h has no include guards, this file enables inclusion of eclipseclass.h in .h files
-#include <eclipseclass.h>
-
-#endif /* __PLUGINS_READYLOG_AGENT_ECLIPSE_CLASS_INCLUDE_H_ */
