028291cb13aefc9b409250c9671389ae0de15655
Andre Burghof <andre.burghof@alumni.fh-aachen.de>
2013-03-20 17:18:40 +0100

    fvmodels: luminance colormodel added
    The luminance colormodel only searches for white pixels.

---
+++ src/libs/fvmodels/color/thresholds_luminance.cpp
@@ -0,0 +1,76 @@
+
+/***************************************************************************
+ *  thresholds.cpp - Implementation of a thresholds color model
+ *
+ *  Created: Wed May 18 13:59:18 2005
+ *  Copyright  2005  Tim Niemueller  [www.niemueller.de]
+ *                   Matrin Heracles <martin.heracles@rwth-aachen.de>
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#include <iostream>
+
+#include <fvmodels/color/thresholds_luminance.h>
+
+using namespace std;
+
+namespace firevision {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+/** @class ColorModelThresholds <fvmodels/color/thresholds.h>
+ * Really simple thresholds-based model with some hard-coded thresholds. Was
+ * just for initial development of color models.
+ */
+
+ColorModelLuminance::ColorModelLuminance(const unsigned int threshold_white_low) {
+	threshold_white_low_ = threshold_white_low;
+}
+
+color_t
+ColorModelLuminance::determine(unsigned int y,
+				unsigned int u,
+				unsigned int v) const
+{
+  if ( y >= threshold_white_low_) {
+    return C_WHITE;
+  }
+  else {
+    return C_OTHER;
+  }
+}
+
+const char *
+ColorModelLuminance::get_name()
+{
+  return "ColorModelLuminance";
+}
+
+
+/** Print the thresholds to stdout.
+ */
+void
+ColorModelLuminance::print_thresholds()
+{
+  cout << "ColorModelThresholds" << endl
+       << "==========================================================" << endl
+       << "Orange:  u_low=" << THRESHOLD_WHITE_Y_LOW
+       << endl;
+}
+
+} // end namespace firevision
