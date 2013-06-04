49584c8936d7a14fc5fb6de7590c692064734d20
Bahram Maleki-Fard <maleki-fard@kbsg.rwth-aachen.de>
2013-06-04 13:31:15 +0200

    kinova: Add JacoArm class for libsub connection to Jaco arm
    This is where the libusb action takes place.

---
+++ src/plugins/kinova/kinova_api.cpp
@@ -0,0 +1,47 @@
+
+/***************************************************************************
+ *  kinova_api.cpp - Kinova API for libusb connection
+ *
+ *  Created: Tue Jun 04 13:13:20 2013
+ *  Copyright  2013  Bahram Maleki-Fard
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "kinova_api.h"
+
+#include <libusb.h>
+
+namespace fawkes {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+/** @class JacoArm <plugins/kinova/kinova_api.h>
+ * Class to control a Kinova Jaco arm
+ * @author Bahram Maleki-Fard
+ */
+
+/** Constructor. */
+JacoArm::JacoArm()
+{
+}
+
+/** Destructor. */
+JacoArm::~JacoArm()
+{
+}
+
+} // end of namespace fawkes
---
+++ src/plugins/kinova/kinova_api.h
@@ -0,0 +1,42 @@
+
+/***************************************************************************
+ *  kinova_api.h - Kinova API for libusb connection
+ *
+ *  Created: Tue Jun 04 13:13:20 2013
+ *  Copyright  2013  Bahram Maleki-Fard
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_KINOVA_KINOVA_API_H_
+#define __PLUGINS_KINOVA_KINOVA_API_H_
+
+namespace fawkes {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+class JacoArm
+{
+ public:
+  JacoArm();
+  virtual ~JacoArm();
+
+ private:
+};
+
+} // end of namespace fawkes
+
+#endif
