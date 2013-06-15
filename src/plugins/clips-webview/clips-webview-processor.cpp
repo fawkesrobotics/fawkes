410f6d8b50a7d852f4980d67c693742d88057dac
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2013-06-15 23:07:54 +0200

    clips-webview: plugin to introspect CLIPS via webview

---
+++ src/plugins/clips-webview/clips-webview-processor.cpp
@@ -0,0 +1,109 @@
+
+/***************************************************************************
+ *  clips-webview-processor.cpp - CLIPS introspection via webview
+ *
+ *  Created: Sat Jun 15 20:17:53 2013
+ *  Copyright  2006-2013  Tim Niemueller [www.niemueller.de]
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "clips-webview-processor.h"
+
+#include <core/exception.h>
+#include <core/threading/mutex_locker.h>
+#include <webview/page_reply.h>
+#include <webview/file_reply.h>
+#include <webview/error_reply.h>
+
+#include <cstring>
+
+#include <clipsmm.h>
+#include <clips/clips.h>
+
+using namespace fawkes;
+
+/** @class ClipsWebRequestProcessor "rrdweb_processor.h"
+ * Clips web request processor.
+ * Process web requests to the rrd URL space.
+ * @author Tim Niemueller
+ */
+
+/** Constructor.
+ * @param clips CLIPS Environment to query for facts
+ * @param logger logger to report problems
+ * @param baseurl base URL of the Clips webrequest processor
+ */
+ClipsWebRequestProcessor::ClipsWebRequestProcessor(fawkes::LockPtr<CLIPS::Environment> &clips,
+						   fawkes::Logger *logger, const char *baseurl)
+  : WebRequestProcessor(/* handle session data */ false)
+{
+  clips_  = clips;
+  logger_ = logger;
+
+  baseurl_     = baseurl;
+  baseurl_len_ = strlen(baseurl);
+}
+
+
+/** Destructor. */
+ClipsWebRequestProcessor::~ClipsWebRequestProcessor()
+{
+}
+
+WebReply *
+ClipsWebRequestProcessor::process_request(const char *url,
+					  const char *method,
+					  const char *version,
+					  const char *upload_data,
+					  size_t *upload_data_size,
+					  void **session_data)
+{
+  if ( strncmp(baseurl_, url, baseurl_len_) == 0 ) {
+    // It is in our URL prefix range
+    std::string subpath = std::string(url).substr(baseurl_len_);
+
+    WebPageReply *r = new WebPageReply("Clips Graphs");
+    *r += "<h2>CLIPS Facts</h2>\n";
+
+    MutexLocker lock(clips_.objmutex_ptr());
+
+    *r += "<table>";
+    *r += "<tr><th>Index</th><th>Fact</th></tr>\n";
+
+    CLIPS::Fact::pointer fact = clips_->get_facts();
+    while (fact) {
+      CLIPS::Template::pointer tmpl = fact->get_template();
+
+      char tmp[16384];
+      OpenStringDestination(clips_->cobj(), (char *)"ProcPPForm", tmp, 16383);
+      PrintFact(clips_->cobj(), (char *)"ProcPPForm",
+		(struct fact *)fact->cobj(), FALSE, FALSE);
+      CloseStringDestination(clips_->cobj(), (char *)"ProcPPForm");
+
+      r->append_body("<tr><td>f-%li</td><td>%s</td></tr>\n",
+		     fact->index(), tmp);
+
+      fact = fact->next();
+    }
+    
+
+    *r += "</table>";
+
+    return r;
+  } else {
+    return NULL;
+  }
+}
---
+++ src/plugins/clips-webview/clips-webview-processor.h
@@ -0,0 +1,60 @@
+
+/***************************************************************************
+ *  clips-webview-processor.h - CLIPS introspection via webview
+ *
+ *  Created: Sat Jun 15 20:15:18 2013
+ *  Copyright  2006-2013  Tim Niemueller [www.niemueller.de]
+ *
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#ifndef __PLUGINS_CLIPS_WEBVIEW_CLIPS_WEBVIEW_PROCESSOR_H_
+#define __PLUGINS_CLIPS_WEBVIEW_CLIPS_WEBVIEW_PROCESSOR_H_
+
+#include <core/utils/lockptr.h>
+#include <webview/request_processor.h>
+
+namespace fawkes {
+  class Logger;
+}
+
+namespace CLIPS {
+  class Environment;
+}
+
+class ClipsWebRequestProcessor : public fawkes::WebRequestProcessor
+{
+ public:
+  ClipsWebRequestProcessor(fawkes::LockPtr<CLIPS::Environment> &clips, fawkes::Logger *logger,
+			   const char *baseurl);
+
+  virtual ~ClipsWebRequestProcessor();
+
+  virtual fawkes::WebReply * process_request(const char *url,
+					     const char *method,
+					     const char *version,
+					     const char *upload_data,
+					     size_t *upload_data_size,
+					     void **session_data);
+
+ private:
+  fawkes::LockPtr<CLIPS::Environment> clips_;
+  fawkes::Logger       *logger_;
+
+  const char           *baseurl_;
+  size_t                baseurl_len_;
+};
+
+#endif
