c3aad2b5c90470cab123b8dafb2c94c2e1771e2b
Bahram Maleki-Fard <maleki-fard@kbsg.rwth-aachen.de>
2013-10-17 00:11:04 +0200

    colli: portet Laser class from RCSoftX
    This one was a bigger package, as it depended on the previous stuff.
    
    It contained some hostname-depended config-entry-fetching. I removed that,
    as we can/should just provide a different YAML file in those cases.

---
+++ src/plugins/colli/utils/rob/robo_laser.cpp
@@ -0,0 +1,317 @@
+/*
+ ©º°¨¨°º©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©º°¨¨°º©
+ ©                                                                            ©
+ ©                                            ####   ####           .-""-.    ©
+ ©       # #                             #   #    # #    #         /[] _ _\   ©
+ ©       # #                                 #    # #             _|_o_LII|_  ©
+ © ,###, # #  ### ## ## ##   ###  ## ##  #   #    # #       ###  / | ==== | \ ©
+ © #   # # # #   # ## ## #  #   #  ## #  #   ###### #      #     |_| ==== |_| ©
+ © #   # # # ####  #  #  #  #   #  #  #  #   #    # #      ####   ||" ||  ||  ©
+ © #   # # # #     #  #  #  #   #  #  #  #   #    # #    #    #   ||LI  o ||  ©
+ © '###'# # # #### #  #  ##  ### # #  ## ## #      # ####  ###    ||'----'||  ©
+ ©                                                               /__|    |__\ ©
+ ©                                                                            ©
+ ©º°¨¨°º©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©º°¨¨°º©
+*/
+
+/* Written by Stefan Jacobs
+ * for module Colli-A*
+ *
+ * Containing Implementation for laser interface.
+ *
+ */
+
+/***********************************************************************
+ *
+ * $Id$
+ *
+ * description:
+ *
+ * last modification: $Date$
+ *         by author: $Author$
+ *
+ **********************************************************************/
+
+/*! \file Laser.cpp
+<pre>
+<b>File:</b>          Laser.cpp
+<b>Project:</b>       Collision Avoidance, Gruppe 1
+<b>Authors:</b>       Stefan Jacobs <Stefan_J@gmx.de>, Gruppe 1
+<b>Created:</b>       03/06/2002
+<b>Last Revision:</b> 10/06/2002
+<b>Contents:</b>      Laser Access.
+                       Gets pure Laserdata.
+</pre>
+*/
+
+#include "robo_laser.h"
+
+#include <interfaces/Laser360Interface.h>
+#include <logging/logger.h>
+#include <config/config.h>
+
+#include <utils/time/time.h>
+#include <utils/math/angle.h>
+
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+
+
+using namespace std;
+
+namespace fawkes
+{
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+const float Laser::LASER_X_OFFSET = 0.21;
+
+// Initialize the Laser by giving the Laser_Client to it, and
+// calculating the number of readings we get ( the size of the pos array )
+// and initialize it directly
+Laser::Laser( Laser360Interface* laser,
+              Logger* logger,
+              Configuration* config) throw (int)
+{
+  newtime = new Time();
+  newtime->stamp();
+  oldtime = new Time();
+  m_pLaserScannerObj = laser;
+
+  m_NumberOfReadings = m_pLaserScannerObj->maxlenof_distances();
+
+  if ( m_NumberOfReadings < 1 ) {
+    logger->log_error("Laser", "EXCEPTION in Laser(Constructor):  Found less than 1 readings! Throwing exception 1");
+    throw ( 1 );
+    return;
+  }
+
+  try {
+      m_pReadings = new LaserPoint( m_NumberOfReadings );
+  } catch(...)  {
+    throw ( 2 );
+    return;
+  }
+
+  string cfg_path = "/plugins/colli/laser_calibration/";
+
+  if( config->exists(cfg_path.c_str()) ) {
+    // query config file for ignore-readings
+    m_IgnoreFRStart = config->get_int((cfg_path + "IgnoreFRStart").c_str());
+    m_IgnoreFREnd = config->get_int((cfg_path + "IgnoreFREnd").c_str());
+    m_IgnoreRRStart = config->get_int((cfg_path + "IgnoreRRStart").c_str());
+    m_IgnoreRREnd = config->get_int((cfg_path + "IgnoreRREnd").c_str());
+    m_IgnoreRLStart = config->get_int((cfg_path + "IgnoreRLStart").c_str());
+    m_IgnoreRLEnd = config->get_int((cfg_path + "IgnoreRLEnd").c_str());
+    m_IgnoreFLStart = config->get_int((cfg_path + "IgnoreFLStart").c_str());
+    m_IgnoreFLEnd = config->get_int((cfg_path + "IgnoreFLEnd").c_str());
+
+    // calculate ignore reagions to floats from degree readings
+    m_fIgnoreFRStart = deg2rad(m_IgnoreFRStart);
+    m_fIgnoreFREnd   = deg2rad(m_IgnoreFREnd);
+
+    m_fIgnoreRRStart = deg2rad(m_IgnoreRRStart);
+    m_fIgnoreRREnd   = deg2rad(m_IgnoreRREnd);
+
+    m_fIgnoreRLStart = deg2rad(m_IgnoreRLStart);
+    m_fIgnoreRLEnd   = deg2rad(m_IgnoreRLEnd);
+
+    m_fIgnoreFLStart = deg2rad(m_IgnoreFLStart);
+    m_fIgnoreFLEnd   = deg2rad(m_IgnoreFLEnd);
+
+  } else {
+    logger->log_warn("Laser", "Config path '%s' not existing, using default ignore-readings calibration", cfg_path.c_str());
+
+    // Config non valid!
+    m_fIgnoreFRStart = -1;
+    m_fIgnoreFREnd   = -1;
+
+    m_fIgnoreRRStart = -1;
+    m_fIgnoreRREnd   = -1;
+
+    m_fIgnoreRLStart = -1;
+    m_fIgnoreRLEnd   = -1;
+
+    m_fIgnoreFLStart = -1;
+    m_fIgnoreFLEnd   = -1;
+  }
+}
+
+
+// Destruct objects is killing all memory allocation
+Laser::~Laser( )
+{
+  delete this->newtime;
+  delete this->oldtime;
+  delete this->m_pReadings;
+}
+
+
+// Update the laserdata. Call this with the Laser_Clientect in your Loop.
+// -1 is no new data, so nothing is to do; 0 is ok; 1 is error
+int
+Laser::UpdateLaser( )
+{
+  oldtime->set_time( newtime );
+
+  newtime->stamp();
+
+  m_pLaserScannerObj->read();
+  m_NumberOfReadings = m_pLaserScannerObj->maxlenof_distances();
+
+  // get all readings
+  CalculateReadings();
+  CalculatePositions();
+
+  return 0;
+}
+
+
+// put all readings in scan object
+void
+Laser::CalculateReadings()
+{
+  float rad = 0.f;
+  float rad_inc = (2*M_PI) / m_NumberOfReadings;
+  for (int i = 0; i < m_NumberOfReadings; i++) {
+    m_pReadings->SetRadians(i, rad);
+    m_pReadings->SetLength(i, max(m_pLaserScannerObj->distances(i) - 0.02, 0.0 ) );
+    rad += rad_inc;
+  }
+}
+
+void
+Laser::CalculatePositions()
+{
+  for ( int i = 0; i < m_NumberOfReadings; i++ )
+    m_pReadings->SetPos(i);
+}
+
+/* =========================================================================================== */
+/* GETTER STUFF */
+/* =========================================================================================== */
+
+
+// -------------------------- //
+// LASERREADINGS GETTER       //
+// -------------------------- //
+
+
+// returns laser reading length
+float
+Laser::GetReadingLength( const int number ) const
+{
+  return m_pReadings->GetLength( number );
+}
+
+
+// returns laser reading its positions x coordinate
+float
+Laser::GetReadingPosX( const int number ) const
+{
+  return m_pReadings->GetPosX(number);
+}
+
+
+// returns laser reading its positions y coordinate
+float
+Laser::GetReadingPosY( const int number ) const
+{
+  return m_pReadings->GetPosY(number);
+}
+
+
+// -------------------------- //
+// MISC GETTER                //
+// -------------------------- //
+
+
+// returns number of readings available
+int
+Laser::GetNumberOfReadings() const
+{
+  return m_NumberOfReadings;
+}
+
+
+// returns current laserdata timestamp
+Time
+Laser::GetCurrentTimestamp() const
+{
+  return m_pLaserScannerObj->timestamp();
+}
+
+
+// returns angle for reading
+float
+Laser::GetRadiansForReading( const int number ) const
+{
+  return m_pReadings->GetRadians(number);
+}
+
+
+float
+Laser::TimeDiff() const
+{
+  return ((*newtime - *oldtime).in_sec());
+}
+
+
+// the famous is pipe method
+bool
+Laser::IsPipe( float i ) const
+{
+  i = normalize_rad( i );
+
+  int reading_num = (int)( ( (i+0.001) / (2.0*M_PI)) * m_NumberOfReadings );
+  // look below for explanation
+
+  if ( GetReadingLength( reading_num ) == 0.0 )
+    return true;
+
+  if( ( (i >= m_fIgnoreFRStart) && (i <= m_fIgnoreFREnd) )
+   || ( (i >= m_fIgnoreRRStart) && (i <= m_fIgnoreRREnd) )
+   || ( (i >= m_fIgnoreRLStart) && (i <= m_fIgnoreRLEnd) )
+   || ( (i >= m_fIgnoreFLStart) && (i <= m_fIgnoreFLEnd) ) ) {
+    // Meist sind die Kabel das Problem!!!!
+    // Wenn Danger if turning im Colli erscheint, erst Kabel checken.
+    //      cout << "Got pipe at " << i << endl;
+    return true;
+  }
+
+  return false;
+}
+
+
+bool
+Laser::IsOnlyPipe( float i ) const
+{
+  i = normalize_rad( i );
+
+  //  int reading_num = (int)( ( (i+0.001) / (2.0*M_PI)) * m_NumberOfReadings );
+  // -0.01 because of numerical problems...
+  // numbers were before something like 1 2 3 5 6 6 7
+  //    cout << i << " has reading num " << reading_num << " and length : " << GetReadingLength( reading_num ) << endl;
+
+//   if ( ( (i > (26.0*M_PI)/180.0) && (i < (37.0*M_PI)/180.0) )
+//        || ( (i > (95.0*M_PI)/180.0) && (i < (115.0*M_PI)/180.0) )
+//        || ( (i > ((246.0-360.0)*M_PI)/180.0) && (i < ((263.0-360.0)*M_PI)/180.0) )
+//        || ( (i > ((323.0-360.0)*M_PI)/180.0) && (i < ((334.0-360.0)*M_PI)/180.0) )
+//       )
+
+  if( ( (i > m_fIgnoreFRStart) && (i < m_fIgnoreFREnd) )
+   || ( (i > m_fIgnoreRRStart) && (i < m_fIgnoreRREnd) )
+   || ( (i > m_fIgnoreRLStart) && (i < m_fIgnoreRLEnd) )
+   || ( (i > m_fIgnoreFLStart) && (i < m_fIgnoreFLEnd ) ) ) {
+    // Meist sind die Kabel das Problem!!!!
+    // Wenn Danger if turning im Colli erscheint, erst Kabel checken.
+    //      cout << "Got pipe at " << i << endl;
+    return true;
+  }
+
+  return false;
+}
+
+} // namespace fawkes
---
+++ src/plugins/colli/utils/rob/robo_laser.h
@@ -0,0 +1,218 @@
+/*
+ ©º°¨¨°º©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©º°¨¨°º©
+ ©                                                                            ©
+ ©                                            ####   ####           .-""-.    ©
+ ©       # #                             #   #    # #    #         /[] _ _\   ©
+ ©       # #                                 #    # #             _|_o_LII|_  ©
+ © ,###, # #  ### ## ## ##   ###  ## ##  #   #    # #       ###  / | ==== | \ ©
+ © #   # # # #   # ## ## #  #   #  ## #  #   ###### #      #     |_| ==== |_| ©
+ © #   # # # ####  #  #  #  #   #  #  #  #   #    # #      ####   ||" ||  ||  ©
+ © #   # # # #     #  #  #  #   #  #  #  #   #    # #    #    #   ||LI  o ||  ©
+ © '###'# # # #### #  #  ##  ### # #  ## ## #      # ####  ###    ||'----'||  ©
+ ©                                                               /__|    |__\ ©
+ ©                                                                            ©
+ ©º°¨¨°º©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©©º°¨¨°º©º°¨¨°º©
+*/
+
+/* Written by Stefan Jacobs
+ * for module Colli-A*
+ *
+ * Containing Header for laser interface.
+ *
+ */
+
+/***********************************************************************
+ *
+ * $Id$
+ *
+ * description:
+ *
+ * last modification: $Date$
+ *         by author: $Author$
+ *
+ **********************************************************************/
+// This is Gruppe 1s Laser Object
+// Written by Stefan Jacobs @ 3.6.2002
+// Last modification 10.6.2002.
+
+/*! \file Laser.h
+<pre>
+<b>File:</b>          Laser.h
+<b>Project:</b>       Collision Avoidance, Gruppe 1
+<b>Authors:</b>       Stefan Jacobs <Stefan_J@gmx.de>, Gruppe 1
+<b>Created:</b>       03/06/2002
+<b>Last Revision:</b> 10/06/2002
+<b>Contents:</b>      Laser.
+                       Provides access to the pure laser
+             data.
+</pre>
+*/
+
+#ifndef _COLLI_UTILS_ROB_ROBO_LASER_H_
+#define _COLLI_UTILS_ROB_ROBO_LASER_H_
+
+#include "robo_laserpoint.h"
+
+namespace fawkes
+{
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+class Laser360Interface;
+class Logger;
+class Configuration;
+class Time;
+
+/** My laser class.
+ *  This is a class to access the laserreadings in a
+ *   fast and efficient way.
+ */
+class Laser
+{
+ public:
+
+  // =============================== //
+  // CLASS METHODS                   //
+  // =============================== //
+
+  /** Constructor.
+   *  This is the constructor. Has to be called with the laser
+   *   object.
+   *  \exception (int 1) The number of readings given by the laser are
+   *             smaller or equal 0.... Perhaps the laser is currently
+   *             offline. Try again!
+   *  \exception (int 2) The readings array could not be allocated!
+   *  @param laser is the bbClients Laser_Client.
+   */
+  Laser( fawkes::Laser360Interface* laser,
+         fawkes::Logger* logger,
+         fawkes::Configuration* config) throw (int);
+
+
+  /** Destructor.
+   */
+  ~Laser();
+
+
+  // =============================== //
+  // CONTINOUS METHOD                //
+  // =============================== //
+
+  /** Updates the laserdata.
+   *  Call this with the Laser_Clientect in your
+   *   Loop (the laserobject has to be updated
+   *  previously, or you get no new data!).
+   *  @return  -1 is no new data, so nothing is to do;
+   *            0 is ok;
+   *            1 is an error occured;
+   */
+  int UpdateLaser( );
+
+
+  // ================================================= //
+  // Return the actual readings. Nothing interpolated. //
+  // ================================================= //
+
+  /** Returns a special readings length.
+   *  @param number is the number of this reading.
+   *  @return float is the numbers length.
+   */
+  float GetReadingLength( const int number ) const;
+
+  /** Returns a special readings x coordinate.
+   *  @param number is the number of this reading.
+   *  @return float is the numbers x coordinate.
+   */
+  float GetReadingPosX  ( const int number ) const;
+
+  /** Returns a special readings y coordinate.
+   *  @param number is the number of this reading.
+   *  @return float is the numbers y coordinate.
+   */
+  float GetReadingPosY  ( const int number ) const;
+
+
+  bool IsPipe( float i ) const;
+  bool IsOnlyPipe( float i ) const;
+
+  // ================================================= //
+  // RETURN Misc Things                                //
+  // ================================================= //
+
+  // Return the number of readings we got.
+  int GetNumberOfReadings() const;
+
+  // Return the angle in radians for this reading
+  float GetRadiansForReading( const int number ) const;
+
+  // Returns the date of the current laserdata.
+  Time GetCurrentTimestamp() const;
+
+  float TimeDiff() const;
+  // ======================================================= //
+
+  /** Offset of the laser X coordinate behind the motor axle */
+  static const float LASER_X_OFFSET;
+
+ protected:
+
+  // the number of readings
+  int m_NumberOfReadings;
+  float m_Resolution;
+
+  // our readings
+  LaserPoint * m_pReadings;
+
+ private:
+
+  // METHODS, you don't have to care about
+
+  void CalculateReadings();
+  void CalculatePositions();
+
+  // VARIABLES
+
+  // the laser
+  fawkes::Laser360Interface *m_pLaserScannerObj;
+  Time * newtime;
+  Time * oldtime;
+
+
+  // are the pipes valid???
+  bool m_bValidConfig;
+
+
+  // ignore following readings
+  int m_IgnoreFRStart;
+  int m_IgnoreFREnd;
+
+  int m_IgnoreRRStart;
+  int m_IgnoreRREnd;
+
+  int m_IgnoreRLStart;
+  int m_IgnoreRLEnd;
+
+  int m_IgnoreFLStart;
+  int m_IgnoreFLEnd;
+
+  // ignore following float readings
+  float m_fIgnoreFRStart;
+  float m_fIgnoreFREnd;
+
+  float m_fIgnoreRRStart;
+  float m_fIgnoreRREnd;
+
+  float m_fIgnoreRLStart;
+  float m_fIgnoreRLEnd;
+
+  float m_fIgnoreFLStart;
+  float m_fIgnoreFLEnd;
+
+
+};
+
+} // namespace fawkes
+
+#endif
+
