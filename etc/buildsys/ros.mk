a43d550c7bf72e3124574886a9169d13b3ffbce2
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2014-05-06 18:23:39 +0200

    buildsys: get flags for multiple ROS packages at once
    Add functions to check for and get the flags of multiple ROS packages at
    the same time. This makes Makefiles better readable for multiple
    dependencies.

--- etc/buildsys/ros.mk
+++ etc/buildsys/ros.mk
@@ -25,10 +25,15 @@ ros-pkg-path = $(shell rospack find $(1))
 
 ros-pkg-msg-cflags = -I$(call ros-pkg-path,$(1))/msg_gen/cpp/include \
 		     -I$(call ros-pkg-path,$(1))/srv_gen/cpp/include
-ros-pkg-cflags = $(call ros-pkg-msg-cflags,$(1)) \
+ros-pkg-cflags   = $(call ros-pkg-msg-cflags,$(1)) \
 	$(shell rospack export --lang=cpp --attrib=cflags $(1) 2>/dev/null)
-ros-pkg-lflags = $(shell rospack export --lang=cpp --attrib=lflags $(1) 2>/dev/null)
-ros-have-pkg = $(if $(shell rospack find $(1) 2>&1 >/dev/null; echo $${?/0/}),0,1)
+ros-pkg-lflags   = $(shell rospack export --lang=cpp --attrib=lflags $(1) 2>/dev/null)
+ros-have-pkg     = $(if $(shell rospack find $(1) 2>&1 >/dev/null; echo $${?/0/}),0,1)
+
+ros-have-pkgs    = $(if $(strip $(subst 1,,$(foreach p,$1,$(call ros-have-pkg,$p)))),0,1)
+ros-pkgs-cflags  = $(foreach p,$1,$(call ros-pkg-cflags,$p) )
+ros-pkgs-lflags  = $(foreach p,$1,$(call ros-pkg-lflags,$p) )
+ros-missing-pkgs = $(strip $(foreach p,$1,$(if $(subst 1,,$(call ros-have-pkg,$p)),$p )))
 
 HAVE_ROSPACK := $(if $(shell type -p rospack; echo $${?/1/}),1,0)
 
