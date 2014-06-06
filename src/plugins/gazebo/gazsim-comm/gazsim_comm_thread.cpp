7ed6b6586acfd476fca60832770a3d526ba58705
Frederik Zwilling <zwilling@kbsg.rwth-aachen.de>
2014-06-06 18:54:10 +0200

    gazsim-comm: sending and receiving raw msgs
    This enables forwarding any msgs, for example encrypted msgs in the LLSF.

--- src/plugins/gazebo/gazsim-comm/gazsim_comm_thread.cpp
+++ src/plugins/gazebo/gazsim-comm/gazsim_comm_thread.cpp
@@ -27,6 +27,7 @@
 #include <protobuf_comm/message_register.h>
 #include <stdlib.h>
 #include "gazsim_comm_thread.h"
+#include <algorithm>
 
 using namespace fawkes;
 using namespace protobuf_comm;
@@ -54,15 +55,22 @@ GazsimCommThread::init()
   initialized_ = false;
 
   //read config values
-  // address_ = config->get_string("/gazsim/comm/address");
-  // send_port_ = config->get_uint("/gazsim/comm/send-port");
-  // recv_port_ = config->get_uint("/gazsim/comm/recv-port");
   proto_dirs_ = config->get_strings("/gazsim/proto-dirs");
   package_loss_ = config->get_float("/gazsim/comm/package-loss");
   addresses_ = config->get_strings("/gazsim/comm/addresses");
   send_ports_ = config->get_uints("/gazsim/comm/send-ports");
   recv_ports_ = config->get_uints("/gazsim/comm/recv-ports");
-  if(addresses_.size() != send_ports_.size() || addresses_.size() != recv_ports_.size())
+  use_crypto1_ = config->get_bool("/gazsim/comm/use-crypto1");
+  use_crypto2_ = config->get_bool("/gazsim/comm/use-crypto1");
+  send_ports_crypto1_ = config->get_uints("/gazsim/comm/send-ports-crypto1");
+  recv_ports_crypto1_ = config->get_uints("/gazsim/comm/recv-ports-crypto1");
+  send_ports_crypto2_ = config->get_uints("/gazsim/comm/send-ports-crypto2");
+  recv_ports_crypto2_ = config->get_uints("/gazsim/comm/recv-ports-crypto2");
+  if(addresses_.size() != send_ports_.size() || addresses_.size() != recv_ports_.size()
+     || ( use_crypto1_ && addresses_.size() != send_ports_crypto1_.size())
+     || ( use_crypto1_ && addresses_.size() != recv_ports_crypto1_.size())
+     || ( use_crypto2_ && addresses_.size() != send_ports_crypto2_.size())
+     || ( use_crypto2_ && addresses_.size() != recv_ports_crypto2_.size()))
   {
     logger->log_warn(name(), "/gazsim/comm/ has an invalid configuration!");
   }
@@ -97,11 +105,25 @@ GazsimCommThread::init()
 
   //create peer connections 
   peers_.resize(addresses_.size());
+  peers_crypto1_.resize(addresses_.size());
+  peers_crypto2_.resize(addresses_.size());
   for(unsigned int i = 0; i < addresses_.size(); i++)
   {
     peers_[i] = new ProtobufBroadcastPeer(addresses_[i], send_ports_[i],
 					  recv_ports_[i], proto_dirs_);
-    peers_[i]->signal_received().connect(boost::bind(&GazsimCommThread::receive_msg, this, _1, _2, _3, _4));
+    peers_[i]->signal_received_raw().connect(boost::bind(&GazsimCommThread::receive_raw_msg, this, _1, _2, _3, _4));
+    if(use_crypto1_)
+    {
+      peers_crypto1_[i] = new ProtobufBroadcastPeer(addresses_[i], send_ports_crypto1_[i],
+					  recv_ports_crypto1_[i], proto_dirs_);
+      peers_crypto1_[i]->signal_received_raw().connect(boost::bind(&GazsimCommThread::receive_raw_msg, this, _1, _2, _3, _4));
+    }
+    if(use_crypto2_)
+    {
+      peers_crypto2_[i] = new ProtobufBroadcastPeer(addresses_[i], send_ports_crypto2_[i],
+						    recv_ports_crypto2_[i], proto_dirs_);
+      peers_crypto2_[i]->signal_received_raw().connect(boost::bind(&GazsimCommThread::receive_raw_msg, this, _1, _2, _3, _4));
+    }
   }
   initialized_ = true;
 }
@@ -154,6 +176,62 @@ GazsimCommThread::receive_msg(boost::asio::ip::udp::endpoint &endpoint,
     if(send_ports_[i] != incoming_peer_port)
     {
       peers_[i]->send(msg);
+    }
+  }
+}
+
+/**
+ * Receive and forward raw msg
+ * @param endpoint port msg received from
+ * @param header header of the msg
+ * @param data data stream
+ * @param length length of the data stream
+ */
+void
+GazsimCommThread::receive_raw_msg(boost::asio::ip::udp::endpoint &endpoint,
+				  protobuf_comm::frame_header_t &header, void * data,
+				  size_t length)
+{
+  //logger->log_info(name(), "Got raw Message from port %d", endpoint.port());
+  unsigned int incoming_peer_port = endpoint.port(); //this is suprisingly the send-port
+ 
+  if(!initialized_)
+  {
+    return;
+  }
+
+  //simulate package loss
+  double rnd = ((double) rand()) / ((double) RAND_MAX); //0.0 <= rnd <= 1.0
+  if(rnd < package_loss_)
+  {
+    return;
+  }
+
+  //check which set of peers the message comes from
+  std::vector<protobuf_comm::ProtobufBroadcastPeer*> peers;
+  std::vector<unsigned int> send_ports;
+  if(std::find(send_ports_.begin(), send_ports_.end(), incoming_peer_port) != send_ports_.end())
+  {
+    peers = peers_;
+    send_ports = send_ports_;
+  }
+  else if(use_crypto1_ && std::find(send_ports_crypto1_.begin(), send_ports_crypto1_.end(), incoming_peer_port) != send_ports_crypto1_.end())
+  {
+    peers = peers_crypto1_;
+    send_ports = send_ports_crypto1_;
+  }  
+  else if(use_crypto2_ && std::find(send_ports_crypto2_.begin(), send_ports_crypto2_.end(), incoming_peer_port) != send_ports_crypto1_.end())
+  {
+    peers = peers_crypto2_;
+    send_ports = send_ports_crypto2_;
+  }
+
+  //send message to all other peers
+  for(unsigned int i = 0; i < peers.size(); i++)
+  {
+    if(send_ports[i] != incoming_peer_port)
+    {
+      peers[i]->send_raw(header, data, length);
     }
   }
 }
