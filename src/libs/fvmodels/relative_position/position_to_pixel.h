20665307e53efb10c29e15e27a53437e84e859d3
Victor Mataré <matare@lih.rwth-aachen.de>
2014-07-07 20:55:44 +0200

    fvmodels:PositionToPixel: remove empty destructor

--- src/libs/fvmodels/relative_position/position_to_pixel.h
+++ src/libs/fvmodels/relative_position/position_to_pixel.h
@@ -36,8 +36,6 @@ public:
   PositionToPixel(  fawkes::tf::Transformer* tf, std::string cam_frame,
                   float cam_aperture_x, float cam_aperture_y,
                   unsigned int cam_width_x, unsigned int cam_height_y, float cam_angle_y = 0 );
-  virtual
-  ~PositionToPixel();
 
   fawkes::upoint_t get_pixel_position(fawkes::cart_coord_3d_t& position, std::string& frame, const fawkes::Time& time);
 
