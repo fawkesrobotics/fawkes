e8b9a7f0eb478c66ea2f77e8477922798ebf1dec
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2014-08-26 13:20:02 +0200

    utils: split string to list, add dual join function

--- src/libs/utils/misc/string_split.h
+++ src/libs/utils/misc/string_split.h
@@ -54,6 +54,58 @@ std::vector<std::string> str_split(const std::string &s, char delim = '/')
  * @return queue of split strings
  */
 static inline
+std::list<std::string> str_split_list(const std::string &s, char delim = '/')
+{
+  std::list<std::string> elems;
+  std::stringstream ss(s);
+  std::string item;
+  while(std::getline(ss, item, delim)) {
+    if (item != "")  elems.push_back(item);
+  }
+  return elems;
+}
+
+
+/** Join vector of strings string using given delimiter.
+ * @param v vector with strings to join
+ * @param delim delimiter
+ * @return string of strings in vector separated by given delimiter
+ */
+static inline
+std::string str_join(const std::vector<std::string> &v, char delim = '/')
+{
+  std::string rv;
+  for (size_t i = 0; i < v.size(); ++i) {
+    if (i > 0) rv += delim;
+    rv += v[i];
+  }
+  return rv;
+}
+
+/** Join list of strings string using given delimiter.
+ * @param l list with strings to join
+ * @param delim delimiter
+ * @return string of strings in list separated by given delimiter
+ */
+static inline
+std::string str_join(const std::list<std::string> &l, char delim = '/')
+{
+  std::string rv;
+  bool first = true;
+  for (std::list<std::string>::const_iterator i = l.begin(); i != l.end(); ++i) {
+    if (first)  first = false;
+    else        rv += delim;
+    rv += *i;
+  }
+  return rv;
+}
+
+/** Split string by delimiter.
+ * @param s string to split
+ * @param delim delimiter
+ * @return queue of split strings
+ */
+static inline
 std::queue<std::string> str_split_to_queue(const std::string &s, char delim = '/')
 {
   std::queue<std::string> elems;
