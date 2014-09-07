84234c8fca71707e3b72ac76ac9a5f5a2b449802
Bahram Maleki-Fard <maleki-fard@kbsg.rwth-aachen.de>
2014-09-07 23:35:04 +0200

    skill or_object: simplify arguments, allow tables wherever applicable
    The skill accepts now tables as arguments, so we can add/delete/move/etc
    multiple objects with one call of the skill. For example
     or_object{add={{name="c1", path=""}, {name="c2", path=""}}}
    or
     or_object{delete={"cup1", "cup2"}}
    See skill documentation for further information.

--- src/lua/skills/generic/openrave/or_object.lua
+++ src/lua/skills/generic/openrave/or_object.lua
@@ -3,7 +3,7 @@
 --  or_object.lua - object handling in openrave
 --
 --  Created: Thu Mar 03 14:32:47 2011
---  Copyright  2011  Bahram Maleki-Fard
+--  Copyright  2011-2014  Bahram Maleki-Fard
 --
 ----------------------------------------------------------------------------
 
@@ -37,49 +37,63 @@ It basically allows all the object manipulation posiibilities provided in
 *plugins/openrave/environment.h" (i.e. messages from OpenRAVEInterface)
 to be called from the skill level.
 
+We have some modes that apply to all objects:
 
-Arguments needed for most calls:
- NAME: name of the object (string)
+  or_object{delete_all=true}
+   Deletes all objects from environment (except robot parts)
 
-Possible call modes:
+  or_object{release_all=true}
+   Releases all object from the currently active robot
 
-or_object{add=true,  name=NAME, path=PATH}
- Adds an object to the environment.
- NAME: name to be given to object (string)
- PATH: path to xml file defining the object (string)
 
-or_object{delete=true,  name=NAME}
- Deletes object from environment
+The other modes handle individual objects, but we can pass multiple
+objects in a table. Some only require the object name, so we pass
+all the names in one table:
 
-or_object{delete_all=true}
- Deletes all objects from environment (except robot parts)
+  or_object{delete={"obj1", "obj2", ...}}
+   Deletes objects from environment.
+   Each obj_ is a string: name of the object to be deleted.
 
-or_object{attach=true,  name=NAME}
- Attaches object to the currently active robot
+  or_object{attach={"obj1", "obj2", ...}}
+   Attaches object to the currently active robot
+   Each obj_ is a string: name of the object to be attached.
 
-or_object{release=true,  name=NAME}
- Releases object from the currently active robot
+  or_object{release={"obj1", "obj2", ...}}
+   Releases object from the currently active robot
+   Each obj_ is a string: name of the object to be released.
 
-or_object{release_all=true}
- Releases all object from the currently active robot
+Others require more arguments, so we pass tables for each object:
 
-or_object{move=true,  name=NAME, x=X, y=Y, z=Z}
- Moves object to a global position in the environment. Need to consider, that
- coordinates define the center-point of the object.
- X: x-coordinate (float)
- Y: y-coordinate (float)
- Z: z-coordinate (float)
+  or_object{add={obj1, obj2, ...}}
+   Add objects to the environment.
+   Each obj_ is a table: {name="NAME", path="PATH"}
+    NAME: name to be given to object (string)
+    PATH: path to xml file defining the object (string)
 
-or_object{rotate=true,  name=NAME, x=X, y=Y, z=Z}
- Rotates object around its center point. Rotation values are absolute, not relative
- to current rotation.
- X: 1st rotation,  around x-axis (float)
- Y: 2nd rotation,  around y-axis (float)
- Z: 3rd rotation,  around z-axis (float)
+  or_object{move={obj1, obj2, ...}}
+   Moves object to a global position in the environment. Need to consider, that
+   coordinates define the center-point of the object.
+   Each obj_ is a table: {name="NAME", x=X, y=Y, z=Z}
+    NAME: name of the object (string)
+    X: x-coordinate (float)
+    Y: y-coordinate (float)
+    Z: z-coordinate (float)
 
-or_object{rename=true,  name=NAME, new_name=NEW_NAME}
- Renames an object.
- NEW_NAME: new name of object (string)
+  or_object{rotate={obj1, obj2, ...}}
+   Rotates object around its center point. Rotation values are absolute, not relative
+   to current rotation.
+   Each obj_ is a table: {name="NAME", x=X, y=Y, z=Z [,w=W]}
+    NAME: name of the object (string)
+    X: 1st rotation,  around x-axis (float)
+    Y: 2nd rotation,  around y-axis (float)
+    Z: 3rd rotation,  around z-axis (float)
+    W: optional (float). If given, (X,Y,Z,W) is taken as a quaternion
+
+  or_object{rename={obj1, obj2, ...}}
+   Renames an object.
+   Each obj_ is a table: {name="NAME", new_name="NEW_NAME"}
+    NAME: current name of the object (string)
+    NEW_NAME: new name of object (string)
 ]==]
 
 -- Constants
@@ -94,6 +108,54 @@ skillenv.skill_module(...)
 function jc_msg_final(state)
    return state.fsm.vars.msgid == if_openrave:msgid() and
           if_openrave:is_final()
+end
+
+function add_object(o)
+   if not (o.name and o.path) then
+      return 0
+   end
+   return if_openrave:msgq_enqueue_copy(if_openrave.AddObjectMessage:new( o.name, o.path ))
+end
+function delete_object(o)
+   if not type(o)=="string" then
+      return 0
+   end
+   return if_openrave:msgq_enqueue_copy(if_openrave.DeleteObjectMessage:new( o ))
+end
+function attach_object(o)
+   if not type(o)=="string" then
+      return 0
+   end
+   return if_openrave:msgq_enqueue_copy(if_openrave.AttachObjectMessage:new( o ))
+end
+function release_object(o)
+   if not type(o)=="string" then
+      return 0
+   end
+   return if_openrave:msgq_enqueue_copy(if_openrave.ReleaseObjectMessage:new( o ))
+end
+function move_object(o)
+   if not (o.name and o.x and o.y and o.z) then
+      return 0
+   end
+   return if_openrave:msgq_enqueue_copy(if_openrave.MoveObjectMessage:new( o.name, o.x, o.y, o.z ))
+end
+function rotate_object(o)
+   if not (o.name and o.x and o.y and o.z) then
+      return 0
+   end
+
+   if o.w then
+      return if_openrave:msgq_enqueue_copy(if_openrave.RotateObjectMessage:new( o.name, o.x, o.y, o.z, o.w ))
+   else
+      return if_openrave:msgq_enqueue_copy(if_openrave.RotateObjectMessage:new( o.name, o.x, o.y, o.z ))
+   end
+end
+function rename_object(o)
+   if not (o.name and o.new_name) then
+      return 0
+   end
+   return if_openrave:msgq_enqueue_copy(if_openrave.RenameObjectMessage:new( o.name, o.new_name ))
 end
 
 -- States
@@ -113,111 +175,122 @@ fsm:define_states{
    {"ROTATE",      JumpState},
    {"RENAME",      JumpState},
 
+   {"PROCESS",      JumpState},
+
    {"CHECK", JumpState},
-   {"CHECK_DELETE", JumpState}
+   {"CHECK_PROCESS", JumpState}
 }
 
 -- Transitions
 fsm:add_transitions {
-   {"INIT", "RELEASE_ALL", cond="vars.release_all", desc="release all"}, -- put here, because do not need name for it
-   {"INIT", "DELETE_ALL", cond="vars.delete_all", desc="delete all"},
-
-   {"INIT", "FAILED", cond="not vars.name", desc="no object name given"},
+   --~ {"INIT", "FAILED", precond="not if_openrave:has_writer()", desc="no writer for interface"},
 
    {"INIT", "ADD", cond="vars.add", desc="add"},
    {"INIT", "DELETE", cond="vars.delete", desc="delete"},
+   {"INIT", "DELETE_ALL", cond="vars.delete_all", desc="delete all"},
    {"INIT", "ATTACH", cond="vars.attach", desc="attach"},
    {"INIT", "RELEASE", cond="vars.release", desc="release"},
+   {"INIT", "RELEASE_ALL", cond="vars.release_all", desc="release all"},
    {"INIT", "MOVE", cond="vars.move", desc="move"},
    {"INIT", "ROTATE", cond="vars.rotate", desc="rotate"},
    {"INIT", "RENAME", cond="vars.rename", desc="rename"},
+   {"INIT", "FAILED", cond=true, desc="unknown argument"},
 
-   {"ADD", "FAILED", precond="not (vars.path)", desc="insufficient arguments"},
-   {"MOVE", "FAILED", precond="not (vars.x and vars.y and vars.z)", desc="insufficient arguments"},
-   {"ROTATE", "FAILED", precond="not (vars.x and vars.y and vars.z)", desc="insufficient arguments"},
-   {"RENAME", "FAILED", precond="not (vars.new_name)", desc="insufficient arguments"},
-
-   {"ADD", "CHECK", cond=jc_msg_final, desc="final"},
-   {"DELETE", "CHECK_DELETE", cond=jc_msg_final, desc="final"},
    {"DELETE_ALL", "CHECK", cond=jc_msg_final, desc="final"},
-   {"ATTACH", "CHECK", cond=jc_msg_final, desc="final"},
-   {"RELEASE", "CHECK", cond=jc_msg_final, desc="final"},
    {"RELEASE_ALL", "CHECK", cond=jc_msg_final, desc="final"},
-   {"MOVE", "CHECK", cond=jc_msg_final, desc="final"},
-   {"ROTATE", "CHECK", cond=jc_msg_final, desc="final"},
-   {"RENAME", "CHECK", cond=jc_msg_final, desc="final"},
 
-   {"CHECK_DELETE", "DELETE", cond="not vars.done", desc="delete next"},
-   {"CHECK_DELETE", "CHECK", cond=true, desc="final"},
+   {"ADD", "PROCESS", cond=true, desc="set up"},
+   {"DELETE", "PROCESS", cond=true, desc="set up"},
+   {"ATTACH", "PROCESS", cond=true, desc="set up"},
+   {"RELEASE", "PROCESS", cond=true, desc="set up"},
+   {"MOVE", "PROCESS", cond=true, desc="set up"},
+   {"ROTATE", "PROCESS", cond=true, desc="set up"},
+   {"RENAME", "PROCESS", cond=true, desc="set up"},
+
+   {"PROCESS", "FAILED", precond="type(vars.objects) ~= 'table'", desc="arguments needs to be a table"},
+   {"PROCESS", "FAILED", precond="#vars.objects == 0", desc="empty argument table"},
+   {"PROCESS", "FAILED", cond="vars.bad_args", desc="insufficient arguments"},
+   {"PROCESS", "CHECK_PROCESS", cond=jc_msg_final, desc="final"},
+
+   {"CHECK_PROCESS", "CHECK", precond="#vars.objects == 0", desc="all objects processed"},
+   {"CHECK_PROCESS", "PROCESS", precond=true, desc="process next object"},
 
    {"CHECK", "FINAL", cond="vars.success", desc="command succeeded"},
    {"CHECK", "FAILED", cond=true, desc="command failed"}
 }
 
 function INIT:init()
+   self.fsm.vars.done = false
    self.fsm.vars.success = true
-end
+   self.fsm.vars.bad_args = false
 
-function ADD:init()
-   self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.AddObjectMessage:new( self.fsm.vars.name,
-                                                                                         self.fsm.vars.path ))
-end
-
-function DELETE:init()
-   if type(self.fsm.vars.name) == "table" then
-      self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.DeleteObjectMessage:new( self.fsm.vars.name[1] ))
-      table.remove(self.fsm.vars.name, 1)
-      self.fsm.vars.done = #self.fsm.vars.name == 0
-   else
-      self.fsm.vars.done = true
-      self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.DeleteObjectMessage:new( self.fsm.vars.name ))
-   end
-end
-function DELETE:exit()
-   self.fsm.vars.success = self.fsm.vars.success and if_openrave:is_success()
+   self.fsm.vars.objects = {}
+   self.fsm.vars.func = nil
 end
 
 function DELETE_ALL:init()
    self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.DeleteAllObjectsMessage:new())
 end
 
-function ATTACH:init()
-   self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.AttachObjectMessage:new( self.fsm.vars.name ))
-end
-
-function RELEASE:init()
-   self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.ReleaseObjectMessage:new( self.fsm.vars.name ))
-end
-
 function RELEASE_ALL:init()
    self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.ReleaseAllObjectsMessage:new())
 end
 
+function ADD:init()
+   self.fsm.vars.objects = self.fsm.vars.add
+   self.fsm.vars.func = add_object
+end
+
+function DELETE:init()
+   self.fsm.vars.objects = self.fsm.vars.delete
+   self.fsm.vars.func = delete_object
+end
+
+function ATTACH:init()
+   self.fsm.vars.objects = self.fsm.vars.attach
+   self.fsm.vars.func = attach_object
+end
+
+function RELEASE:init()
+   self.fsm.vars.objects = self.fsm.vars.release
+   self.fsm.vars.func = release_object
+end
+
 function MOVE:init()
-   self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.MoveObjectMessage:new( self.fsm.vars.name,
-                                                                                          self.fsm.vars.x,
-                                                                                          self.fsm.vars.y,
-                                                                                          self.fsm.vars.z ))
+   self.fsm.vars.objects = self.fsm.vars.move
+   self.fsm.vars.func = move_object
 end
 
 function ROTATE:init()
-   if self.fsm.vars.w then
-      self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.RotateObjectQuatMessage:new( self.fsm.vars.name,
-                                                                                                   self.fsm.vars.x,
-                                                                                                   self.fsm.vars.y,
-                                                                                                   self.fsm.vars.z,
-                                                                                                   self.fsm.vars.w ))
-   else
-      self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.RotateObjectMessage:new( self.fsm.vars.name,
-                                                                                               self.fsm.vars.x,
-                                                                                               self.fsm.vars.y,
-                                                                                               self.fsm.vars.z ))
-   end
+   self.fsm.vars.objects = self.fsm.vars.rotate
+   self.fsm.vars.func = rotate_object
 end
 
 function RENAME:init()
-   self.fsm.vars.msgid = if_openrave:msgq_enqueue_copy(if_openrave.RenameObjectMessage:new( self.fsm.vars.name,
-                                                                                            self.fsm.vars.new_name ))
+   self.fsm.vars.objects = self.fsm.vars.rename
+   self.fsm.vars.func = rename_object
+end
+
+
+function PROCESS:init()
+   local obj = self.fsm.vars.objects[1]
+   if obj == nil then
+      --print("Object is nil. Forgot table-brackets or string-quotes?")
+      self.fsm.vars.bad_args = true
+      return
+   end
+
+   self.fsm.vars.msgid = self.fsm.vars.func( obj )
+   if self.fsm.vars.msgid == 0 then
+      --print("Insufficient arguments for this object")
+      self.fsm.vars.bad_args = true
+   else
+      --print("Processed one of "..#self.fsm.vars.objects.." remaining objects")
+      table.remove(self.fsm.vars.objects, 1)
+   end
+end
+function PROCESS:exit()
+   self.fsm.vars.success = self.fsm.vars.success and if_openrave:is_success()
 end
 
 function CHECK:init()
