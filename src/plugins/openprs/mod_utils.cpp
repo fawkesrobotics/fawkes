5b65f2df3ec38143d2e80956009032d0cd34d950
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2014-09-09 10:47:23 +0200

    openprs: add op-names function and some minor cleanup
    Add evaluable function which allows to retrieve the OP names of a list
    of OP instances or intentions. Along the way improve the code a bit.

--- src/plugins/openprs/mod_utils.cpp
+++ src/plugins/openprs/mod_utils.cpp
@@ -25,23 +25,43 @@
 
 extern "C" {
   typedef struct op_structure Op_Structure;
+
+  typedef enum {IS_ACTIVE, IS_SLEEPING, IS_SUSP_ACTIVE, IS_SUSP_SLEEPING} Intention_Status;
+  typedef Slist *Thread_Intention_Block_List;
+  typedef Slist *Condition_List;
+  struct intention {
+     Fact * fact;
+     Goal * goal;
+     Thread_Intention_Block_List fils;
+     Thread_Intention_Block_List active_tibs;
+     Op_Instance *top_op;
+     short priority;
+     Intention_Status status;
+     Symbol id;
+     Thread_Intention_Block *critical_section;
+     PDate creation;
+     Condition_List activation_conditions_list;
+     Sprinter *failed_goal_sprinter;
+     OPRS_LIST failed_goal_stack;
+  };
 }
 
 #include <op-instance_f-pub.h>
 #include <op-structure_f-pub.h>
+#include <lisp-list_f-pub.h>
 
 extern "C"
 Term *
 func_op_name(TermList terms)
 {
-  Term *op_t, *res;
+  Term *op_t;
   Op_Instance *opi;
 
   op_t  = (Term *)get_list_pos(terms, 1);
   if (! op_t ) {
     report_fatal_external_error((char *)"Cannot retrieve OP instance term");
   }
-  if (! op_t->type == TT_OP_INSTANCE) {
+  if (op_t->type != TT_OP_INSTANCE) {
     report_fatal_external_error((char *)"Term is not of type OP_INSTANCE");
   }
 
@@ -50,12 +70,47 @@ func_op_name(TermList terms)
   if (! op_s) {
     report_fatal_external_error((char *)"Failed to get OP structure from OP instance");
   }
-  Symbol name = op_name(op_s);
 
-  res = MAKE_OBJECT(Term);
-  res->type = TT_ATOM;
-  res->u.id = name;
-  return res;
+  return build_id(op_name(op_s));
+}
+
+
+extern "C"
+Term *
+func_op_names(TermList terms)
+{
+  Term *ops_t;
+
+  ops_t  = (Term *)get_list_pos(terms, 1);
+  if (! ops_t ) {
+    report_fatal_external_error((char *)"Cannot retrieve OP instance term");
+  }
+  if (ops_t->type != LISP_LIST) {
+    report_fatal_external_error((char *)"Term is not of type LISP_LIST");
+  }
+
+  TermList name_list = sl_make_slist();
+
+  for (L_List p_l = ops_t->u.l_list; p_l; p_l = l_cdr(p_l)) {
+    Term *t = l_car(p_l);
+    if (t->type == TT_INTENTION) {
+      Op_Instance *opi = (Op_Instance *)(t->u.in->top_op);
+      Op_Structure *op_s = op_instance_op(opi);
+      name_list = build_term_list(name_list, build_id(op_name(op_s)));
+    } else if (t->type == TT_OP_INSTANCE) {
+      Op_Instance *opi = (Op_Instance *)(t->u.opi);
+      Op_Structure *op_s = op_instance_op(opi);
+      if (! op_s) {
+        name_list = build_term_list(name_list, build_id(declare_atom("NOT-AN-OP")));
+      } else {
+        name_list = build_term_list(name_list, build_id(op_name(op_s)));
+      }
+    } else {
+      name_list = build_term_list(name_list, build_id(declare_atom("NOT-AN-OP-INSTANCE")));
+    }
+  }
+
+  return build_term_l_list_from_c_list(name_list);
 }
 
 
@@ -65,6 +120,7 @@ void init()
 {
   printf("*** LOADING mod_utils\n");
   make_and_declare_eval_funct("op-name", func_op_name, 1);
+  make_and_declare_eval_funct("op-names", func_op_names, 1);
 
   const char *gdb_delay_env = getenv("FAWKES_OPRS_GDB_DELAY");
   if (gdb_delay_env && strcmp(gdb_delay_env, "true") == 0) {
