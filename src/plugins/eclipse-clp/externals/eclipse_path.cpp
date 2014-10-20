5495d3a034a3fc3f622d88f22ee15a0627278225
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2014-10-20 17:27:22 +0200

    eclipse-clp: code cleanup, indentation, initialization
    Cleanup the code, add missing headers, fix indentation, properly
    initialize singleton and avoid throwing exceptions.

--- src/plugins/eclipse-clp/externals/eclipse_path.cpp
+++ src/plugins/eclipse-clp/externals/eclipse_path.cpp
@@ -1,3 +1,25 @@
+
+/***************************************************************************
+ *  eclipse_path.cpp - Eclipse-CLP path externals
+ *
+ *  Created: Thu Feb 27 15:21:35 2014
+ *  Copyright  2014 Gesche Gierse
+ *             2014 Tim Niemueller
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
 #include "eclipse_path.h"
 #include <iterator>
 #include <iostream>
@@ -13,28 +35,23 @@ EclipsePath* EclipsePath::m_instance = NULL;
 /** Constructor. */
 EclipsePath::EclipsePath()
 {
-	if (m_instance == NULL){
-		m_instance = this;
-   	}else{
-		//throw Exception( "There is already an instance of type EclipsePath instantiated" );
-	}
-	
 }
 
 void
-EclipsePath::create_initial_object(){
-	m_instance = new EclipsePath();
-	m_instance->add_regex(boost::regex("@BASEDIR@"), BASEDIR);
-	m_instance->add_regex(boost::regex("@CONFDIR@"), CONFDIR);
-	m_instance->add_regex(boost::regex("@FAWKESDIR@"), FAWKES_BASEDIR);
+EclipsePath::create_initial_object()
+{
+  if (m_instance)  return;
+
+  m_instance = new EclipsePath();
+  m_instance->add_regex(boost::regex("@BASEDIR@"), BASEDIR);
+  m_instance->add_regex(boost::regex("@CONFDIR@"), CONFDIR);
+  m_instance->add_regex(boost::regex("@FAWKESDIR@"), FAWKES_BASEDIR);
 }
 
 EclipsePath* EclipsePath::instance()
 {
-	if ( !m_instance )
-	{ throw Exception( "No instance of type EclipsePath initialised" ); }
-
-	return m_instance;
+  create_initial_object();
+  return m_instance;
 }
 
 
@@ -42,74 +59,73 @@ EclipsePath* EclipsePath::instance()
 void
 EclipsePath::add_path(std::string path)
 {
-	//std::cout << "add path: " << path << '\n';
-	paths.push_back(path);
+  paths.push_back(path);
 }
 
 
 void
 EclipsePath::add_path_check(std::string path)
 {
-	instance()->add_path(path);
-	instance()->apply_regexes();
+  instance()->add_path(path);
+  instance()->apply_regexes();
 }
 
 std::string
 EclipsePath::locate_file(std::string filename)
 {
-	if (paths.empty()){
-		return "";
-	}
-	//std::cout << "locate file: " << filename << '\n';
-	for (std::vector<std::string>::iterator it = paths.begin(); it != paths.end(); ++it){
-		path p (*it);
-		p /= filename;
-		//std::cout << "locate file: created path for:" << p.native() << '\n' ;
-		try
-		{
-			if (exists(p))
-			{
-				//std::cout << "found file " << filename << " at:" << '\n'; 
-				return p.native();
-			}
-		}
-		catch (const filesystem_error& ex)
-		{
-			throw Exception( "Filesystem error" );
-		}
+  if (paths.empty()){
+    return "";
+  }
+  //std::cout << "locate file: " << filename << '\n';
+  for (std::vector<std::string>::iterator it = paths.begin(); it != paths.end(); ++it){
+    path p (*it);
+    p /= filename;
+    //std::cout << "locate file: created path for:" << p.native() << '\n' ;
+    try
+    {
+      if (exists(p))
+      {
+	//std::cout << "found file " << filename << " at:" << '\n'; 
+	return p.native();
+      }
+    }
+    catch (const filesystem_error& ex)
+    {
+      throw Exception( "Filesystem error" );
+    }
 		
-	}
-	return "";
+  }
+  return "";
 }
 
 void
 EclipsePath::apply_regexes()
 {
-	int i;
-	std::vector<std::string>::iterator it;
-	for (i = 0, it = paths.begin(); it != paths.end(); ++it, i++){
-		for (std::map<boost::regex,std::string>::iterator re=regexes.begin(); re!=regexes.end(); ++re){
-		std::string result = boost::regex_replace(*it, re->first, re->second);
-		//std::cout << "path: " << paths[i] << '\n'; 
-		paths[i]=result;
-		//std::cout << "applying: " << re->first << "=>" << re->second << "\nregex result:" << result << '\n';
+  int i;
+  std::vector<std::string>::iterator it;
+  for (i = 0, it = paths.begin(); it != paths.end(); ++it, i++){
+    for (std::map<boost::regex,std::string>::iterator re=regexes.begin(); re!=regexes.end(); ++re){
+      std::string result = boost::regex_replace(*it, re->first, re->second);
+      //std::cout << "path: " << paths[i] << '\n'; 
+      paths[i]=result;
+      //std::cout << "applying: " << re->first << "=>" << re->second << "\nregex result:" << result << '\n';
 
-	}
-	}
+    }
+  }
 }
 
 void
 EclipsePath::print_all_paths()
 {
-	for ( std::vector<std::string>::iterator it = paths.begin(); it != paths.end(); ++it){
-              //std::cout << *it << '\n';
-	}
+  for ( std::vector<std::string>::iterator it = paths.begin(); it != paths.end(); ++it){
+    //std::cout << *it << '\n';
+  }
 }
 
 void
 EclipsePath::add_regex(boost::regex re, std::string str)
 {
-	regexes.insert( std::pair<boost::regex,std::string>(re, str) );
+  regexes.insert( std::pair<boost::regex,std::string>(re, str) );
 }
 
 
@@ -117,36 +133,15 @@ EclipsePath::add_regex(boost::regex re, std::string str)
 int
 p_locate_file(...)
 {
-	char* filename;
-	if ( EC_succeed != EC_arg(1).is_string ( &filename ) )
-	{
-		printf( "p_locate_file(): no filename given\n" );
-	}
-	std::string p = EclipsePath::instance()->locate_file(filename);
-	if (EC_succeed != EC_arg(2).unify( EC_word(p.c_str()) ) ){
-	  printf( "p_locate_file(): could not bind return valie\n" );
-	  return EC_fail;
-	}
-	return p.empty() ? EC_fail : EC_succeed;
+  char* filename;
+  if ( EC_succeed != EC_arg(1).is_string ( &filename ) )
+  {
+    printf( "p_locate_file(): no filename given\n" );
+  }
+  std::string p = EclipsePath::instance()->locate_file(filename);
+  if (EC_succeed != EC_arg(2).unify( EC_word(p.c_str()) ) ){
+    printf( "p_locate_file(): could not bind return valie\n" );
+    return EC_fail;
+  }
+  return p.empty() ? EC_fail : EC_succeed;
 }
-
-
-//int main()
-//{
-//	std::cout << "main" << '\n' ;
-//	EclipsePath::create_initial_object();
-//	EclipsePath::instance()->add_path_check("@basedir@/src/plugins/eclipse-clp");
-//	EclipsePath::instance()->add_path("/home/ggierse");
-//	std::cout << "created object" << '\n';
-//	path res = EclipsePath::instance()->locate_file("fawkes");
-//	std::cout << res << '\n';
-//	std::cout << "Paths are now: \n";
-//	std::vector<std::string> paths = EclipsePath::instance()->paths;
-//	for ( std::vector<std::string>::iterator it = paths.begin(); it != paths.end(); ++it){
-//		std::cout << *it << '\n';
-//	}
-//	std::map<boost::regex,std::string> regexes = EclipsePath::instance()->regexes;
-//        for ( std::map<boost::regex,std::string>::iterator iit = regexes.begin(); iit != regexes.end(); ++iit){
-//                std::cout << iit->first << "=>" << iit->second <<  '\n';
-//        }
-//}
