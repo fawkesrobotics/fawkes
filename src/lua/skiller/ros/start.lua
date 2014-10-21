02e14fc8cd3c1d62c5a3a6cc6324b976fc9d3d3c
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2014-10-21 11:15:44 +0200

    skiller: setup roslua logging to be piped through Fawkes
    Replace roslua's default logger with one that outputs to the Fawkes
    logger. This eases the integration of roslua-based code into the Fawkes
    skiller with ROS integration.

--- src/lua/skiller/ros/start.lua
+++ src/lua/skiller/ros/start.lua
@@ -19,10 +19,9 @@
 --
 --  Read the full text in the LICENSE.GPL file in the doc directory.
 
-require("roslua.logging")
-require("fawkes.logprint")
+local roslua_logging = require("roslua.logging")
+local logprint = require("fawkes.logprint")
 require("fawkes.depinit")
---roslua.logging.register_print_funcs(fawkes.logprint)
 
 require("actionlib")
 require("fawkes.mathext")
@@ -33,9 +32,24 @@ local actinitmod = require("skiller.ros.action_initializer")
 local topinitmod = require("skiller.ros.topic_initializer")
 local srvinitmod = require("skiller.ros.service_initializer")
 
+function ros_log_fawkes(level, time, msg)
+   if level == roslua.logging.DEBUG then
+      logprint.print_debug("%s", msg)
+   elseif level == roslua.logging.WARN then
+      logprint.print_warn("%s", msg)
+   elseif level == roslua.logging.ERROR then
+      logprint.print_error("%s", msg)
+   elseif level == roslua.logging.FATAL then
+      logprint.print_error("%s", msg)
+   end
+   logprint.print_info("%s", msg)
+end
+
+roslua_logging.remove_stdout_logger()
+roslua_logging.add_logger(ros_log_fawkes)
+
 skiller.ros.init{no_signal_handler = true,
-		 no_action_server  = true,
-		 no_print_funcs    = true}
+		 no_action_server  = true}
 
 _G.skillenv = require("skiller.skillenv")
 skillenv.add_export("ActionJumpState", actjsmod.ActionJumpState)
