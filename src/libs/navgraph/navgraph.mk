3c687a7bd455b145d799b1a84d5921e5e318a2f2
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2015-01-13 15:59:20 +0100

    libnavgraph: move buildsys bits to own file
    For easier inclusion in other modules.

---
+++ src/libs/navgraph/navgraph.mk
@@ -0,0 +1,62 @@
+#*****************************************************************************
+#        Makefile Build System for Fawkes: NavGraph Library
+#                            -------------------
+#   Created on Tue Jan 13 10:47:38 2015
+#   Copyright (C) 2015 by Tim Niemueller, AllemaniACs RoboCup Team
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifndef __buildsys_config_mk_
+$(error config.mk must be included before ros.mk)
+endif
+
+ifndef __navgraph_mk_
+__navgraph_mk_ := 1
+
+include $(BUILDSYSDIR)/ext/gmsl
+
+ifneq ($(PKGCONFIG),)
+  HAVE_YAMLCPP   = $(if $(shell $(PKGCONFIG) --exists 'yaml-cpp'; echo $${?/1/}),1,0)
+endif
+ifeq ($(HAVE_YAMLCPP),1)
+  HAVE_YAMLCPP_0_2_6 = $(if $(shell $(PKGCONFIG) --atleast-version='0.2.6' 'yaml-cpp'; echo $${?/1/}),1,0)
+
+  CFLAGS_YAMLCPP  = -DHAVE_YAMLCPP $(shell $(PKGCONFIG) --cflags 'yaml-cpp')
+  LDFLAGS_YAMLCPP = $(shell $(PKGCONFIG) --libs 'yaml-cpp')
+  ifneq ($(HAVE_YAMLCPP_0_2_6),1)
+    CFLAGS_YAMLCPP += -DHAVE_OLD_YAMLCPP
+  endif
+
+  HAVE_YAMLCPP_0_5 = $(if $(shell $(PKGCONFIG) --atleast-version='0.5' 'yaml-cpp'; echo $${?/1/}),1,0)
+  ifeq ($(HAVE_YAMLCPP_0_5),1)
+    CFLAGS_YAMLCPP += -DHAVE_YAMLCPP_0_5
+  endif
+
+  CFLAGS  += $(CFLAGS_YAMLCPP)
+  LDFLAGS += $(LDFLAGS_YAMLCPP)
+endif
+
+ifeq ($(HAVE_YAMLCPP)$(HAVE_CPP11),11)
+  HAVE_NAVGRAPH=1
+
+  CFLAGS_NAVGRAPH=$(CFLAGS_CPP11)
+
+else
+  ifneq ($(HAVE_YAMLCPP),1)
+    NAVGRAPH_ERRORS += "yaml-cpp[-devel]_not_installed"
+  endif
+  ifneq ($(HAVE_CPP11),1)
+    NAVGRAPH_ERRORS += "compiler_does_not_support_C++11"
+  endif
+
+  _NAVGRAPH_COMMA := ,
+  NAVGRAPH_ERROR=$(subst _, ,$(subst :,$(_NAVGRAPH_COMMA) ,$(call merge,:,$(NAVGRAPH_ERRORS))))
+endif
+
+endif # __navgraph_mk_
