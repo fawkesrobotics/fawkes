3298ba9abff7a307fb5cfe085283fb22522338c0
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2015-01-23 10:12:26 +0100

    blackboard: add wrapper that automatically sets ownership info
    This wrapper takes an owner name for newly created interfaces which is
    set automatically unless it is overridden by a direct argument to one of
    the opening methods.

---
+++ src/libs/blackboard/ownership.cpp
@@ -0,0 +1,119 @@
+
+/***************************************************************************
+ *  ownership.cpp - BlackBoard with traced ownership
+ *
+ *  Created: Thu Jan 22 15:19:03 2015
+ *  Copyright  2015  Tim Niemueller [www.niemueller.de]
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#include <blackboard/ownership.h>
+
+#include <string>
+#include <cstring>
+
+namespace fawkes {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+/** @class BlackBoardWithOwnership <blackboard/ownership.h>
+ * BlackBoard that traces interface ownership.
+ *
+ * @see Interface
+ * @see Message
+ *
+ * @author Tim Niemueller
+ */
+
+
+/** Constructor.
+ * @param parent parent blackboard to use as actual blackboard
+ * @param owner owner name to record in newly created interfaces
+ */
+BlackBoardWithOwnership::BlackBoardWithOwnership(fawkes::BlackBoard *parent, const char *owner)
+  : blackboard_(parent), owner_(owner)
+{
+}
+
+
+/** Destructor. */
+BlackBoardWithOwnership::~BlackBoardWithOwnership()
+{
+}
+
+
+Interface *
+BlackBoardWithOwnership::open_for_reading(const char *type, const char *identifier, const char *owner)
+{
+  return blackboard_->open_for_reading(type, identifier,
+				       owner ? owner : owner_.c_str());
+}
+
+
+Interface *
+BlackBoardWithOwnership::open_for_writing(const char *type, const char *identifier, const char *owner)
+{
+  return blackboard_->open_for_writing(type, identifier,
+				       owner ? owner : owner_.c_str());
+}
+
+
+std::list<Interface *>
+BlackBoardWithOwnership::open_multiple_for_reading(const char *type_pattern,
+							 const char *id_pattern,
+							 const char *owner)
+{
+  return blackboard_->open_multiple_for_reading(type_pattern, id_pattern,
+						owner ? owner : owner_.c_str());
+}
+
+
+void
+BlackBoardWithOwnership::close(Interface *interface)
+{
+  blackboard_->close(interface);
+}
+
+
+InterfaceInfoList *
+BlackBoardWithOwnership::list_all()
+{
+  return blackboard_->list_all();
+}
+
+
+InterfaceInfoList *
+BlackBoardWithOwnership::list(const char *type_pattern, const char *id_pattern)
+{
+  return blackboard_->list(type_pattern, id_pattern);
+}
+
+
+bool
+BlackBoardWithOwnership::is_alive() const throw()
+{
+  return blackboard_->is_alive();
+}
+
+
+bool
+BlackBoardWithOwnership::try_aliveness_restore() throw()
+{
+  return blackboard_->try_aliveness_restore();
+}
+
+} // end namespace fawkes
---
+++ src/libs/blackboard/ownership.h
@@ -0,0 +1,67 @@
+
+/***************************************************************************
+ *  ownership.h - BlackBoard with traced ownership
+ *
+ *  Created: Thu Jan 22 15:16:15 2015
+ *  Copyright  2015  Tim Niemueller [www.niemueller.de]
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#ifndef __BLACKBOARD_OWNERSHIP_H_
+#define __BLACKBOARD_OWNERSHIP_H_
+
+#include <blackboard/blackboard.h>
+
+
+namespace fawkes {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+class BlackBoardWithOwnership : public BlackBoard
+{
+ public:
+  BlackBoardWithOwnership(BlackBoard *parent, const char *owner);
+  virtual ~BlackBoardWithOwnership();
+
+  virtual Interface *  open_for_reading(const char *interface_type,
+					const char *identifier,
+					const char *owner = NULL);
+  virtual Interface *  open_for_writing(const char *interface_type,
+					const char *identifier,
+					const char *owner = NULL);
+  virtual void         close(Interface *interface);
+
+  virtual InterfaceInfoList *  list_all();
+  virtual InterfaceInfoList *  list(const char *type_pattern,
+				    const char *id_pattern);
+  virtual bool                 is_alive() const throw();
+  virtual bool                 try_aliveness_restore() throw();
+
+  virtual std::list<Interface *>
+    open_multiple_for_reading(const char *type_pattern,
+			      const char *id_pattern = "*",
+			      const char *owner = NULL);
+
+ private: /* members */
+  BlackBoard  *blackboard_;
+  std::string  owner_;
+};
+
+
+} // end namespace fawkes
+
+#endif
