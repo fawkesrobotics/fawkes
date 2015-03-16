cce430f34a242bf36a409035df1f33aa162cf881
Frederik Zwilling <zwilling@kbsg.rwth-aachen.de>
2015-03-16 20:53:18 +0100

    gazsim-webcam: support of multiple simulated webcams

--- src/plugins/gazebo/gazsim-webcam/gazsim_webcam.h
+++ src/plugins/gazebo/gazsim-webcam/gazsim_webcam.h
@@ -1,9 +1,8 @@
 /***************************************************************************
- *  gazsim_webcam_plugin.cpp - Plugin simulates Webcams in Gazebo and
- *                             provides a shared memory buffer
+ *  gazsim_webcam.h - Class to simulate a single webcam from gazebo
  *
- *  Created: Sat Sep 21 17:35:27 2013
- *  Copyright  2013  Frederik Zwilling
+ *  Created: Mon Mar 16 19:38:43 2015
+ *  Copyright  2015  Frederik Zwilling
  ****************************************************************************/
 
 /*  This program is free software; you can redistribute it and/or modify
@@ -19,8 +18,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef __PLUGINS_GAZSIM_WEBCAM_THREAD_H_
-#define __PLUGINS_GAZSIM_WEBCAM_THREAD_H_
+#ifndef __PLUGINS_GAZSIM_WEBCAM_H_
+#define __PLUGINS_GAZSIM_WEBCAM_H_
 
 #include <core/threading/thread.h>
 #include <aspect/clock.h>
@@ -30,6 +29,7 @@
 #include <aspect/blocked_timing.h>
 #include <plugins/gazebo/aspect/gazebo.h>
 #include <string.h>
+#include <config/config.h>
 
 #include <fvutils/ipc/shm_image.h>
 #include <boost/circular_buffer.hpp>
@@ -39,29 +39,36 @@
 #include <gazebo/msgs/MessageTypes.hh>
 #include <gazebo/transport/transport.hh>
 
-#include "gazsim_webcam.h"
 
-class WebcamSimThread
-: public fawkes::Thread,
-  public fawkes::ClockAspect,
-  public fawkes::LoggingAspect,
-  public fawkes::ConfigurableAspect,
-  public fawkes::BlackBoardAspect,
-  public fawkes::BlockedTimingAspect,
-  public fawkes::GazeboAspect
+class GazsimWebcam
 {
  public:
-  WebcamSimThread();
+  GazsimWebcam(std::string shm_id,
+	       gazebo::transport::NodePtr gazebo_world_node,
+	       fawkes::Configuration* config);
+  ~GazsimWebcam();
 
-  virtual void init();
-  virtual void loop();
-  virtual void finalize();
+ private:  
+  //Subscriber to receive webcam data from gazebo
+  gazebo::transport::SubscriberPtr webcam_sub_;
 
- private:
-  std::vector<GazsimWebcam*> webcams_;
-  
+  //handler function for incoming webcam data messages
+  void on_webcam_data_msg(ConstImageStampedPtr &msg);
+
+  //shared memory buffer
+  firevision::SharedMemoryImageBuffer *shm_buffer_;
+  //reference to the buffer of shm_buffer_
+  unsigned char *buffer_;
+
   //config values
-  std::vector<std::string> shm_ids_;
+  //topic name of the gazebo message publisher
+  std::string topic_name_;
+  double width_, height_;
+  //id of the shared memory object
+  std::string shm_id_;
+  std::string frame_;
+  firevision::colorspace_t format_from_;
+  firevision::colorspace_t format_to_;
 };
 
 #endif
