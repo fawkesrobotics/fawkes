ec0cd369a445fbd898d64f96dad1907fae61d406
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2015-05-22 23:42:01 +0200

    laser-pointclouds: add support for Laser1080Interface

--- src/plugins/laser-pointclouds/laser_pointcloud_thread.cpp
+++ src/plugins/laser-pointclouds/laser_pointcloud_thread.cpp
@@ -27,6 +27,7 @@
 
 #include <interfaces/Laser360Interface.h>
 #include <interfaces/Laser720Interface.h>
+#include <interfaces/Laser1080Interface.h>
 
 using namespace fawkes;
 
@@ -61,11 +62,14 @@ LaserPointCloudThread::init()
   std::list<Laser720Interface *> l720ifs =
     blackboard->open_multiple_for_reading<Laser720Interface>("*");
 
+  std::list<Laser1080Interface *> l1080ifs =
+    blackboard->open_multiple_for_reading<Laser1080Interface>("*");
+
   LockList<Laser360Interface *>::iterator i;
   for (i = l360ifs.begin(); i != l360ifs.end(); ++i) {
     InterfaceCloudMapping mapping;
     mapping.id = interface_to_pcl_name((*i)->id());
-    mapping.is_360 = true;
+    mapping.size = 360;
     mapping.interface_typed.as360 = *i;
     mapping.interface = *i;
     mapping.interface->read();
@@ -83,7 +87,7 @@ LaserPointCloudThread::init()
   for (j = l720ifs.begin(); j != l720ifs.end(); ++j) {
     InterfaceCloudMapping mapping;
     mapping.id = interface_to_pcl_name((*j)->id());
-    mapping.is_360 = false;
+    mapping.size = 720;
     mapping.interface_typed.as720 = *j;
     mapping.interface = *j;
     mapping.interface->read();
@@ -98,10 +102,30 @@ LaserPointCloudThread::init()
     __mappings.push_back(mapping);
   }
 
+  LockList<Laser1080Interface *>::iterator k;
+  for (k = l1080ifs.begin(); k != l1080ifs.end(); ++k) {
+    InterfaceCloudMapping mapping;
+    mapping.id = interface_to_pcl_name((*k)->id());
+    mapping.size = 1080;
+    mapping.interface_typed.as1080 = *k;
+    mapping.interface = *k;
+    mapping.interface->read();
+    mapping.cloud = new pcl::PointCloud<pcl::PointXYZ>();
+    mapping.cloud->points.resize(1080);
+    mapping.cloud->header.frame_id = (*k)->frame();
+    mapping.cloud->height = 1;
+    mapping.cloud->width = 1080;
+    pcl_manager->add_pointcloud(mapping.id.c_str(), mapping.cloud);
+    bbil_add_reader_interface(*k);
+    bbil_add_writer_interface(*k);
+    __mappings.push_back(mapping);
+  }
+
   blackboard->register_listener(this);
 
   bbio_add_observed_create("Laser360Interface", "*");
   bbio_add_observed_create("Laser720Interface", "*");
+  bbio_add_observed_create("Laser1080Interface", "*");
   blackboard->register_observer(this);
 
   // Generate lookup tables for sin and cos
@@ -112,6 +136,10 @@ LaserPointCloudThread::init()
   for (unsigned int i = 0; i < 720; ++i) {
     sin_angles720[i] = sinf(deg2rad((float)i / 2.));
     cos_angles720[i] = cosf(deg2rad((float)i / 2.));
+  }
+  for (unsigned int i = 0; i < 1080; ++i) {
+    sin_angles1080[i] = sinf(deg2rad((float)i / 3.));
+    cos_angles1080[i] = cosf(deg2rad((float)i / 3.));
   }
 }
 
@@ -142,19 +170,28 @@ LaserPointCloudThread::loop()
     if (! m->interface->changed()) {
       continue;
     }
-    if (m->is_360) {
+    if (m->size == 360) {
       m->cloud->header.frame_id = m->interface_typed.as360->frame();
       float *distances = m->interface_typed.as360->distances();
       for (unsigned int i = 0; i < 360; ++i) {
         m->cloud->points[i].x = distances[i] * cos_angles360[i];
         m->cloud->points[i].y = distances[i] * sin_angles360[i];
       }
-    } else {
+
+    } else if (m->size == 720) {
       m->cloud->header.frame_id = m->interface_typed.as720->frame();
       float *distances = m->interface_typed.as720->distances();
       for (unsigned int i = 0; i < 720; ++i) {
         m->cloud->points[i].x = distances[i] * cos_angles720[i];
         m->cloud->points[i].y = distances[i] * sin_angles720[i];
+      }
+
+    } else if (m->size == 1080) {
+      m->cloud->header.frame_id = m->interface_typed.as1080->frame();
+      float *distances = m->interface_typed.as1080->distances();
+      for (unsigned int i = 0; i < 1080; ++i) {
+        m->cloud->points[i].x = distances[i] * cos_angles1080[i];
+        m->cloud->points[i].y = distances[i] * sin_angles1080[i];
       }
     }
 
@@ -182,7 +219,7 @@ LaserPointCloudThread::bb_interface_created(const char *type, const char *id) th
     }
 
     try {
-      mapping.is_360 = true;
+      mapping.size = 360;
       mapping.interface_typed.as360 = lif;
       mapping.interface = lif;
       mapping.cloud->points.resize(360);
@@ -207,12 +244,37 @@ LaserPointCloudThread::bb_interface_created(const char *type, const char *id) th
     }
 
     try {
-      mapping.is_360 = false;
+      mapping.size = 720;
       mapping.interface_typed.as720 = lif;
       mapping.interface = lif;
       mapping.cloud->points.resize(720);
       mapping.cloud->header.frame_id = lif->frame();
       mapping.cloud->width = 720;
+      pcl_manager->add_pointcloud(mapping.id.c_str(), mapping.cloud);
+    } catch (Exception &e) {
+      logger->log_warn(name(), "Failed to add pointcloud %s: %s",
+                       mapping.id.c_str(), e.what());
+      blackboard->close(lif);
+      return;
+    }
+
+  } else if (strncmp(type, "Laser1080Interface", __INTERFACE_TYPE_SIZE) != 0) {
+    Laser1080Interface *lif;
+    try {
+      lif = blackboard->open_for_reading<Laser1080Interface>(id);
+    } catch (Exception &e) {
+      // ignored
+      logger->log_warn(name(), "Failed to open %s:%s: %s", type, id, e.what());
+      return;
+    }
+
+    try {
+      mapping.size = 1080;
+      mapping.interface_typed.as1080 = lif;
+      mapping.interface = lif;
+      mapping.cloud->points.resize(1080);
+      mapping.cloud->header.frame_id = lif->frame();
+      mapping.cloud->width = 1080;
       pcl_manager->add_pointcloud(mapping.id.c_str(), mapping.cloud);
     } catch (Exception &e) {
       logger->log_warn(name(), "Failed to add pointcloud %s: %s",
@@ -263,6 +325,7 @@ LaserPointCloudThread::conditional_close(Interface *interface) throw()
 {
   Laser360Interface *l360if = dynamic_cast<Laser360Interface *>(interface);
   Laser720Interface *l720if = dynamic_cast<Laser720Interface *>(interface);
+  Laser1080Interface *l1080if = dynamic_cast<Laser1080Interface *>(interface);
 
   bool close = false;
   InterfaceCloudMapping mapping;
@@ -271,16 +334,18 @@ LaserPointCloudThread::conditional_close(Interface *interface) throw()
 
   fawkes::LockList<InterfaceCloudMapping>::iterator m;
   for (m = __mappings.begin(); m != __mappings.end(); ++m) {
-    bool match = ((  m->is_360 && l360if && (*l360if == *m->interface_typed.as360)) ||
-                  (! m->is_360 && l720if && (*l720if == *m->interface_typed.as720)));
+    bool match = ((m->size == 360 && l360if && (*l360if == *m->interface_typed.as360)) ||
+                  (m->size == 720 && l720if && (*l720if == *m->interface_typed.as720)) ||
+                  (m->size == 1080 && l1080if && (*l1080if == *m->interface_typed.as1080)));
+
 
     if (match) {
       if (! m->interface->has_writer() && (m->interface->num_readers() == 1)) {
         // It's only us
         logger->log_info(name(), "Last on %s, closing", m->interface->uid());
-         close = true;
-         mapping = *m;
-         __mappings.erase(m);
+	close = true;
+	mapping = *m;
+	__mappings.erase(m);
         break;
       }
     }
