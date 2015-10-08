6174f3e7aaa580d0f40d8ef4c0ded5b10a7b1104
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2015-10-08 23:59:38 +0200

    buildsys: find CGAL on Ubuntu

--- etc/buildsys/cgal.mk
+++ etc/buildsys/cgal.mk
@@ -30,7 +30,7 @@ CGAL_ERRORS=
 
 ifeq ($(CGAL_HAVE_BOOST_LIBS),1)
   ifneq ($(wildcard $(SYSROOT)/usr/include/CGAL/version.h $(SYSROOT)/usr/local/include/CGAL/version.h),)
-    ifneq ($(wildcard $(SYSROOT)/usr/include/gmp.h $(SYSROOT)/usr/local/include/gmp.h),)
+    ifneq ($(wildcard $(SYSROOT)/usr/include/gmp.h $(SYSROOT)/usr/local/include/gmp.h $(SYSROOT)/usr/include/$(ARCH)-linux-gnu/gmp.h),)
       ifneq ($(wildcard $(SYSROOT)/usr/include/mpfr.h $(SYSROOT)/usr/local/include/mpfr.h),)
         HAVE_CGAL:=1
         CFLAGS_CGAL:= -DHAVE_CGAL $(call boost-libs-cflags,$(CGAL_REQ_BOOST_LIBS)) -Wno-deprecated-register
