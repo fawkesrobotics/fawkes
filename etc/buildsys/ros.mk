d7c2294fd04bce6bec1e945084baf2e9b7b785b1
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2015-10-09 10:09:01 +0200

    buildsys: add support for ROS hydro/indigo
    Support for fuerte is kept intact. No groovy anywhere, so stick with
    the older and newer versions.

--- etc/buildsys/ros.mk
+++ etc/buildsys/ros.mk
@@ -20,24 +20,42 @@ endif
 ifndef __buildsys_ros_mk_
 __buildsys_ros_mk_ := 1
 
+HAVE_ROSVERSION := $(if $(shell type -p rosversion; echo $${?/1/}),1,0)
+HAVE_PKGCONFIG := 
 
-ros-pkg-path = $(shell rospack find $(1))
+ifeq ($(HAVE_ROSVERSION), 1)
+  ROS_VERSION = $(shell rosversion -d)
 
-ros-pkg-msg-cflags = -I$(call ros-pkg-path,$(1))/msg_gen/cpp/include \
-		     -I$(call ros-pkg-path,$(1))/srv_gen/cpp/include
-ros-pkg-cflags   = $(call ros-pkg-msg-cflags,$(1)) \
+  ros-pkg-path = $(shell rospack find $(1))
+  ros-have-pkgs    = $(if $(strip $(subst 1,,$(foreach p,$1,$(call ros-have-pkg,$p)))),0,1)
+  ros-pkgs-cflags  = $(foreach p,$1,$(call ros-pkg-cflags,$p) )
+  ros-pkgs-lflags  = $(foreach p,$1,$(call ros-pkg-lflags,$p) )
+  ros-missing-pkgs = $(strip $(foreach p,$1,$(if $(subst 1,,$(call ros-have-pkg,$p)),$p )))
+
+  ifeq ($(ROS_VERSION),fuerte)
+    ros-have-pkg     = $(if $(shell rospack find $(1) 2>&1 >/dev/null; echo $${?/0/}),0,1)
+    ros-pkg-msg-cflags = -I$(call ros-pkg-path,$(1))/msg_gen/cpp/include \
+			 -I$(call ros-pkg-path,$(1))/srv_gen/cpp/include
+    ros-pkg-cflags   = $(call ros-pkg-msg-cflags,$(1)) \
 	$(shell rospack export --lang=cpp --attrib=cflags $(1) 2>/dev/null)
-ros-pkg-lflags   = $(shell rospack export --lang=cpp --attrib=lflags $(1) 2>/dev/null)
-ros-have-pkg     = $(if $(shell rospack find $(1) 2>&1 >/dev/null; echo $${?/0/}),0,1)
+    ros-pkg-lflags   = $(shell rospack export --lang=cpp --attrib=lflags $(1) 2>/dev/null)
+  else
+    # This has been tested with indigo and hydro, there is no groovy around to test
 
-ros-have-pkgs    = $(if $(strip $(subst 1,,$(foreach p,$1,$(call ros-have-pkg,$p)))),0,1)
-ros-pkgs-cflags  = $(foreach p,$1,$(call ros-pkg-cflags,$p) )
-ros-pkgs-lflags  = $(foreach p,$1,$(call ros-pkg-lflags,$p) )
-ros-missing-pkgs = $(strip $(foreach p,$1,$(if $(subst 1,,$(call ros-have-pkg,$p)),$p )))
+    ROS_BASE=$(firstword $(foreach p,$(call split,:,$(CMAKE_PREFIX_PATH)),$(patsubst %/.catkin,%,$(wildcard $p/.catkin))))
 
-HAVE_ROSPACK := $(if $(shell type -p rospack; echo $${?/1/}),1,0)
+    ifneq ($(PKGCONFIG),)
+      ifeq ($(findstring $(ROS_BASE),$(call split,:,$(PKG_CONFIG_PATH))),)
+        # That is essentially what roscd without argument is doing
+        export PKG_CONFIG_PATH   := $(ROS_BASE)/lib/pkgconfig$(if $(PKG_CONFIG_PATH),:$(PKG_CONFIG_PATH))
+      endif
+    endif
 
-ifeq ($(HAVE_ROSPACK), 1)
+    ros-have-pkg     = $(if $(shell $(PKGCONFIG) --exists $(1); echo $${?/1/}),1,0)
+    ros-pkg-cflags   = $(shell $(PKGCONFIG) --cflags $(1))
+    ros-pkg-lflags   = $(shell $(PKGCONFIG) --libs $(1))
+  endif
+
   HAVE_ROS = $(call ros-have-pkg,roscpp)
 else
   HAVE_ROS = 0
