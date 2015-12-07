1a280daf55afdb8190a6226e0720333f1f13109b
Victor Mataré <matare@lih.rwth-aachen.de>
2015-12-07 18:21:17 +0100

    robotino: prefer OpenRobotino V2 over V1 during build

--- src/plugins/robotino/robotino.mk
+++ src/plugins/robotino/robotino.mk
@@ -16,23 +16,22 @@
 OPENROBOTINOAPI_DIR=/usr/local/OpenRobotinoAPI
 OPENROBOTINOAPI2_DIR=/usr/local/robotino
 
-ifneq ($(wildcard $(OPENROBOTINOAPI_DIR)/1/include/robotinoapi_version.h),)
-  HAVE_OPENROBOTINO=1
-  OPENROBOTINO_API_VERSION = 1
-  CFLAGS_OPENROBOTINO  = -DHAVE_OPENROBOTINO_API_1 \
-			 -Wno-reorder -Wno-unused-function -Wno-delete-non-virtual-dtor \
-			 -I$(OPENROBOTINOAPI_DIR)/1/include -I$(OPENROBOTINOAPI_DIR)/share/include
-  LDFLAGS_OPENROBOTINO = -L$(OPENROBOTINOAPI_DIR)/1/lib/linux -L$(OPENROBOTINOAPI_DIR)/share/lib \
-			 -lrec_robotino_com -lrec_core_lt \
-			 -lrec_iocontrol_robotstate -lrec_iocontrol_remotestate
-
+ifneq ($(wildcard $(OPENROBOTINOAPI2_DIR)/api2/include/rec/robotino/api2/rec_robotino_api2_version.h),)
+HAVE_OPENROBOTINO=1
+OPENROBOTINO_API_VERSION = 2
+CFLAGS_OPENROBOTINO  = -DHAVE_OPENROBOTINO_API_2 \
+		   -I$(OPENROBOTINOAPI2_DIR)/api2/include -I$(OPENROBOTINOAPI2_DIR)/common/include
+LDFLAGS_OPENROBOTINO = -L$(OPENROBOTINOAPI2_DIR)/api2/lib -L$(OPENROBOTINOAPI2_DIR)/common/lib \
+		   -lrec_robotino_api2
 else
-  ifneq ($(wildcard $(OPENROBOTINOAPI2_DIR)/api2/include/rec/robotino/api2/rec_robotino_api2_version.h),)
-    HAVE_OPENROBOTINO=1
-    OPENROBOTINO_API_VERSION = 2
-    CFLAGS_OPENROBOTINO  = -DHAVE_OPENROBOTINO_API_2 \
-			   -I$(OPENROBOTINOAPI2_DIR)/api2/include -I$(OPENROBOTINOAPI2_DIR)/common/include
-    LDFLAGS_OPENROBOTINO = -L$(OPENROBOTINOAPI2_DIR)/api2/lib -L$(OPENROBOTINOAPI2_DIR)/common/lib \
-			   -lrec_robotino_api2
+  ifneq ($(wildcard $(OPENROBOTINOAPI_DIR)/1/include/robotinoapi_version.h),)
+	HAVE_OPENROBOTINO=1
+	OPENROBOTINO_API_VERSION = 1
+	CFLAGS_OPENROBOTINO  = -DHAVE_OPENROBOTINO_API_1 \
+			   -Wno-reorder -Wno-unused-function -Wno-delete-non-virtual-dtor \
+			   -I$(OPENROBOTINOAPI_DIR)/1/include -I$(OPENROBOTINOAPI_DIR)/share/include
+	LDFLAGS_OPENROBOTINO = -L$(OPENROBOTINOAPI_DIR)/1/lib/linux -L$(OPENROBOTINOAPI_DIR)/share/lib \
+			   -lrec_robotino_com -lrec_core_lt \
+			   -lrec_iocontrol_robotstate -lrec_iocontrol_remotestate
   endif
 endif
