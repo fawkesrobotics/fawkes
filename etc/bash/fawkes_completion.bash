0685876efce394c6c5aef7bd0894e1e5833e2546
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2016-07-02 22:57:25 +0200

    bash completion: add completion for meta plugins
    We parse the yaml config and look read the meta plugins from the config.
    The yaml parse is quite simple:
    - it expects meta plugins to be defined in cfg/conf.d/meta_plugins.yaml,
    - it can only parse a subset of yaml,
    - it doesn't allow any other config values in the same config file;
      these will be misinterpreted as meta plugins.

--- etc/bash/fawkes_completion.bash
+++ etc/bash/fawkes_completion.bash
@@ -1,3 +1,20 @@
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])}
+         printf("%s%s%s ", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
 _fawkes()
 {
   local cur prev opts base
@@ -26,7 +43,9 @@ _fawkes()
   esac
   local available_plugins=$(basename -s .so\
     $(ls  $(dirname ${COMP_WORDS[0]/#\~/$HOME})/../plugins))
-  COMPREPLY=( $(compgen -W "${available_plugins}" -- ${cur}) )
+  local meta_plugins=$(parse_yaml $(dirname\
+    ${COMP_WORDS[0]/#\~/$HOME})/../cfg/conf.d/meta_plugins.yaml)
+  COMPREPLY=( $(compgen -W "${available_plugins} ${meta_plugins}" -- ${cur}) )
   return 0
 }
 complete -F _fawkes fawkes
