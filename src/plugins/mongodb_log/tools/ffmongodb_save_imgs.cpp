1a0192d280d74f3e5219ed9432d6c81b77a9f13f
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2016-09-05 13:31:23 +0200

    mongodb_log: only replace auto_ptrs if we use at least C++11
    Replacing auto_ptrs with unique_ptrs or shared_ptrs is only possible
    with C++11 because earlier versions don't support the new pointer types.

--- src/plugins/mongodb_log/tools/ffmongodb_save_imgs.cpp
+++ src/plugins/mongodb_log/tools/ffmongodb_save_imgs.cpp
@@ -136,8 +136,13 @@ main(int argc, char **argv)
 	.sort("timestamp", 1);
     }
 
+#if __cplusplus >= 201103L
     std::unique_ptr<mongo::DBClientCursor> cursor =
       mongodb_client->query(query_coll, q);
+#else
+    std::auto_ptr<mongo::DBClientCursor> cursor =
+      mongodb_client->query(query_coll, q);
+#endif
 
     while (cursor->more()) {
       BSONObj doc = cursor->next();
