cccd37c6fe2e6551b5672569b15f4dd2bfb06d01
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2016-09-06 18:35:08 +0200

    plugin_generator: add missing _ to deflector

--- src/tools/plugin_generator/plugin_generator.cpp
+++ src/tools/plugin_generator/plugin_generator.cpp
@@ -74,7 +74,7 @@ PluginGenerator::PluginGenerator(std::string directory,
   _class_name_thread = format_class_name(_plugin_name_underscore, "Thread");
   _class_name_plugin = format_class_name(_plugin_name_underscore, "Plugin");
 
-  _deflector = "__PLUGINS_" + fawkes::StringConversions::to_upper(_plugin_name_underscore) + "THREAD_H_";
+  _deflector = "__PLUGINS_" + fawkes::StringConversions::to_upper(_plugin_name_underscore) + "_THREAD_H_";
 }
 
 
