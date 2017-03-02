72fff933cce040cdb22c7bcc4cc774b188959478
Bjoern Schaepers <bjoern.schaepers@rwth-aachen.de>
2017-03-02 11:26:38 +0100

    joystick: fix logging warnings
    There are only 8 axes and correctly 8 arguments, but 9 placeholders.
    Remove the last one to fix this.

--- src/plugins/joystick/ffjoystick.cpp
+++ src/plugins/joystick/ffjoystick.cpp
@@ -146,7 +146,7 @@ class JoystickBlackBoardLogger
       __joystick_if->read();
       float *axis_value = __joystick_if->axis();
       __logger->log_info("Joystick", "Axes:    0: %f  1: %f  2: %f  3: %f  4: %f  "
-			 "5: %f  6: %f  7: %f  8: %f",
+			 "5: %f  6: %f  7: %f",
 			 axis_value[0], axis_value[1],
 			 axis_value[2], axis_value[3],
 			 axis_value[4], axis_value[5],
