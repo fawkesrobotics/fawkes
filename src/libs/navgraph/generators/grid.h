c5f3dfab70ad7a54e8e82484fcab76a0ff817464
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2017-04-24 14:43:43 +0200

    libnavgraph: add grid-based navgraph generator
    The new generator generates an even-spaced grid within the given
    bounding box. It calculates the grid line segments and their
    intersections marking the center points of the grid cells. These are
    then filtered by nearby obstacles.
    
    The possible parameters are spacing (grid cell size) and margin (minimum
    distance of edges to obstacles).

---
+++ src/libs/navgraph/generators/grid.h
@@ -0,0 +1,51 @@
+
+/***************************************************************************
+ *  grid.h - generate navgraph based on a grid
+ *
+ *  Created: Sun Apr 23 18:22:09 2017
+ *  Copyright  2015-2017  Tim Niemueller [www.niemueller.de]
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version. A runtime exception applies to
+ *  this software (see LICENSE.GPL_WRE file mentioned below for details).
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
+ */
+
+#ifndef __LIBS_NAVGRAPH_GENERATOR_GRID_H_
+#define __LIBS_NAVGRAPH_GENERATOR_GRID_H_
+
+#include <navgraph/navgraph.h>
+#include <navgraph/generators/generator.h>
+
+#include <utils/math/polygon.h>
+
+namespace fawkes {
+#if 0 /* just to make Emacs auto-indent happy */
+}
+#endif
+
+class NavGraphGeneratorGrid : public NavGraphGenerator
+{
+ public:
+	NavGraphGeneratorGrid(const std::map<std::string, std::string> &params);
+	virtual ~NavGraphGeneratorGrid();
+
+	virtual void compute(fawkes::LockPtr<fawkes::NavGraph> graph);
+
+ private:
+	float spacing_;
+	float margin_;
+};
+
+} // end of namespace fawkes
+
+#endif
