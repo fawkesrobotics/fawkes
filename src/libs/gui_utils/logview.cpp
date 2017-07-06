cd3039346c853c714c2a08ab054752ab2b5684d9
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2017-07-06 10:09:15 +0200

    gui_utils: adapt to NetworkLogger move

--- src/libs/gui_utils/logview.cpp
+++ src/libs/gui_utils/logview.cpp
@@ -24,7 +24,7 @@
 #include <gui_utils/logview.h>
 #include <gui_utils/connection_dispatcher.h>
 #include <netcomm/fawkes/client.h>
-#include <logging/network.h>
+#include <network_logger/network_logger.h>
 
 #include <gtkmm.h>
 
