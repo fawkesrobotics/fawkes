deb5507b2e242ef2a21a237561e6453d4475f47c
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2017-11-28 14:50:39 +0100

    pddl parser: fix license headers
    Not sure what the actual problem was, but fix the license headers so
    the license check will not complain anymore.

--- src/libs/pddl_parser/pddl_parser.h
+++ src/libs/pddl_parser/pddl_parser.h
@@ -5,18 +5,18 @@
  *  Created: Fri 19 May 2017 11:10:30 CEST
  *  Copyright  2017  Matthias Loebach     
  ****************************************************************************/     
-    
-/*  This program is free software; you can redistribute it and/or modify     
- *  it under the terms of the GNU General Public License as published by     
- *  the Free Software Foundation; either version 2 of the License, or     
- *  (at your option) any later version.     
- *     
- *  This program is distributed in the hope that it will be useful,     
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of     
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     
- *  GNU Library General Public License for more details.     
- *     
- *  Read the full text in the LICENSE.GPL file in the doc directory.     
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
 #ifndef __PLUGINS_PDDL_PARSER_H
