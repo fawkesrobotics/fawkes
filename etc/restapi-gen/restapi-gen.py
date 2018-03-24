a4b2f6de3147d7aa83370f327fe496efbe914cfd
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-03-24 22:45:35 +0100

    restapi-gen: add a few more filters useful for API generation

--- etc/restapi-gen/restapi-gen.py
+++ etc/restapi-gen/restapi-gen.py
@@ -21,11 +21,38 @@
 # SOFTWARE.
 
 import os
+import re
 import sys
 import yaml
 import argparse
 from pprint import pprint
 import jinja2
+from jinja2.ext import Extension
+from jinja2.exceptions import TemplateRuntimeError
+
+# Based on https://github.com/duelafn/python-jinja2-apci/blob/master/jinja2_apci/error.py
+class RaiseExtension(Extension):
+    # This is our keyword(s):
+    tags = set(['raise'])
+
+    # See also: jinja2.parser.parse_include()
+    def parse(self, parser):
+        # the first token is the token that started the tag. In our case we
+        # only listen to "raise" so this will be a name token with
+        # "raise" as value. We get the line number so that we can give
+        # that line number to the nodes we insert.
+        lineno = next(parser.stream).lineno
+
+        # Extract the message from the template
+        message_node = parser.parse_expression()
+
+        return jinja2.nodes.CallBlock(
+            self.call_method('_raise', [message_node], lineno=lineno),
+            [], [], [], lineno=lineno
+        )
+
+    def _raise(self, msg, caller):
+        raise TemplateRuntimeError(msg)
 
 def filter_reftype(value):
 	parts = value.split('/')
@@ -101,6 +128,85 @@ def filter_transitive_types(type, all_schemas):
 	recursive_transitive_types(types, type, all_schemas)
 	return sorted(types)
 
+def filter_path_reftypes(spec):
+	rv = set()
+	for p in spec['paths']:
+		for method in spec['paths'][p]:
+			for code in spec['paths'][p][method]['responses']:
+				if 'content' in spec['paths'][p][method]['responses'][code]:
+					content = spec['paths'][p][method]['responses'][code]['content']
+					if 'application/json' in content:
+						schema = content['application/json']['schema']
+						if 'type' in schema and schema['type'] == 'array':
+							if '$ref' in schema['items']:
+								rv.add(filter_reftype(schema['items']['$ref']))
+							else:
+								raise TemplateRuntimeError("Arrays as response types are only supported "+
+								                           "for referenced types (%s)" % p)
+						elif '$ref' in schema:
+							rv.add(filter_reftype(schema['$ref']))
+						else:
+							raise TemplateRuntimeError("Only referenced schemas are supported "
+							                           "as response types (%s %s %s)" % (p, method, code))
+
+	return sorted(rv)
+
+def filter_path_args(op, only_in=''):
+	rv = []
+	if 'parameters' in op:
+		for p in op['parameters']:
+			if 'schema' not in p:
+				raise TemplateRuntimeError("Parameter requires schema "+
+				                           "(%s of %s)" % (p.name, op.operationId))
+
+			required = ('required' in p and p['required'])
+
+			if p['in'] in ['path', 'query'] and (only_in == '' or p['in'] == only_in):
+				if 'type' in p['schema'] \
+				   and p['schema']['type'] in ['integer', 'number', 'string', 'boolean']:
+
+					rv.append({"name": p['name'],
+					           "type": p['schema']['type'],
+					           "required": required})
+				else:
+					raise TemplateRuntimeError("Only primitive types are supported "+
+					                           "for path args (%s of %s)" % (p.name, op.operationId))
+			elif p['in'] == 'body' and (only_in == '' or p['in'] == only_in):
+				if '$ref' in  p['schema']:
+					rv.append({"name": p['name'],
+					           "type": filter_reftype(p['schema']['$ref']),
+					           "required": required})
+				else:
+					# We can do better here, generating appropriate objects
+					rv.append({"name": p['name'], "type": "any", "required": required})
+	return rv
+
+def filter_path_substargs(path, op, replacement_pattern):
+	if 'parameters' in op:
+		for p in op['parameters']:
+			if p['in'] == 'path':
+				replace_by = replacement_pattern.replace('$$', filter_sanitize(p['name']))
+				path = path.replace('{%s}' % p['name'], replace_by)
+	return path	
+
+def filter_regex_replace(value='', pattern='', replacement=''):
+    _re = re.compile(pattern)
+    return _re.sub(replacement, value)
+
+def write_file(filespec, res):
+	if res is not None:
+		if args.stdout:
+			print(res)
+		elif args.dry_run:
+			print("Generated %s/%s.%s successfully (not writing, dry-run)" %
+			      (args.output_dir, name, f['suffix']))
+		else:
+			if os.path.lexists(f['filename']) and not args.force:
+				print("File %s already exists" % f['filename'])
+				sys.exit(-2)
+			with open(f['filename'], "w") as output_file:
+				output_file.write(res)
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Run RCLL Cluster Sim Jobs')
 	parser.add_argument('--api', '-a', metavar="FILE", required=True,
@@ -122,6 +228,7 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(args.template_dir),
+	                           extensions=[RaiseExtension],
 	                           autoescape=False,
 	                           line_statement_prefix = '%');
 
@@ -132,6 +239,10 @@ if __name__ == '__main__':
 	jinja.filters['prefix']           = filter_prefix
 	jinja.filters['rstrip']           = filter_rstrip
 	jinja.filters['transitive_types'] = filter_transitive_types
+	jinja.filters['path_reftypes']    = filter_path_reftypes
+	jinja.filters['path_args']        = filter_path_args
+	jinja.filters['path_substargs']   = filter_path_substargs
+	jinja.filters['regex_replace']    = filter_regex_replace
 
 	files = []
 	for f in args.file:
