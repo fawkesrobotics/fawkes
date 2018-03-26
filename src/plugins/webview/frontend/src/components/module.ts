9842751c250d9060e8f3b40c1f623bb1b4692846
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-03-26 22:05:00 +0200

    webview-frontend: add component to render dot graphs
    The component takes a dot graph description and outputs an SVG where it
    is included. Uses viz.js (a graphviz version compiled with asm.js).

--- src/plugins/webview/frontend/src/components/module.ts
+++ src/plugins/webview/frontend/src/components/module.ts
@@ -2,23 +2,27 @@
 // Copyright  2018  Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
 // License: Apache 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {SharedModule} from '../shared.module';
+import { SharedModule } from '../shared.module';
+import { PipesModule } from '../pipes/module';
 
 import { PropertyComponent } from './property/component';
 import { HSpaceComponent } from './hspace/component';
 import { ListZeroStateComponent } from './zerostate/component';
+import { DotGraphComponent } from './dotgraph/component';
 
 const COMPONENTS = [
   PropertyComponent,
   HSpaceComponent,
   ListZeroStateComponent,
+  DotGraphComponent,
 ];
 
 @NgModule({
   imports: [
     SharedModule,
+    PipesModule,
   ],
   declarations: COMPONENTS,
   exports: COMPONENTS
