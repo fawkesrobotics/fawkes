99ba26887101af5671249b6f7597d11bf67eedb3
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-03-28 23:51:25 +0200

    webview-frontend: property component supports vertical
    Add a direction parameter to the property component to allow for better
    styling depending on the principal expected direction for the property.

--- src/plugins/webview/frontend/src/components/property/component.ts
+++ src/plugins/webview/frontend/src/components/property/component.ts
@@ -3,11 +3,13 @@
 // Copyright  2017  The Kubernetes Authors
 // License: Apache 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'ff-property',
   templateUrl: './template.html',
   styleUrls: ['./style.scss'],
 })
-export class PropertyComponent {}
+export class PropertyComponent {
+  @Input() direction?: string = "horizontal";
+}
