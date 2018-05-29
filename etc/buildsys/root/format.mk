50eb4313bf415df5b145f7499f5afb772fccfa55
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-05-29 11:36:39 +0200

    buildsys: add format-emacs target
    This calls Emacs in batch mode to format files. Note, however, that this
    does not produce the exact same style as clang-format. It is informative
    and to enable tuning of the Emacs style to more closely match the
    clang-format mandated style (which is authoritative).
    
    This contains a minimal Emacs configuration to configure code
    formatting. Some more effort is necessary to more closely align Emacs
    and clang-format.
    
    Using Emacs is an order of magnitude slower than clang-format.

--- etc/buildsys/root/format.mk
+++ etc/buildsys/root/format.mk
@@ -35,6 +35,24 @@ format: check-parallel
     exit 1; \
   fi
 
+.PHONY: format-emacs
+format-emacs: check-parallel
+	$(SILENT) echo -e "$(INDENT_PRINT)[FMT] Formatting (emacs) where necessary"
+	$(SILENT) echo -e "$(INDENT_PRINT)[FMT] $(TYELLOW)Use 'format' target to use clang-format$(TNORMAL)"
+	$(SILENTSYMB)if type -p emacs >/dev/null; then \
+		ALL_FILES=$$(git ls-files *.{h,cpp}); \
+		if type -p parallel >/dev/null; then \
+			export FAWKES_BASEDIR=$(abspath $(FAWKES_BASEDIR)); \
+			parallel -u --will-cite --bar --results /tmp/emacs-format -- emacs -q --batch {} -l $(abspath $(FAWKES_BASEDIR))/etc/format-scripts/emacs-format-cpp.el -f format-code ::: $$ALL_FILES; \
+			rm -rf /tmp/emacs-format; \
+		else \
+			echo "$$ALL_FILES" | xargs -P$$(nproc) -n1 -I '{}' -- emacs -q --batch {} -l $(abspath $(FAWKES_BASEDIR))/etc/format-scripts/emacs-format-cpp.el -f format-code; \
+		fi; \
+	else \
+		echo -e "$(INDENT_PRINT)$(TRED)--- Cannot format code$(TNORMAL) (emacs not found)"; \
+    exit 1; \
+  fi
+
 # Using clang-format is the default, faster and better results
 .PHONY: format
 format: format-clang
