64c3360c01638ffa9dc0645e2e4ff1eba6ae46da
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-10-29 17:43:02 +0100

    mongodb: remove double underscore in header guards
    Automatically performed using:
    for f in $(git grep __ src/ | awk -F: '{ print $1 }' | sort -u); do
      sed -i -e '/\(#define\|#ifndef\) /'
    '{/__\(STDC_LIMIT_MACROS\|format\|attribute\|INTERFACE_TYPE_SIZE\|'
    'INTERFACE_HASH_SIZE\|INTERFACE_ID_SIZE\|INTERFACE_UID_SIZE\)/!'
    's/__\([^][:space:];[(){}.,"+*:-]\+\)_H_/_\1_H_/g}' $f;
    done
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/mongodb/aspect/mongodb_inifin.h
+++ src/plugins/mongodb/aspect/mongodb_inifin.h
@@ -20,8 +20,8 @@
  *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
  */
 
-#ifndef __PLUGINS_MONGODB_ASPECT_MONGODB_INIFIN_H_
-#define __PLUGINS_MONGODB_ASPECT_MONGODB_INIFIN_H_
+#ifndef _PLUGINS_MONGODB_ASPECT_MONGODB_INIFIN_H_
+#define _PLUGINS_MONGODB_ASPECT_MONGODB_INIFIN_H_
 
 #include <aspect/inifins/inifin.h>
 
--- src/plugins/mongodb/mongodb_thread.h
+++ src/plugins/mongodb/mongodb_thread.h
@@ -19,8 +19,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef __PLUGINS_MONGODB_MONGODB_THREAD_H_
-#define __PLUGINS_MONGODB_MONGODB_THREAD_H_
+#ifndef _PLUGINS_MONGODB_MONGODB_THREAD_H_
+#define _PLUGINS_MONGODB_MONGODB_THREAD_H_
 
 #include <plugins/mongodb/aspect/mongodb_conncreator.h>
 #include <plugins/mongodb/aspect/mongodb_inifin.h>
