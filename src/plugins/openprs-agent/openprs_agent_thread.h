278650466d5390377a252570b6e61bb4884a32a7
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-10-29 17:43:03 +0100

    openprs-agent: remove double underscore in header guards
    Automatically performed using:
    for f in $(git grep __ src/ | awk -F: '{ print $1 }' | sort -u); do
      sed -i -e '/\(#define\|#ifndef\) /'
    '{/__\(STDC_LIMIT_MACROS\|format\|attribute\|INTERFACE_TYPE_SIZE\|'
    'INTERFACE_HASH_SIZE\|INTERFACE_ID_SIZE\|INTERFACE_UID_SIZE\)/!'
    's/__\([^][:space:];[(){}.,"+*:-]\+\)_H_/_\1_H_/g}' $f;
    done
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/openprs-agent/openprs_agent_thread.h
+++ src/plugins/openprs-agent/openprs_agent_thread.h
@@ -19,8 +19,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef __PLUGINS_OPENPRS_AGENT_OPENPRS_AGENT_THREAD_H_
-#define __PLUGINS_OPENPRS_AGENT_OPENPRS_AGENT_THREAD_H_
+#ifndef _PLUGINS_OPENPRS_AGENT_OPENPRS_AGENT_THREAD_H_
+#define _PLUGINS_OPENPRS_AGENT_OPENPRS_AGENT_THREAD_H_
 
 #include <core/threading/thread.h>
 #include <aspect/logging.h>
