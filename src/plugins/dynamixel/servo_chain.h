fc71a879dd2f9a7fcd15a826ec25a85561d8e426
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-10-29 17:42:59 +0100

    dynamixel: remove double underscore in header guards
    Automatically performed using:
    for f in $(git grep __ src/ | awk -F: '{ print $1 }' | sort -u); do
      sed -i -e '/\(#define\|#ifndef\) /'
    '{/__\(STDC_LIMIT_MACROS\|format\|attribute\|INTERFACE_TYPE_SIZE\|'
    'INTERFACE_HASH_SIZE\|INTERFACE_ID_SIZE\|INTERFACE_UID_SIZE\)/!'
    's/__\([^][:space:];[(){}.,"+*:-]\+\)_H_/_\1_H_/g}' $f;
    done
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/dynamixel/servo_chain.h
+++ src/plugins/dynamixel/servo_chain.h
@@ -20,8 +20,8 @@
  *  Read the full text in the LICENSE.GPL_WRE file in the doc directory.
  */
 
-#ifndef __PLUGINS_DYNAMIXEL_SERVO_CHAIN_H_
-#define __PLUGINS_DYNAMIXEL_SERVO_CHAIN_H_
+#ifndef _PLUGINS_DYNAMIXEL_SERVO_CHAIN_H_
+#define _PLUGINS_DYNAMIXEL_SERVO_CHAIN_H_
 
 #include <core/exception.h>
 
