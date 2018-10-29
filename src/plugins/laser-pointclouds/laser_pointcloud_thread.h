6ae88976caca9aeaba599630aebe642ae7286e0d
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-10-29 17:43:01 +0100

    laser-pointclouds: remove double underscore in header guards
    Automatically performed using:
    for f in $(git grep __ src/ | awk -F: '{ print $1 }' | sort -u); do
      sed -i -e '/\(#define\|#ifndef\) /'
    '{/__\(STDC_LIMIT_MACROS\|format\|attribute\|INTERFACE_TYPE_SIZE\|'
    'INTERFACE_HASH_SIZE\|INTERFACE_ID_SIZE\|INTERFACE_UID_SIZE\)/!'
    's/__\([^][:space:];[(){}.,"+*:-]\+\)_H_/_\1_H_/g}' $f;
    done
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/laser-pointclouds/laser_pointcloud_thread.h
+++ src/plugins/laser-pointclouds/laser_pointcloud_thread.h
@@ -19,8 +19,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef __PLUGINS_LASER_POINTCLOUDS_LASER_POINTCLOUD_THREAD_H_
-#define __PLUGINS_LASER_POINTCLOUDS_LASER_POINTCLOUD_THREAD_H_
+#ifndef _PLUGINS_LASER_POINTCLOUDS_LASER_POINTCLOUD_THREAD_H_
+#define _PLUGINS_LASER_POINTCLOUDS_LASER_POINTCLOUD_THREAD_H_
 
 // must be first for reliable ROS detection
 #include <pcl/point_cloud.h>
