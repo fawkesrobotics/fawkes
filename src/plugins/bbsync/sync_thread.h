623fb541a4d3ded7fcc6ef60d9df41afc89d03d0
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-10-29 17:42:58 +0100

    bbsync: remove double underscore in header guards
    Automatically performed using:
    for f in $(git grep __ src/ | awk -F: '{ print $1 }' | sort -u); do
      sed -i -e '/\(#define\|#ifndef\) /'
    '{/__\(STDC_LIMIT_MACROS\|format\|attribute\|INTERFACE_TYPE_SIZE\|'
    'INTERFACE_HASH_SIZE\|INTERFACE_ID_SIZE\|INTERFACE_UID_SIZE\)/!'
    's/__\([^][:space:];[(){}.,"+*:-]\+\)_H_/_\1_H_/g}' $f;
    done
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/bbsync/sync_thread.h
+++ src/plugins/bbsync/sync_thread.h
@@ -20,8 +20,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef __PLUGINS_BBSYNC_SYNC_THREAD_H_
-#define __PLUGINS_BBSYNC_SYNC_THREAD_H_
+#ifndef _PLUGINS_BBSYNC_SYNC_THREAD_H_
+#define _PLUGINS_BBSYNC_SYNC_THREAD_H_
 
 #include "sync_listener.h"
 #include "writer_listener.h"
