8f7756d2346b7a503acc48109bf4478a5bdc896f
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-10-29 17:43:05 +0100

    static_transforms: remove double underscore in header guards
    Automatically performed using:
    for f in $(git grep __ src/ | awk -F: '{ print $1 }' | sort -u); do
      sed -i -e '/\(#define\|#ifndef\) /'
    '{/__\(STDC_LIMIT_MACROS\|format\|attribute\|INTERFACE_TYPE_SIZE\|'
    'INTERFACE_HASH_SIZE\|INTERFACE_ID_SIZE\|INTERFACE_UID_SIZE\)/!'
    's/__\([^][:space:];[(){}.,"+*:-]\+\)_H_/_\1_H_/g}' $f;
    done
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/static_transforms/static_transforms_thread.h
+++ src/plugins/static_transforms/static_transforms_thread.h
@@ -20,8 +20,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef __PLUGINS_STATIC_TRANSFORMS_STATIC_TRANSFORMS_THREAD_H_
-#define __PLUGINS_STATIC_TRANSFORMS_STATIC_TRANSFORMS_THREAD_H_
+#ifndef _PLUGINS_STATIC_TRANSFORMS_STATIC_TRANSFORMS_THREAD_H_
+#define _PLUGINS_STATIC_TRANSFORMS_STATIC_TRANSFORMS_THREAD_H_
 
 #include <core/threading/thread.h>
 #include <aspect/blocked_timing.h>
