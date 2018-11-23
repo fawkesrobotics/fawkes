b87e30712f68cff138c7f80685fb55da2cd6aa53
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-11-23 22:23:29 +0100

    bash: add shebang and license to bash completion

--- etc/bash/fawkes_completion.bash
+++ etc/bash/fawkes_completion.bash
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+##########################################################################
+#  fawkes_completion.bash - bash completion integration
+#
+#  Created: Sat Jul 2 22:29:23 2016 +0200
+#  Copyright  2016  Till Hofmann
+##########################################################################
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  Read the full text in the LICENSE.GPL file in the doc directory.
+
 function parse_yaml {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
