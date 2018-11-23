f47c54fb5cd9f2094f92708025e1f9b6e07dc371
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-11-23 23:11:15 +0100

    gazsim-comm: fix invalid iterator comparison
    Along the way some formatting fixes.
    
    Detected by https://www.codacy.com.

--- src/plugins/gazebo/gazsim-comm/gazsim_comm_thread.cpp
+++ src/plugins/gazebo/gazsim-comm/gazsim_comm_thread.cpp
@@ -66,11 +66,11 @@ GazsimCommThread::init()
   recv_ports_crypto1_ = config->get_uints("/gazsim/comm/recv-ports-crypto1");
   send_ports_crypto2_ = config->get_uints("/gazsim/comm/send-ports-crypto2");
   recv_ports_crypto2_ = config->get_uints("/gazsim/comm/recv-ports-crypto2");
-  if(addresses_.size() != send_ports_.size() || addresses_.size() != recv_ports_.size()
-     || ( use_crypto1_ && addresses_.size() != send_ports_crypto1_.size())
-     || ( use_crypto1_ && addresses_.size() != recv_ports_crypto1_.size())
-     || ( use_crypto2_ && addresses_.size() != send_ports_crypto2_.size())
-     || ( use_crypto2_ && addresses_.size() != recv_ports_crypto2_.size()))
+  if (addresses_.size() != send_ports_.size() || addresses_.size() != recv_ports_.size()
+      || ( use_crypto1_ && addresses_.size() != send_ports_crypto1_.size())
+      || ( use_crypto1_ && addresses_.size() != recv_ports_crypto1_.size())
+      || ( use_crypto2_ && addresses_.size() != send_ports_crypto2_.size())
+      || ( use_crypto2_ && addresses_.size() != recv_ports_crypto2_.size()))
   {
     logger->log_warn(name(), "/gazsim/comm/ has an invalid configuration!");
   }
@@ -107,20 +107,20 @@ GazsimCommThread::init()
   peers_.resize(addresses_.size());
   peers_crypto1_.resize(addresses_.size());
   peers_crypto2_.resize(addresses_.size());
-  for(unsigned int i = 0; i < addresses_.size(); i++)
+  for (unsigned int i = 0; i < addresses_.size(); i++)
   {
     peers_[i] = new ProtobufBroadcastPeer(addresses_[i], send_ports_[i],
 					  recv_ports_[i], proto_dirs_);
     peers_[i]->signal_received_raw().connect(boost::bind(&GazsimCommThread::receive_raw_msg, this, _1, _2, _3, _4));
     peers_[i]->signal_send_error().connect(boost::bind(&GazsimCommThread::peer_send_error, this, addresses_[i], send_ports_[i], _1));
-    if(use_crypto1_)
+    if (use_crypto1_)
     {
       peers_crypto1_[i] = new ProtobufBroadcastPeer(addresses_[i], send_ports_crypto1_[i],
 					  recv_ports_crypto1_[i], proto_dirs_);
       peers_crypto1_[i]->signal_received_raw().connect(boost::bind(&GazsimCommThread::receive_raw_msg, this, _1, _2, _3, _4));
       peers_crypto1_[i]->signal_send_error().connect(boost::bind(&GazsimCommThread::peer_send_error, this, addresses_[i], send_ports_crypto1_[i], _1));
     }
-    if(use_crypto2_)
+    if (use_crypto2_)
     {
       peers_crypto2_[i] = new ProtobufBroadcastPeer(addresses_[i], send_ports_crypto2_[i],
 						    recv_ports_crypto2_[i], proto_dirs_);
@@ -135,7 +135,7 @@ GazsimCommThread::init()
 void
 GazsimCommThread::finalize()
 {
-  for(unsigned int i = 0; i < peers_.size(); i++)
+  for (unsigned int i = 0; i < peers_.size(); i++)
   {
     delete peers_[i];
   }
@@ -162,14 +162,14 @@ GazsimCommThread::receive_raw_msg(boost::asio::ip::udp::endpoint &endpoint,
   //logger->log_info(name(), "Got raw Message from port %d", endpoint.port());
   unsigned int incoming_peer_port = endpoint.port(); //this is suprisingly the send-port
  
-  if(!initialized_)
+  if (!initialized_)
   {
     return;
   }
 
   //simulate package loss
   double rnd = ((double) rand()) / ((double) RAND_MAX); //0.0 <= rnd <= 1.0
-  if(rnd < package_loss_)
+  if (rnd < package_loss_)
   {
     return;
   }
@@ -177,26 +177,30 @@ GazsimCommThread::receive_raw_msg(boost::asio::ip::udp::endpoint &endpoint,
   //check which set of peers the message comes from
   std::vector<protobuf_comm::ProtobufBroadcastPeer*> peers;
   std::vector<unsigned int> send_ports;
-  if(std::find(send_ports_.begin(), send_ports_.end(), incoming_peer_port) != send_ports_.end())
+  if (std::find(send_ports_.begin(), send_ports_.end(), incoming_peer_port) != send_ports_.end())
   {
     peers = peers_;
     send_ports = send_ports_;
   }
-  else if(use_crypto1_ && std::find(send_ports_crypto1_.begin(), send_ports_crypto1_.end(), incoming_peer_port) != send_ports_crypto1_.end())
+  else if (use_crypto1_ &&
+           std::find(send_ports_crypto1_.begin(),
+                     send_ports_crypto1_.end(), incoming_peer_port) != send_ports_crypto1_.end())
   {
     peers = peers_crypto1_;
     send_ports = send_ports_crypto1_;
   }  
-  else if(use_crypto2_ && std::find(send_ports_crypto2_.begin(), send_ports_crypto2_.end(), incoming_peer_port) != send_ports_crypto1_.end())
+  else if (use_crypto2_ &&
+           std::find(send_ports_crypto2_.begin(),
+                     send_ports_crypto2_.end(), incoming_peer_port) != send_ports_crypto2_.end())
   {
     peers = peers_crypto2_;
     send_ports = send_ports_crypto2_;
   }
 
   //send message to all other peers
-  for(unsigned int i = 0; i < peers.size(); i++)
+  for (unsigned int i = 0; i < peers.size(); i++)
   {
-    if(send_ports[i] != incoming_peer_port)
+    if (send_ports[i] != incoming_peer_port)
     {
       peers[i]->send_raw(header, data, length);
     }
