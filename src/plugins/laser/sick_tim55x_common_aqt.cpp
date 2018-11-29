fawkesrobotics/fawkes

commit 3765f3135f30314fbbe841b6d0f44c6eccb22daf

laser: fix format type mismatch
Detected by https://www.codacy.com.

src/plugins/laser/sick_tim55x_common_aqt.cpp
@@ -302,7 +302,7 @@ SickTiM55xCommonAcquisitionThread::parse_datagram(const unsigned char *datagram,
 
   // 22: Scaling offset (00000000) -- always 0
   // 23: Starting angle (FFF92230)
-  int starting_angle_val = -1;
+  unsigned int starting_angle_val = 0;
   sscanf(fields[23].c_str(), "%x", &starting_angle_val);
   float angle_min = (starting_angle_val / 10000.0) / 180.0 * M_PI - M_PI / 2;
 

commit 32c7438f89f053f483d48b1b97a9d8d897c731ec

laser: fix type error
This was introduced in 3765f31 when fixing a Codacy error. This was
raised for good reason: we are using sscanf() to read a "%x" format. The
documentation (man sscanf) states that this must be read into an
unsigned integer. The Sick TiM protocol documentation indeed states the
type "Uint_32" for the starting angle field, however, the range is
declared as -450000d to +2250000d.

So we need to read an unsigned int and then interpret it as a signed
integer to be compliant with sscanf and the device protocol.

Thanks to @vmatare for reporting this issue.

src/plugins/laser/sick_tim55x_common_aqt.cpp
@@ -302,8 +302,9 @@ SickTiM55xCommonAcquisitionThread::parse_datagram(const unsigned char *datagram,
 
   // 22: Scaling offset (00000000) -- always 0
   // 23: Starting angle (FFF92230)
-  unsigned int starting_angle_val = 0;
-  sscanf(fields[23].c_str(), "%x", &starting_angle_val);
+  unsigned int starting_angle_hexval = 0;
+  sscanf(fields[23].c_str(), "%x", &starting_angle_hexval);
+  int starting_angle_val = static_cast<int>(starting_angle_hexval);
   float angle_min = (starting_angle_val / 10000.0) / 180.0 * M_PI - M_PI / 2;
 
   // 24: Angular step width (2710)