fc272b90c288d56c97ac1d6bc1cd228e99a4b5a6
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2018-12-03 11:47:49 +0100

    ros-joint: make plugin constructor explicit
    Automatically performed using:
    for f in $(git grep --name-only '.*Plugin(Configuration' | sort -u); do
      sed -e 's/\(\s\)\([a-zA-Z0-9]\+Plugin(Configuration\)/\1explicit \2/' -i $f
    done
    
    Committed using:
    for p in $SELECTED_PLUGINS; do
      cat <<-EOM | git commit -F- $p
        THIS MESSAGE
      EOM
    done

--- src/plugins/ros/joint_plugin.cpp
+++ src/plugins/ros/joint_plugin.cpp
@@ -35,7 +35,7 @@ class RosJointPlugin : public fawkes::Plugin
   /** Constructor.
    * @param config Fawkes configuration
    */
-  RosJointPlugin(Configuration *config)
+  explicit RosJointPlugin(Configuration *config)
     : Plugin(config)
   {
     thread_list.push_back(new RosJointThread());
