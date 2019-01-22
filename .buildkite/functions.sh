0ceb05ab5758ecabbb19c22f5dee2b797ba8a573
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-01-22 13:10:25 +0100

    buildkite: strip emojis for annotation for now
    Emoji image links are broken and converted into artifact links (which do
    not exist). This seems to be a shortcoming in buildkite atm. Support has
    been contacted. For now, avoid broken images by filtering them out.

--- .buildkite/functions.sh
+++ .buildkite/functions.sh
@@ -356,5 +356,6 @@ patch_check_needs_clean_all()
 emoji_html_img()
 {
 	local S=$1
-	echo $S | sed -E 's|:([^:]+):|<img src="https://buildkiteassets.com/emojis/img-buildkite-64/\1.png" class="emoji">|g'
+	#echo $S | sed -E 's|:([^:]+):|<img src="https://buildkiteassets.com/emojis/img-buildkite-64/\1.png" class="emoji">|g'
+	echo $S | sed -E 's|:([^:]+): ?||g'
 }
