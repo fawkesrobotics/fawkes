9e55bfa2307d37ddc3622dc9069e739945c20bd3
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:46:09 +0100

    protobuf_clips: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/libs/protobuf_clips/communicator.h
+++ src/libs/protobuf_clips/communicator.h
@@ -37,177 +37,205 @@
 #ifndef _PROTOBUF_CLIPS_COMMUNICATOR_H_
 #define _PROTOBUF_CLIPS_COMMUNICATOR_H_
 
+#include <core/threading/mutex.h>
+#include <protobuf_comm/server.h>
+
+#include <clipsmm.h>
 #include <list>
 #include <map>
-#include <clipsmm.h>
-
-#include <protobuf_comm/server.h>
-#include <core/threading/mutex.h>
 
 namespace protobuf_comm {
-  class ProtobufStreamClient;
-  class ProtobufBroadcastPeer;
-}
+class ProtobufStreamClient;
+class ProtobufBroadcastPeer;
+} // namespace protobuf_comm
 
 namespace fawkes {
-  class Logger;
+class Logger;
 }
 
 namespace protobuf_clips {
 
 class ClipsProtobufCommunicator
 {
- public:
-  ClipsProtobufCommunicator(CLIPS::Environment *env, fawkes::Mutex &env_mutex,
-			    fawkes::Logger *logger = NULL);
-  ClipsProtobufCommunicator(CLIPS::Environment *env, fawkes::Mutex &env_mutex,
-			    std::vector<std::string> &proto_path,
-			    fawkes::Logger *logger = NULL);
-  ~ClipsProtobufCommunicator();
+public:
+	ClipsProtobufCommunicator(CLIPS::Environment *env,
+	                          fawkes::Mutex &     env_mutex,
+	                          fawkes::Logger *    logger = NULL);
+	ClipsProtobufCommunicator(CLIPS::Environment *      env,
+	                          fawkes::Mutex &           env_mutex,
+	                          std::vector<std::string> &proto_path,
+	                          fawkes::Logger *          logger = NULL);
+	~ClipsProtobufCommunicator();
 
-  void enable_server(int port);
-  void disable_server();
+	void enable_server(int port);
+	void disable_server();
 
-  /** Get Protobuf server.
+	/** Get Protobuf server.
    * @return protobuf server */
-  protobuf_comm::ProtobufStreamServer *  server() const
-  { return server_; }
+	protobuf_comm::ProtobufStreamServer *
+	server() const
+	{
+		return server_;
+	}
 
-  /** Get protobuf_comm peers.
+	/** Get protobuf_comm peers.
    * @return protobuf_comm peer */
-  const std::map<long int, protobuf_comm::ProtobufBroadcastPeer *> &
-  peers() const
-  { return peers_; }
+	const std::map<long int, protobuf_comm::ProtobufBroadcastPeer *> &
+	peers() const
+	{
+		return peers_;
+	}
 
-  /** Get the communicator's message register.
+	/** Get the communicator's message register.
    * @return message register */
-  protobuf_comm::MessageRegister &  message_register()
-  { return *message_register_; }
+	protobuf_comm::MessageRegister &
+	message_register()
+	{
+		return *message_register_;
+	}
 
-  /** Signal invoked for a message that has been sent to a server client.
+	/** Signal invoked for a message that has been sent to a server client.
    * @return signal
    */
-  boost::signals2::signal<void (protobuf_comm::ProtobufStreamServer::ClientID,
-				std::shared_ptr<google::protobuf::Message>)> &
-    signal_server_sent() { return sig_server_sent_; }
+	boost::signals2::signal<void(protobuf_comm::ProtobufStreamServer::ClientID,
+	                             std::shared_ptr<google::protobuf::Message>)> &
+	signal_server_sent()
+	{
+		return sig_server_sent_;
+	}
 
-  /** Signal invoked for a message that has been sent to a client.
+	/** Signal invoked for a message that has been sent to a client.
    * @return signal
    */
-  boost::signals2::signal<void (std::string, unsigned short,
-				std::shared_ptr<google::protobuf::Message>)> &
-    signal_client_sent() { return sig_client_sent_; }
+	boost::signals2::signal<
+	  void(std::string, unsigned short, std::shared_ptr<google::protobuf::Message>)> &
+	signal_client_sent()
+	{
+		return sig_client_sent_;
+	}
 
-  /** Signal invoked for a message that has been sent via broadcast.
+	/** Signal invoked for a message that has been sent via broadcast.
    * @return signal
    */
-  boost::signals2::signal<void (long,
-				std::shared_ptr<google::protobuf::Message>)> &
-    signal_peer_sent() { return sig_peer_sent_; }
+	boost::signals2::signal<void(long, std::shared_ptr<google::protobuf::Message>)> &
+	signal_peer_sent()
+	{
+		return sig_peer_sent_;
+	}
 
- private:
-  void          setup_clips();
+private:
+	void setup_clips();
 
-  CLIPS::Value  clips_pb_register_type(std::string full_name);
-  CLIPS::Values clips_pb_field_names(void *msgptr);
-  CLIPS::Value  clips_pb_has_field(void *msgptr, std::string field_name);
-  CLIPS::Value  clips_pb_field_value(void *msgptr, std::string field_name);
-  CLIPS::Value  clips_pb_field_type(void *msgptr, std::string field_name);
-  CLIPS::Value  clips_pb_field_label(void *msgptr, std::string field_name);
-  CLIPS::Values clips_pb_field_list(void *msgptr, std::string field_name);
-  CLIPS::Value  clips_pb_field_is_list(void *msgptr, std::string field_name);
-  CLIPS::Value  clips_pb_create(std::string full_name);
-  CLIPS::Value  clips_pb_ref(void *msgptr);
-  void          clips_pb_destroy(void *msgptr);
-  void          clips_pb_set_field(void *msgptr, std::string field_name, CLIPS::Value value);
-  void          clips_pb_add_list(void *msgptr, std::string field_name, CLIPS::Value value);
-  void          clips_pb_send(long int client_id, void *msgptr);
-  std::string   clips_pb_tostring(void *msgptr);
-  long int      clips_pb_client_connect(std::string host, int port);
-  void          clips_pb_disconnect(long int client_id);
-  void          clips_pb_broadcast(long int peer_id, void *msgptr);
-  void          clips_pb_enable_server(int port);
+	CLIPS::Value  clips_pb_register_type(std::string full_name);
+	CLIPS::Values clips_pb_field_names(void *msgptr);
+	CLIPS::Value  clips_pb_has_field(void *msgptr, std::string field_name);
+	CLIPS::Value  clips_pb_field_value(void *msgptr, std::string field_name);
+	CLIPS::Value  clips_pb_field_type(void *msgptr, std::string field_name);
+	CLIPS::Value  clips_pb_field_label(void *msgptr, std::string field_name);
+	CLIPS::Values clips_pb_field_list(void *msgptr, std::string field_name);
+	CLIPS::Value  clips_pb_field_is_list(void *msgptr, std::string field_name);
+	CLIPS::Value  clips_pb_create(std::string full_name);
+	CLIPS::Value  clips_pb_ref(void *msgptr);
+	void          clips_pb_destroy(void *msgptr);
+	void          clips_pb_set_field(void *msgptr, std::string field_name, CLIPS::Value value);
+	void          clips_pb_add_list(void *msgptr, std::string field_name, CLIPS::Value value);
+	void          clips_pb_send(long int client_id, void *msgptr);
+	std::string   clips_pb_tostring(void *msgptr);
+	long int      clips_pb_client_connect(std::string host, int port);
+	void          clips_pb_disconnect(long int client_id);
+	void          clips_pb_broadcast(long int peer_id, void *msgptr);
+	void          clips_pb_enable_server(int port);
 
-  long int      clips_pb_peer_create(std::string host, int port);
-  long int      clips_pb_peer_create_local(std::string host,
-					   int send_port, int recv_port);
-  long int      clips_pb_peer_create_crypto(std::string host, int port,
-					    std::string crypto_key = "", std::string cipher = "");
-  long int      clips_pb_peer_create_local_crypto(std::string host,
-						  int send_port, int recv_port,
-						  std::string crypto_key = "", std::string cipher = "");
-  void          clips_pb_peer_destroy(long int peer_id);
-  void          clips_pb_peer_setup_crypto(long int peer_id,
-					   std::string crypto_key, std::string cipher);
+	long int clips_pb_peer_create(std::string host, int port);
+	long int clips_pb_peer_create_local(std::string host, int send_port, int recv_port);
+	long int clips_pb_peer_create_crypto(std::string host,
+	                                     int         port,
+	                                     std::string crypto_key = "",
+	                                     std::string cipher     = "");
+	long int clips_pb_peer_create_local_crypto(std::string host,
+	                                           int         send_port,
+	                                           int         recv_port,
+	                                           std::string crypto_key = "",
+	                                           std::string cipher     = "");
+	void     clips_pb_peer_destroy(long int peer_id);
+	void     clips_pb_peer_setup_crypto(long int peer_id, std::string crypto_key, std::string cipher);
 
+	typedef enum { CT_SERVER, CT_CLIENT, CT_PEER } ClientType;
+	void clips_assert_message(std::pair<std::string, unsigned short> &    endpoint,
+	                          uint16_t                                    comp_id,
+	                          uint16_t                                    msg_type,
+	                          std::shared_ptr<google::protobuf::Message> &msg,
+	                          ClientType                                  ct,
+	                          unsigned int                                client_id = 0);
+	void handle_server_client_connected(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                                    boost::asio::ip::tcp::endpoint &              endpoint);
+	void handle_server_client_disconnected(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                                       const boost::system::error_code &             error);
 
-  typedef enum {
-    CT_SERVER, CT_CLIENT, CT_PEER
-  } ClientType;
-  void clips_assert_message(std::pair<std::string, unsigned short> &endpoint,
-			    uint16_t comp_id, uint16_t msg_type,
-			    std::shared_ptr<google::protobuf::Message> &msg,
-			    ClientType ct, unsigned int client_id = 0);
-  void handle_server_client_connected(protobuf_comm::ProtobufStreamServer::ClientID client,
-				      boost::asio::ip::tcp::endpoint &endpoint);
-  void handle_server_client_disconnected(protobuf_comm::ProtobufStreamServer::ClientID client,
-					 const boost::system::error_code &error);
+	void handle_server_client_msg(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                              uint16_t                                      component_id,
+	                              uint16_t                                      msg_type,
+	                              std::shared_ptr<google::protobuf::Message>    msg);
 
-  void handle_server_client_msg(protobuf_comm::ProtobufStreamServer::ClientID client,
-				uint16_t component_id, uint16_t msg_type,
-				std::shared_ptr<google::protobuf::Message> msg);
+	void handle_server_client_fail(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                               uint16_t                                      component_id,
+	                               uint16_t                                      msg_type,
+	                               std::string                                   msg);
 
-  void handle_server_client_fail(protobuf_comm::ProtobufStreamServer::ClientID client,
-				 uint16_t component_id, uint16_t msg_type,
-				 std::string msg);
+	void handle_peer_msg(long int                                   peer_id,
+	                     boost::asio::ip::udp::endpoint &           endpoint,
+	                     uint16_t                                   component_id,
+	                     uint16_t                                   msg_type,
+	                     std::shared_ptr<google::protobuf::Message> msg);
+	void handle_peer_recv_error(long int                        peer_id,
+	                            boost::asio::ip::udp::endpoint &endpoint,
+	                            std::string                     msg);
+	void handle_peer_send_error(long int peer_id, std::string msg);
 
-  void handle_peer_msg(long int peer_id,
-		       boost::asio::ip::udp::endpoint &endpoint,
-		       uint16_t component_id, uint16_t msg_type,
-		       std::shared_ptr<google::protobuf::Message> msg);
-  void handle_peer_recv_error(long int peer_id, boost::asio::ip::udp::endpoint &endpoint, std::string msg);
-  void handle_peer_send_error(long int peer_id, std::string msg);
+	void handle_client_connected(long int client_id);
+	void handle_client_disconnected(long int client_id, const boost::system::error_code &error);
+	void handle_client_msg(long int                                   client_id,
+	                       uint16_t                                   comp_id,
+	                       uint16_t                                   msg_type,
+	                       std::shared_ptr<google::protobuf::Message> msg);
+	void handle_client_receive_fail(long int    client_id,
+	                                uint16_t    comp_id,
+	                                uint16_t    msg_type,
+	                                std::string msg);
 
-  void handle_client_connected(long int client_id);
-  void handle_client_disconnected(long int client_id,
-				    const boost::system::error_code &error);
-  void handle_client_msg(long int client_id,
-			 uint16_t comp_id, uint16_t msg_type,
-			 std::shared_ptr<google::protobuf::Message> msg);
-  void handle_client_receive_fail(long int client_id,
-				  uint16_t comp_id, uint16_t msg_type, std::string msg);
+	static std::string to_string(const CLIPS::Value &v);
 
-  static std::string to_string(const CLIPS::Value &v);
+private:
+	CLIPS::Environment *clips_;
+	fawkes::Mutex &     clips_mutex_;
 
- private:
-  CLIPS::Environment   *clips_;
-  fawkes::Mutex        &clips_mutex_;
+	fawkes::Logger *logger_;
 
-  fawkes::Logger       *logger_;
+	protobuf_comm::MessageRegister *     message_register_;
+	protobuf_comm::ProtobufStreamServer *server_;
 
-  protobuf_comm::MessageRegister       *message_register_;
-  protobuf_comm::ProtobufStreamServer  *server_;
+	boost::signals2::signal<void(protobuf_comm::ProtobufStreamServer::ClientID,
+	                             std::shared_ptr<google::protobuf::Message>)>
+	  sig_server_sent_;
+	boost::signals2::signal<
+	  void(std::string, unsigned short, std::shared_ptr<google::protobuf::Message>)>
+	  sig_client_sent_;
+	boost::signals2::signal<void(long int, std::shared_ptr<google::protobuf::Message>)>
+	  sig_peer_sent_;
 
-  boost::signals2::signal<void (protobuf_comm::ProtobufStreamServer::ClientID,
-				std::shared_ptr<google::protobuf::Message>)> sig_server_sent_;
-  boost::signals2::signal<void (std::string, unsigned short,
-				std::shared_ptr<google::protobuf::Message>)> sig_client_sent_;
-  boost::signals2::signal<void (long int, std::shared_ptr<google::protobuf::Message>)> sig_peer_sent_;
-  
-  fawkes::Mutex map_mutex_;
-  long int next_client_id_;
+	fawkes::Mutex map_mutex_;
+	long int      next_client_id_;
 
+	std::map<long int, protobuf_comm::ProtobufStreamServer::ClientID>         server_clients_;
+	typedef std::map<protobuf_comm::ProtobufStreamServer::ClientID, long int> RevServerClientMap;
+	RevServerClientMap                                                        rev_server_clients_;
+	std::map<long int, protobuf_comm::ProtobufStreamClient *>                 clients_;
+	std::map<long int, protobuf_comm::ProtobufBroadcastPeer *>                peers_;
 
-  std::map<long int, protobuf_comm::ProtobufStreamServer::ClientID> server_clients_;
-  typedef std::map<protobuf_comm::ProtobufStreamServer::ClientID, long int> RevServerClientMap;
-  RevServerClientMap rev_server_clients_;
-  std::map<long int, protobuf_comm::ProtobufStreamClient *>  clients_;
-  std::map<long int, protobuf_comm::ProtobufBroadcastPeer *> peers_;
+	std::map<long int, std::pair<std::string, unsigned short>> client_endpoints_;
 
-  std::map<long int, std::pair<std::string, unsigned short>> client_endpoints_;
-
-  std::list<std::string>  functions_;
-  CLIPS::Fact::pointer    avail_fact_;
+	std::list<std::string> functions_;
+	CLIPS::Fact::pointer   avail_fact_;
 };
 
 } // end namespace protobuf_clips
