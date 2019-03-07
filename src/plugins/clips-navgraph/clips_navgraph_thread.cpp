2e30d9be2eb188bc1afd630cb7d5bacf85bdded0
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:48:32 +0100

    clips-navgraph: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/clips-navgraph/clips_navgraph_thread.cpp
+++ src/plugins/clips-navgraph/clips_navgraph_thread.cpp
@@ -21,9 +21,9 @@
 
 #include "clips_navgraph_thread.h"
 
-#include <navgraph/navgraph.h>
-#include <navgraph/constraints/static_list_edge_constraint.h>
 #include <navgraph/constraints/constraint_repo.h>
+#include <navgraph/constraints/static_list_edge_constraint.h>
+#include <navgraph/navgraph.h>
 
 #include <clipsmm.h>
 
@@ -36,165 +36,166 @@ using namespace fawkes;
 
 /** Constructor. */
 ClipsNavGraphThread::ClipsNavGraphThread()
-  : Thread("ClipsNavGraphThread", Thread::OPMODE_WAITFORWAKEUP),
-    CLIPSFeature("navgraph"), CLIPSFeatureAspect(this)
+: Thread("ClipsNavGraphThread", Thread::OPMODE_WAITFORWAKEUP),
+  CLIPSFeature("navgraph"),
+  CLIPSFeatureAspect(this)
 {
 }
-
 
 /** Destructor. */
 ClipsNavGraphThread::~ClipsNavGraphThread()
 {
 }
 
-
 void
 ClipsNavGraphThread::init()
 {
-  navgraph->add_change_listener(this);
+	navgraph->add_change_listener(this);
 
-  edge_constraint_ = new NavGraphStaticListEdgeConstraint("clips");
-  navgraph->constraint_repo()->register_constraint(edge_constraint_);
+	edge_constraint_ = new NavGraphStaticListEdgeConstraint("clips");
+	navgraph->constraint_repo()->register_constraint(edge_constraint_);
 }
-
 
 void
 ClipsNavGraphThread::finalize()
 {
-  navgraph->constraint_repo()->unregister_constraint(edge_constraint_->name());
-  delete edge_constraint_;
+	navgraph->constraint_repo()->unregister_constraint(edge_constraint_->name());
+	delete edge_constraint_;
 
-  navgraph->remove_change_listener(this);
-  envs_.clear();
+	navgraph->remove_change_listener(this);
+	envs_.clear();
 }
 
-
 void
-ClipsNavGraphThread::clips_context_init(const std::string &env_name,
-					LockPtr<CLIPS::Environment> &clips)
+ClipsNavGraphThread::clips_context_init(const std::string &          env_name,
+                                        LockPtr<CLIPS::Environment> &clips)
 {
-  envs_[env_name] = clips;
-  logger->log_info(name(), "Called to initialize environment %s", env_name.c_str());
+	envs_[env_name] = clips;
+	logger->log_info(name(), "Called to initialize environment %s", env_name.c_str());
 
-  clips.lock();
-  clips->batch_evaluate(SRCDIR"/clips/navgraph.clp");
-  clips_navgraph_load(clips);
+	clips.lock();
+	clips->batch_evaluate(SRCDIR "/clips/navgraph.clp");
+	clips_navgraph_load(clips);
 
-  clips->add_function("navgraph-block-edge",
-    sigc::slot<void, std::string, std::string>(
-      sigc::bind<0>(
-        sigc::mem_fun(*this, &ClipsNavGraphThread::clips_navgraph_block_edge),
-	env_name)
-    )
-  );
+	clips->add_function("navgraph-block-edge",
+	                    sigc::slot<void, std::string, std::string>(sigc::bind<0>(
+	                      sigc::mem_fun(*this, &ClipsNavGraphThread::clips_navgraph_block_edge),
+	                      env_name)));
 
-  clips->add_function("navgraph-unblock-edge",
-    sigc::slot<void, std::string, std::string>(
-      sigc::bind<0>(
-        sigc::mem_fun(*this, &ClipsNavGraphThread::clips_navgraph_unblock_edge),
-	env_name)
-    )
-  );
+	clips->add_function("navgraph-unblock-edge",
+	                    sigc::slot<void, std::string, std::string>(sigc::bind<0>(
+	                      sigc::mem_fun(*this, &ClipsNavGraphThread::clips_navgraph_unblock_edge),
+	                      env_name)));
 
-  clips.unlock();
+	clips.unlock();
 }
 
 void
 ClipsNavGraphThread::clips_context_destroyed(const std::string &env_name)
 {
-  envs_.erase(env_name);
-  logger->log_info(name(), "Removing environment %s", env_name.c_str());
+	envs_.erase(env_name);
+	logger->log_info(name(), "Removing environment %s", env_name.c_str());
 }
 
 void
 ClipsNavGraphThread::clips_navgraph_load(LockPtr<CLIPS::Environment> &clips)
 {
-  try {
-    const std::vector<NavGraphNode> &nodes = navgraph->nodes();
-    const std::vector<NavGraphEdge> &edges = navgraph->edges();
+	try {
+		const std::vector<NavGraphNode> &nodes = navgraph->nodes();
+		const std::vector<NavGraphEdge> &edges = navgraph->edges();
 
-    clips->assert_fact_f("(navgraph (name \"%s\"))", navgraph->name().c_str());
+		clips->assert_fact_f("(navgraph (name \"%s\"))", navgraph->name().c_str());
 
-    for (const NavGraphNode &n : nodes) {
-      std::string props_string;
-      const std::map<std::string, std::string> &properties = n.properties();
-      for (auto p : properties) {
-	props_string += " \"" + p.first + "\" \"" + p.second + "\"";
-      }
-      clips->assert_fact_f("(navgraph-node (name \"%s\") (pos %f %f) (properties %s))",
-			   n.name().c_str(), n.x(), n.y(), props_string.c_str());
-    }
+		for (const NavGraphNode &n : nodes) {
+			std::string                               props_string;
+			const std::map<std::string, std::string> &properties = n.properties();
+			for (auto p : properties) {
+				props_string += " \"" + p.first + "\" \"" + p.second + "\"";
+			}
+			clips->assert_fact_f("(navgraph-node (name \"%s\") (pos %f %f) (properties %s))",
+			                     n.name().c_str(),
+			                     n.x(),
+			                     n.y(),
+			                     props_string.c_str());
+		}
 
-    for (const NavGraphEdge &e : edges) {
-      std::string props_string;
-      const std::map<std::string, std::string> &properties = e.properties();
-      for (auto p : properties) {
-	props_string += " \"" + p.first + "\" \"" + p.second + "\"";
-      }
-      clips->assert_fact_f("(navgraph-edge (from \"%s\") (to \"%s\") (directed %s) "
-			   "(properties %s))",
-			   e.from().c_str(), e.to().c_str(),
-			   e.is_directed() ? "TRUE" : "FALSE", props_string.c_str());
-    }
+		for (const NavGraphEdge &e : edges) {
+			std::string                               props_string;
+			const std::map<std::string, std::string> &properties = e.properties();
+			for (auto p : properties) {
+				props_string += " \"" + p.first + "\" \"" + p.second + "\"";
+			}
+			clips->assert_fact_f("(navgraph-edge (from \"%s\") (to \"%s\") (directed %s) "
+			                     "(properties %s))",
+			                     e.from().c_str(),
+			                     e.to().c_str(),
+			                     e.is_directed() ? "TRUE" : "FALSE",
+			                     props_string.c_str());
+		}
 
-  } catch (Exception &e) {
-    logger->log_warn(name(), "Failed to assert navgraph, exception follows");
-    logger->log_warn(name(), e);
-    clips->assert_fact_f("(navgraph-load-fail %s)", *(e.begin()));
-  }
+	} catch (Exception &e) {
+		logger->log_warn(name(), "Failed to assert navgraph, exception follows");
+		logger->log_warn(name(), e);
+		clips->assert_fact_f("(navgraph-load-fail %s)", *(e.begin()));
+	}
 }
-
 
 void
 ClipsNavGraphThread::clips_navgraph_block_edge(std::string env_name,
-					       std::string from, std::string to)
+                                               std::string from,
+                                               std::string to)
 {
-  const std::vector<NavGraphEdge> &graph_edges = navgraph->edges();
+	const std::vector<NavGraphEdge> &graph_edges = navgraph->edges();
 
-  for (const NavGraphEdge &edge : graph_edges) {
-    if (edge.from() == from && edge.to() == to) {
-      edge_constraint_->add_edge(edge);
-      return;
-    }
-  }
+	for (const NavGraphEdge &edge : graph_edges) {
+		if (edge.from() == from && edge.to() == to) {
+			edge_constraint_->add_edge(edge);
+			return;
+		}
+	}
 
-  logger->log_warn(name(), "Environment %s tried to block edge %s--%s, "
-		   "which does not exist in graph", env_name.c_str(),
-		   from.c_str(), to.c_str());
+	logger->log_warn(name(),
+	                 "Environment %s tried to block edge %s--%s, "
+	                 "which does not exist in graph",
+	                 env_name.c_str(),
+	                 from.c_str(),
+	                 to.c_str());
 }
-
 
 void
 ClipsNavGraphThread::clips_navgraph_unblock_edge(std::string env_name,
-						 std::string from, std::string to)
+                                                 std::string from,
+                                                 std::string to)
 {
-  const std::vector<NavGraphEdge> &graph_edges = navgraph->edges();
+	const std::vector<NavGraphEdge> &graph_edges = navgraph->edges();
 
-  for (const NavGraphEdge &edge : graph_edges) {
-    if (edge.from() == from && edge.to() == to) {
-      edge_constraint_->remove_edge(edge);
-      return;
-    }
-  }
+	for (const NavGraphEdge &edge : graph_edges) {
+		if (edge.from() == from && edge.to() == to) {
+			edge_constraint_->remove_edge(edge);
+			return;
+		}
+	}
 
-  logger->log_warn(name(), "Environment %s tried to unblock edge %s--%s, "
-		   "which does not exist in graph", env_name.c_str(),
-		   from.c_str(), to.c_str());
+	logger->log_warn(name(),
+	                 "Environment %s tried to unblock edge %s--%s, "
+	                 "which does not exist in graph",
+	                 env_name.c_str(),
+	                 from.c_str(),
+	                 to.c_str());
 }
 
 void
 ClipsNavGraphThread::graph_changed() throw()
 {
-  for (auto e : envs_) {
-    logger->log_debug(name(), "Graph changed, re-asserting in environment %s", e.first.c_str());
-    fawkes::LockPtr<CLIPS::Environment> &clips = e.second;
-    clips.lock();
-    clips->evaluate("(navgraph-cleanup)");
-    clips_navgraph_load(clips);
-    clips.unlock();
-  }
+	for (auto e : envs_) {
+		logger->log_debug(name(), "Graph changed, re-asserting in environment %s", e.first.c_str());
+		fawkes::LockPtr<CLIPS::Environment> &clips = e.second;
+		clips.lock();
+		clips->evaluate("(navgraph-cleanup)");
+		clips_navgraph_load(clips);
+		clips.unlock();
+	}
 }
-
 
 void
 ClipsNavGraphThread::loop()
