5c185d1b7f0394b6ba9e621b6cf131783de1453b
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:48:41 +0100

    logview: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/tools/logview/main.cpp
+++ src/tools/logview/main.cpp
@@ -20,131 +20,128 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
+#include <logging/console.h>
 #include <netcomm/fawkes/client.h>
 #include <netcomm/fawkes/client_handler.h>
 #include <netcomm/fawkes/component_ids.h>
 #include <network_logger/network_logger.h>
-#include <logging/console.h>
-#include <utils/system/signal.h>
 #include <utils/system/argparser.h>
+#include <utils/system/signal.h>
 
-#include <cstring>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 using namespace fawkes;
 
 /// @cond INTERNALS
 
-class NetLogConsolePrinter
-  : public FawkesNetworkClientHandler,
-    public SignalHandler
+class NetLogConsolePrinter : public FawkesNetworkClientHandler, public SignalHandler
 {
- public:
-  NetLogConsolePrinter(const char *hostport)
-  {
-    logger = new ConsoleLogger();
-    quit = false;
+public:
+	NetLogConsolePrinter(const char *hostport)
+	{
+		logger = new ConsoleLogger();
+		quit   = false;
 
-    char *hp = strdup(hostport);
-    const char *hostname = strtok(hp, ":");
-    const char *portstr = strtok(NULL, "");
-    int port = 1910;
-    if ( portstr ) {
-      port = atoi(portstr);
-      if ( (port < 0) || ( port > 0xFFFF ) ) {
-	printf("Invalid port given, must be in range [1:65535]. Using default 1910 instead\n");
-	port = 1910;
-      }
-    }
+		char *      hp       = strdup(hostport);
+		const char *hostname = strtok(hp, ":");
+		const char *portstr  = strtok(NULL, "");
+		int         port     = 1910;
+		if (portstr) {
+			port = atoi(portstr);
+			if ((port < 0) || (port > 0xFFFF)) {
+				printf("Invalid port given, must be in range [1:65535]. Using default 1910 instead\n");
+				port = 1910;
+			}
+		}
 
-    client = new FawkesNetworkClient(hostname, port);
-    client->connect();
-    client->register_handler(this, FAWKES_CID_NETWORKLOGGER);
+		client = new FawkesNetworkClient(hostname, port);
+		client->connect();
+		client->register_handler(this, FAWKES_CID_NETWORKLOGGER);
 
-    client->enqueue(new FawkesNetworkMessage(FAWKES_CID_NETWORKLOGGER,
-					     NetworkLogger::MSGTYPE_SUBSCRIBE));
-  }
+		client->enqueue(
+		  new FawkesNetworkMessage(FAWKES_CID_NETWORKLOGGER, NetworkLogger::MSGTYPE_SUBSCRIBE));
+	}
 
-  ~NetLogConsolePrinter()
-  {
-    delete logger;
-    delete client;
-  }
+	~NetLogConsolePrinter()
+	{
+		delete logger;
+		delete client;
+	}
 
-  void handle_signal(int signal)
-  {
-    quit = true;
-    client->wake(FAWKES_CID_NETWORKLOGGER);
-  }
+	void
+	handle_signal(int signal)
+	{
+		quit = true;
+		client->wake(FAWKES_CID_NETWORKLOGGER);
+	}
 
-  virtual void inbound_received(FawkesNetworkMessage *m,
-				unsigned int id) throw()
-  {
-    if ( (m->cid() == FAWKES_CID_NETWORKLOGGER) &&
-	 (m->msgid() == NetworkLogger::MSGTYPE_LOGMESSAGE) ) {
-      NetworkLoggerMessageContent *content = m->msgc<NetworkLoggerMessageContent>();
-      struct timeval t = content->get_time();
-      logger->tlog(content->get_loglevel(), &t, content->get_component(), "%s", content->get_message());
-    }
-  }
+	virtual void
+	inbound_received(FawkesNetworkMessage *m, unsigned int id) throw()
+	{
+		if ((m->cid() == FAWKES_CID_NETWORKLOGGER)
+		    && (m->msgid() == NetworkLogger::MSGTYPE_LOGMESSAGE)) {
+			NetworkLoggerMessageContent *content = m->msgc<NetworkLoggerMessageContent>();
+			struct timeval               t       = content->get_time();
+			logger->tlog(
+			  content->get_loglevel(), &t, content->get_component(), "%s", content->get_message());
+		}
+	}
 
-  virtual void deregistered(unsigned int id) throw()
-  {
-    quit = true;
-  }
+	virtual void
+	deregistered(unsigned int id) throw()
+	{
+		quit = true;
+	}
 
+	virtual void
+	connection_died(unsigned int id) throw()
+	{
+		printf("Connection to host died. Aborting.\n");
+		quit = true;
+	}
 
-  virtual void connection_died(unsigned int id) throw()
-  {
-    printf("Connection to host died. Aborting.\n");
-    quit = true;
-  }
+	virtual void
+	connection_established(unsigned int id) throw()
+	{
+	}
 
+	void
+	run()
+	{
+		while (!quit) {
+			client->wait(FAWKES_CID_NETWORKLOGGER);
+		}
+		client->disconnect();
+	}
 
-  virtual void connection_established(unsigned int id) throw()
-  {
-  }
-
-
-  void run()
-  {
-    while ( ! quit ) {
-      client->wait(FAWKES_CID_NETWORKLOGGER);
-    }
-    client->disconnect();
-  }
-
-
- private:
-  FawkesNetworkClient *client;
-  ConsoleLogger *logger;
-  bool quit;
+private:
+	FawkesNetworkClient *client;
+	ConsoleLogger *      logger;
+	bool                 quit;
 };
 /// @endcond
-
 
 void
 print_usage(const char *program_name)
 {
-  printf("Usage: %s [hostname[:port]]\n", program_name);
+	printf("Usage: %s [hostname[:port]]\n", program_name);
 }
 
 int
 main(int argc, char **argv)
 {
-  ArgumentParser argp(argc, argv, "h");
+	ArgumentParser argp(argc, argv, "h");
 
-  if ( argp.has_arg("h") ) {
-    print_usage(argv[0]);
-    exit(0);
-  }
+	if (argp.has_arg("h")) {
+		print_usage(argv[0]);
+		exit(0);
+	}
 
-  const char *hostport = (argp.num_items() > 0) ? argp.items()[0] : "localhost:1910";
-  NetLogConsolePrinter printer(hostport);
+	const char *         hostport = (argp.num_items() > 0) ? argp.items()[0] : "localhost:1910";
+	NetLogConsolePrinter printer(hostport);
 
-  SignalManager::register_handler(SIGINT, &printer);
-  printer.run();
-
+	SignalManager::register_handler(SIGINT, &printer);
+	printer.run();
 }
-
