97a4eda83ce483ffb2fa18e7091e90c40a72ca0d
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:47:25 +0100

    gazsim-webcam: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/gazebo/gazsim-webcam/gazsim_webcam.cpp
+++ src/plugins/gazebo/gazsim-webcam/gazsim_webcam.cpp
@@ -20,16 +20,15 @@
 
 #include "gazsim_webcam.h"
 
+#include <fvutils/color/conversions.h>
 #include <tf/types.h>
-#include <stdio.h>
-#include <math.h>
 #include <utils/math/angle.h>
 
-#include <gazebo/transport/Node.hh>
 #include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/Node.hh>
 #include <gazebo/transport/transport.hh>
-
-#include <fvutils/color/conversions.h>
+#include <math.h>
+#include <stdio.h>
 
 using namespace fawkes;
 
@@ -38,64 +37,63 @@ using namespace fawkes;
  * @author Frederik Zwilling
  */
 
-
 /** Constructor.
  * The GazsimWebcam object simulates a single webcam in Gazebo
  * @param shm_id The shared memory id the simulated webcam should write to and the prefix of the config values for this camera
  * @param gazebo_world_node gazebo world node to register subscribers
  * @param config config object to access config values
  */
-GazsimWebcam::GazsimWebcam(std::string shm_id,
-			     gazebo::transport::NodePtr gazebo_world_node,
-			     Configuration* config)
+GazsimWebcam::GazsimWebcam(std::string                shm_id,
+                           gazebo::transport::NodePtr gazebo_world_node,
+                           Configuration *            config)
 {
-  shm_buffer_ = NULL;
-  //read config values
-  std::string robot_name = config->get_string("/gazsim/robot-name");
-  shm_id_ = robot_name + "/" + shm_id;
-  topic_name_ = ("~/" 
-		 + robot_name
-                 + config->get_string((std::string("/gazsim/webcam/topic-suffixes/")
-		                        + shm_id).c_str()));
-  width_ = config->get_float((std::string("/gazsim/webcam/widths/") + shm_id).c_str());
-  height_ = config->get_float((std::string("/gazsim/webcam/heights/") + shm_id).c_str());
-  frame_ = config->get_string((std::string("/gazsim/webcam/frames/") + shm_id).c_str());
+	shm_buffer_ = NULL;
+	//read config values
+	std::string robot_name = config->get_string("/gazsim/robot-name");
+	shm_id_                = robot_name + "/" + shm_id;
+	topic_name_ =
+	  ("~/" + robot_name
+	   + config->get_string((std::string("/gazsim/webcam/topic-suffixes/") + shm_id).c_str()));
+	width_  = config->get_float((std::string("/gazsim/webcam/widths/") + shm_id).c_str());
+	height_ = config->get_float((std::string("/gazsim/webcam/heights/") + shm_id).c_str());
+	frame_  = config->get_string((std::string("/gazsim/webcam/frames/") + shm_id).c_str());
 
-  format_from_ = firevision::RGB;
-  format_to_ = firevision::YUV422_PLANAR;
+	format_from_ = firevision::RGB;
+	format_to_   = firevision::YUV422_PLANAR;
 
-  //subscribing to gazebo publisher
-  //the messages are published by the sensor itself and not by a robot plugin
-  //therefore we have to use the world node
-  webcam_sub_ = gazebo_world_node->Subscribe(topic_name_, &GazsimWebcam::on_webcam_data_msg, this);
+	//subscribing to gazebo publisher
+	//the messages are published by the sensor itself and not by a robot plugin
+	//therefore we have to use the world node
+	webcam_sub_ = gazebo_world_node->Subscribe(topic_name_, &GazsimWebcam::on_webcam_data_msg, this);
 
-  //initialize shared memory image buffer
-  shm_buffer_ = new firevision::SharedMemoryImageBuffer( shm_id_.c_str(),
-							 format_to_,
-							 width_,
-							 height_
-							 );
-  if (!shm_buffer_->is_valid()) {
-    throw fawkes::Exception("Shared memory segment not valid");
-  }
-  shm_buffer_->set_frame_id(frame_.c_str());
-  buffer_ = shm_buffer_->buffer();
-  //enable locking
-  shm_buffer_->add_semaphore();
+	//initialize shared memory image buffer
+	shm_buffer_ =
+	  new firevision::SharedMemoryImageBuffer(shm_id_.c_str(), format_to_, width_, height_);
+	if (!shm_buffer_->is_valid()) {
+		throw fawkes::Exception("Shared memory segment not valid");
+	}
+	shm_buffer_->set_frame_id(frame_.c_str());
+	buffer_ = shm_buffer_->buffer();
+	//enable locking
+	shm_buffer_->add_semaphore();
 }
 
 GazsimWebcam::~GazsimWebcam()
 {
-  delete this->shm_buffer_;
+	delete this->shm_buffer_;
 }
 
-void GazsimWebcam::on_webcam_data_msg(ConstImageStampedPtr &msg)
-{  
-  //convert image data and write it in the shared memory buffer
-  //lock the shm so noone can read a half written image
-  shm_buffer_->lock_for_write();
-  convert(format_from_,  format_to_,
- 	  (const unsigned char*) msg->image().data().data(),   buffer_,
-	  width_, height_);
-  shm_buffer_->unlock();
+void
+GazsimWebcam::on_webcam_data_msg(ConstImageStampedPtr &msg)
+{
+	//convert image data and write it in the shared memory buffer
+	//lock the shm so noone can read a half written image
+	shm_buffer_->lock_for_write();
+	convert(format_from_,
+	        format_to_,
+	        (const unsigned char *)msg->image().data().data(),
+	        buffer_,
+	        width_,
+	        height_);
+	shm_buffer_->unlock();
 }
