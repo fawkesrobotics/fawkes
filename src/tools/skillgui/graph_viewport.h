23f3a7826189235732d692c8ab59ed90f65daf5d
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:48:41 +0100

    skillgui: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/tools/skillgui/graph_viewport.h
+++ src/tools/skillgui/graph_viewport.h
@@ -23,70 +23,68 @@
 #ifndef _TOOLS_SKILLGUI_GRAPH_VIEWPORT_H_
 #define _TOOLS_SKILLGUI_GRAPH_VIEWPORT_H_
 
-#include <papyrus.h>
 #include <papyrus-gtkmm/viewport.h>
 
 #include <gvc.h>
 #include <gvcjob.h>
-
+#include <papyrus.h>
 #include <string>
 
 class SkillGuiGraphViewport : public Papyrus::Gtk::Viewport
 {
- public:
-  SkillGuiGraphViewport();
-  ~SkillGuiGraphViewport();
+public:
+	SkillGuiGraphViewport();
+	~SkillGuiGraphViewport();
 
-  void set_gvjob(GVJ_t *job);
-  void set_graph_fsm(const std::string& fsm_name);
-  void set_graph(const std::string& graph);
+	void set_gvjob(GVJ_t *job);
+	void set_graph_fsm(const std::string &fsm_name);
+	void set_graph(const std::string &graph);
 
-  bool get_update_graph();
-  void set_update_graph(bool update);
+	bool get_update_graph();
+	void set_update_graph(bool update);
 
-  void save();
-  void render();
+	void save();
+	void render();
 
-  void zoom_in();
-  void zoom_out();
-  void zoom_fit();
-  void zoom_reset();
+	void zoom_in();
+	void zoom_out();
+	void zoom_fit();
+	void zoom_reset();
 
-  Papyrus::AffineController::pointer get_affine();
+	Papyrus::AffineController::pointer get_affine();
 
-  void add_drawable(Papyrus::Drawable::pointer d);
-  virtual void clear();
-  void set_bb(double bbw, double bbh);
-  void set_pad(double pad_x, double pad_y);
-  void set_translation(double tx, double ty);
-  void set_scale(double scale);
-  bool scale_override();
+	void         add_drawable(Papyrus::Drawable::pointer d);
+	virtual void clear();
+	void         set_bb(double bbw, double bbh);
+	void         set_pad(double pad_x, double pad_y);
+	void         set_translation(double tx, double ty);
+	void         set_scale(double scale);
+	bool         scale_override();
 
- protected:
-  void on_expose(GdkEventExpose *event);
+protected:
+	void on_expose(GdkEventExpose *event);
 
- private:
-  GVC_t *gvc_;
-  GVJ_t *gvjob_;
+private:
+	GVC_t *gvc_;
+	GVJ_t *gvjob_;
 
-  std::string graph_fsm_;
-  std::string graph_;
+	std::string graph_fsm_;
+	std::string graph_;
 
-  double bbw_;
-  double bbh_;
-  double pad_x_;
-  double pad_y_;
-  double translation_x_;
-  double translation_y_;
-  double scale_;
-  bool   update_graph_;
+	double bbw_;
+	double bbh_;
+	double pad_x_;
+	double pad_y_;
+	double translation_x_;
+	double translation_y_;
+	double scale_;
+	bool   update_graph_;
 
-  bool scale_override_;
+	bool scale_override_;
 
-  Gtk::FileChooserDialog *fcd_;
-  Papyrus::AffineController::pointer affine_;
-  Papyrus::Translator::pointer translator_;
+	Gtk::FileChooserDialog *           fcd_;
+	Papyrus::AffineController::pointer affine_;
+	Papyrus::Translator::pointer       translator_;
 };
-
 
 #endif
