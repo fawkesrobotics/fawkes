48bf5a7b9940df5d6efd1122cc0bfb1f96e93285
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:48:33 +0100

    openprs-agent: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/openprs-agent/mod_navgraph.cpp
+++ src/plugins/openprs-agent/mod_navgraph.cpp
@@ -19,12 +19,12 @@
  */
 
 // this must come first due to a define of enqueue in OpenPRS' slistPack_f.h
-#include <netcomm/fawkes/client.h>
-
-#include <plugins/openprs/mod_utils.h>
 #include <config/netconf.h>
-#include <navgraph/yaml_navgraph.h>
 #include <navgraph/navgraph.h>
+#include <navgraph/yaml_navgraph.h>
+#include <netcomm/fawkes/client.h>
+#include <plugins/openprs/mod_utils.h>
+
 #include <oprs_f-pub.h>
 
 using namespace fawkes;
@@ -32,116 +32,111 @@ using namespace fawkes;
 extern "C" void finalize();
 
 // Global variables
-FawkesNetworkClient  *g_fnet_client = NULL;
-NetworkConfiguration *g_config = NULL;
-NavGraph  *g_navgraph = NULL;
+FawkesNetworkClient * g_fnet_client = NULL;
+NetworkConfiguration *g_config      = NULL;
+NavGraph *            g_navgraph    = NULL;
 
-
-extern "C"
-Term *
+extern "C" Term *
 action_navgraph_load(TermList terms)
 {
-  ACTION_ASSERT_ARG_LENGTH("navgraph-load", terms, 0);
+	ACTION_ASSERT_ARG_LENGTH("navgraph-load", terms, 0);
 
-  try {
-    std::string graph_file =
-      g_config->get_string("/navgraph/graph_file");
+	try {
+		std::string graph_file = g_config->get_string("/navgraph/graph_file");
 
-    if (graph_file[0] != '/') {
-      graph_file = std::string(CONFDIR) + "/" + graph_file;
-    }
+		if (graph_file[0] != '/') {
+			graph_file = std::string(CONFDIR) + "/" + graph_file;
+		}
 
-    g_navgraph = load_yaml_navgraph(graph_file);
+		g_navgraph = load_yaml_navgraph(graph_file);
 
-    const std::vector<NavGraphNode> &nodes = g_navgraph->nodes();
-    const std::vector<NavGraphEdge> &edges = g_navgraph->edges();
+		const std::vector<NavGraphNode> &nodes = g_navgraph->nodes();
+		const std::vector<NavGraphEdge> &edges = g_navgraph->edges();
 
-    TermList graph_tl = sl_make_slist();
-    graph_tl = build_term_list(graph_tl, build_string(g_navgraph->name().c_str()));
-    graph_tl = build_term_list(graph_tl, build_string(graph_file.c_str()));
-    add_external_fact((char *)"navgraph", graph_tl);
+		TermList graph_tl = sl_make_slist();
+		graph_tl          = build_term_list(graph_tl, build_string(g_navgraph->name().c_str()));
+		graph_tl          = build_term_list(graph_tl, build_string(graph_file.c_str()));
+		add_external_fact((char *)"navgraph", graph_tl);
 
-    for (auto n : nodes) {
-      TermList props = sl_make_slist();
-      const std::map<std::string, std::string> &properties = n.properties();
-      for (auto p : properties) {
-	TermList prop = sl_make_slist();
-	prop = build_term_list(prop, build_string(p.first.c_str()));
-	prop = build_term_list(prop, build_string(p.second.c_str()));
-	props = build_term_list(props, build_term_l_list_from_c_list(prop));
-      }
+		for (auto n : nodes) {
+			TermList                                  props      = sl_make_slist();
+			const std::map<std::string, std::string> &properties = n.properties();
+			for (auto p : properties) {
+				TermList prop = sl_make_slist();
+				prop          = build_term_list(prop, build_string(p.first.c_str()));
+				prop          = build_term_list(prop, build_string(p.second.c_str()));
+				props         = build_term_list(props, build_term_l_list_from_c_list(prop));
+			}
 
-      TermList node_tl = sl_make_slist();
-      node_tl = build_term_list(node_tl, build_string(n.name().c_str()));
-      node_tl = build_term_list(node_tl, build_float(n.x()));
-      node_tl = build_term_list(node_tl, build_float(n.y()));
-      node_tl = build_term_list(node_tl, build_term_l_list_from_c_list(props));
+			TermList node_tl = sl_make_slist();
+			node_tl          = build_term_list(node_tl, build_string(n.name().c_str()));
+			node_tl          = build_term_list(node_tl, build_float(n.x()));
+			node_tl          = build_term_list(node_tl, build_float(n.y()));
+			node_tl          = build_term_list(node_tl, build_term_l_list_from_c_list(props));
 
-      add_external_fact((char *)"navgraph-node", node_tl);
-    }
+			add_external_fact((char *)"navgraph-node", node_tl);
+		}
 
-    for (auto e : edges) {
-      TermList props = sl_make_slist();
-      const std::map<std::string, std::string> &properties = e.properties();
-      for (auto p : properties) {
-	TermList prop = sl_make_slist();
-	prop = build_term_list(prop, build_string(p.first.c_str()));
-	prop = build_term_list(prop, build_string(p.second.c_str()));
-	props = build_term_list(props, build_term_l_list_from_c_list(prop));
-      }
+		for (auto e : edges) {
+			TermList                                  props      = sl_make_slist();
+			const std::map<std::string, std::string> &properties = e.properties();
+			for (auto p : properties) {
+				TermList prop = sl_make_slist();
+				prop          = build_term_list(prop, build_string(p.first.c_str()));
+				prop          = build_term_list(prop, build_string(p.second.c_str()));
+				props         = build_term_list(props, build_term_l_list_from_c_list(prop));
+			}
 
-      TermList edge_tl = sl_make_slist();
-      edge_tl = build_term_list(edge_tl, build_string(e.from().c_str()));
-      edge_tl = build_term_list(edge_tl, build_string(e.to().c_str()));
-      edge_tl = build_term_list(edge_tl, e.is_directed() ? build_t() : build_nil());
-      edge_tl = build_term_list(edge_tl, build_term_l_list_from_c_list(props));
+			TermList edge_tl = sl_make_slist();
+			edge_tl          = build_term_list(edge_tl, build_string(e.from().c_str()));
+			edge_tl          = build_term_list(edge_tl, build_string(e.to().c_str()));
+			edge_tl          = build_term_list(edge_tl, e.is_directed() ? build_t() : build_nil());
+			edge_tl          = build_term_list(edge_tl, build_term_l_list_from_c_list(props));
 
-      add_external_fact((char *)"navgraph-edge", edge_tl);
-    }
+			add_external_fact((char *)"navgraph-edge", edge_tl);
+		}
 
-  } catch (Exception &e) {
-    fprintf(stderr, "Failed to open navgraph: %s\n", e.what_no_backtrace());
-    ACTION_FAIL();
-  }
+	} catch (Exception &e) {
+		fprintf(stderr, "Failed to open navgraph: %s\n", e.what_no_backtrace());
+		ACTION_FAIL();
+	}
 
-  ACTION_FINAL();
+	ACTION_FINAL();
 }
 
-
 /** Entry function for the OpenPRS module. */
-extern "C"
-void init()
+extern "C" void
+init()
 {
-  printf("*** LOADING mod_navgraph\n");
+	printf("*** LOADING mod_navgraph\n");
 
-  std::string    fawkes_host;
-  unsigned short fawkes_port = 0;
-  get_fawkes_host_port(fawkes_host, fawkes_port);
+	std::string    fawkes_host;
+	unsigned short fawkes_port = 0;
+	get_fawkes_host_port(fawkes_host, fawkes_port);
 
-  printf("Connecting to Fawkes at %s:%u\n", fawkes_host.c_str(), fawkes_port);
-  try {
-    g_fnet_client = new FawkesNetworkClient(fawkes_host.c_str(), fawkes_port);
-    g_fnet_client->connect();
-    g_config      = new NetworkConfiguration(g_fnet_client);
-    g_config->set_mirror_mode(true);
-  } catch (Exception &e) {
-    fprintf(stderr, "Error: cannot establish network connection: %s\n",
-            e.what_no_backtrace());
-  }
+	printf("Connecting to Fawkes at %s:%u\n", fawkes_host.c_str(), fawkes_port);
+	try {
+		g_fnet_client = new FawkesNetworkClient(fawkes_host.c_str(), fawkes_port);
+		g_fnet_client->connect();
+		g_config = new NetworkConfiguration(g_fnet_client);
+		g_config->set_mirror_mode(true);
+	} catch (Exception &e) {
+		fprintf(stderr, "Error: cannot establish network connection: %s\n", e.what_no_backtrace());
+	}
 
-  make_and_declare_action("navgraph-load", action_navgraph_load, 0);
-  add_user_end_kernel_hook(finalize);
+	make_and_declare_action("navgraph-load", action_navgraph_load, 0);
+	add_user_end_kernel_hook(finalize);
 }
 
 /** Finalization function for the OpenPRS module. */
-extern "C"
-void finalize()
+extern "C" void
+finalize()
 {
-  printf("*** DESTROYING mod_navgraph\n");
-  delete g_config;
-  g_config = NULL;
-  delete g_fnet_client;
-  g_fnet_client = NULL;
-  delete g_navgraph;
-  g_navgraph = NULL;
+	printf("*** DESTROYING mod_navgraph\n");
+	delete g_config;
+	g_config = NULL;
+	delete g_fnet_client;
+	g_fnet_client = NULL;
+	delete g_navgraph;
+	g_navgraph = NULL;
 }
--- src/plugins/openprs-agent/oprs_protobuf.h
+++ src/plugins/openprs-agent/oprs_protobuf.h
@@ -37,179 +37,213 @@
 #ifndef _OPENPRS_AGENT_OPRS_PROTOBUF_H_
 #define _OPENPRS_AGENT_OPRS_PROTOBUF_H_
 
-#include <oprs-type-pub.h>
-#include <oprs-type_f-pub.h>
+#include <core/threading/mutex.h>
+#include <core/utils/lock_queue.h>
+#include <protobuf_comm/server.h>
 
 #include <list>
 #include <map>
 #include <memory>
-
-#include <protobuf_comm/server.h>
-#include <core/threading/mutex.h>
-#include <core/utils/lock_queue.h>
+#include <oprs-type-pub.h>
+#include <oprs-type_f-pub.h>
 
 namespace protobuf_comm {
-  class ProtobufStreamClient;
-  class ProtobufBroadcastPeer;
-}
+class ProtobufStreamClient;
+class ProtobufBroadcastPeer;
+} // namespace protobuf_comm
 
 namespace oprs_protobuf {
 
 class OpenPRSProtobuf
 {
- public:
-  OpenPRSProtobuf(std::vector<std::string> &proto_path);
-  ~OpenPRSProtobuf();
+public:
+	OpenPRSProtobuf(std::vector<std::string> &proto_path);
+	~OpenPRSProtobuf();
 
-  /** Get Protobuf server.
+	/** Get Protobuf server.
    * @return protobuf server */
-  protobuf_comm::ProtobufStreamServer *  server() const
-  { return server_; }
+	protobuf_comm::ProtobufStreamServer *
+	server() const
+	{
+		return server_;
+	}
 
-  /** Get protobuf_comm peers.
+	/** Get protobuf_comm peers.
    * @return protobuf_comm peer */
-  const std::map<long int, protobuf_comm::ProtobufBroadcastPeer *> &
-  peers() const
-  { return peers_; }
+	const std::map<long int, protobuf_comm::ProtobufBroadcastPeer *> &
+	peers() const
+	{
+		return peers_;
+	}
 
-  /** Get the communicator's message register.
+	/** Get the communicator's message register.
    * @return message register */
-  protobuf_comm::MessageRegister &  message_register()
-  { return *message_register_; }
+	protobuf_comm::MessageRegister &
+	message_register()
+	{
+		return *message_register_;
+	}
 
-  /** Signal invoked for a message that has been sent to a server client.
+	/** Signal invoked for a message that has been sent to a server client.
    * @return signal
    */
-  boost::signals2::signal<void (protobuf_comm::ProtobufStreamServer::ClientID,
-				std::shared_ptr<google::protobuf::Message>)> &
-    signal_server_sent() { return sig_server_sent_; }
+	boost::signals2::signal<void(protobuf_comm::ProtobufStreamServer::ClientID,
+	                             std::shared_ptr<google::protobuf::Message>)> &
+	signal_server_sent()
+	{
+		return sig_server_sent_;
+	}
 
-  /** Signal invoked for a message that has been sent to a client.
+	/** Signal invoked for a message that has been sent to a client.
    * @return signal
    */
-  boost::signals2::signal<void (std::string, unsigned short,
-				std::shared_ptr<google::protobuf::Message>)> &
-    signal_client_sent() { return sig_client_sent_; }
+	boost::signals2::signal<
+	  void(std::string, unsigned short, std::shared_ptr<google::protobuf::Message>)> &
+	signal_client_sent()
+	{
+		return sig_client_sent_;
+	}
 
-  /** Signal invoked for a message that has been sent via broadcast.
+	/** Signal invoked for a message that has been sent via broadcast.
    * @return signal
    */
-  boost::signals2::signal<void (long int, std::shared_ptr<google::protobuf::Message>)> &
-    signal_peer_sent() { return sig_peer_sent_; }
+	boost::signals2::signal<void(long int, std::shared_ptr<google::protobuf::Message>)> &
+	signal_peer_sent()
+	{
+		return sig_peer_sent_;
+	}
 
-  bool          oprs_pb_register_type(std::string full_name);
-  Term *        oprs_pb_field_names(void *msgptr);
-  bool          oprs_pb_has_field(void *msgptr, std::string field_name);
-  Term *        oprs_pb_field_value(void *msgptr, std::string field_name);
-  Term *        oprs_pb_field_type(void *msgptr, std::string field_name);
-  Term *        oprs_pb_field_label(void *msgptr, std::string field_name);
-  Term *        oprs_pb_field_list(void *msgptr, std::string field_name);
-  bool          oprs_pb_field_is_list(void *msgptr, std::string field_name);
-  std::shared_ptr<google::protobuf::Message> *  oprs_create_msg(std::string full_name);
-  Term *        oprs_pb_ref(void *msgptr);
-  Term *        oprs_pb_destroy(void *msgptr);
-  void          oprs_pb_set_field(void *msgptr, std::string field_name, Term *value);
-  void          oprs_pb_add_list(void *msgptr, std::string field_name, Term *value);
-  void          oprs_pb_send(long int client_id, void *msgptr);
-  Term *        oprs_pb_client_connect(std::string host, int port);
-  void          oprs_pb_disconnect(long int client_id);
-  void          oprs_pb_broadcast(long int peer_id, void *msgptr);
-  void          oprs_pb_enable_server(int port);
-  void          oprs_pb_disable_server();
+	bool  oprs_pb_register_type(std::string full_name);
+	Term *oprs_pb_field_names(void *msgptr);
+	bool  oprs_pb_has_field(void *msgptr, std::string field_name);
+	Term *oprs_pb_field_value(void *msgptr, std::string field_name);
+	Term *oprs_pb_field_type(void *msgptr, std::string field_name);
+	Term *oprs_pb_field_label(void *msgptr, std::string field_name);
+	Term *oprs_pb_field_list(void *msgptr, std::string field_name);
+	bool  oprs_pb_field_is_list(void *msgptr, std::string field_name);
+	std::shared_ptr<google::protobuf::Message> *oprs_create_msg(std::string full_name);
+	Term *                                      oprs_pb_ref(void *msgptr);
+	Term *                                      oprs_pb_destroy(void *msgptr);
+	void  oprs_pb_set_field(void *msgptr, std::string field_name, Term *value);
+	void  oprs_pb_add_list(void *msgptr, std::string field_name, Term *value);
+	void  oprs_pb_send(long int client_id, void *msgptr);
+	Term *oprs_pb_client_connect(std::string host, int port);
+	void  oprs_pb_disconnect(long int client_id);
+	void  oprs_pb_broadcast(long int peer_id, void *msgptr);
+	void  oprs_pb_enable_server(int port);
+	void  oprs_pb_disable_server();
 
-  Term *         oprs_pb_peer_create(const std::string& host, int port);
-  Term *         oprs_pb_peer_create_local(const std::string& host,
-                                           int send_port, int recv_port);
-	Term *         oprs_pb_peer_create_crypto(const std::string& host, int port,
-	                                          const std::string& crypto_key = "",
-	                                          const std::string& cipher = "");
-  Term *         oprs_pb_peer_create_local_crypto(const std::string& host,
-                                                  int send_port, int recv_port,
-                                                  const std::string& crypto_key = "",
-                                                  const std::string& cipher = "");
-	void          oprs_pb_peer_destroy(long int peer_id);
-	void          oprs_pb_peer_setup_crypto(long int peer_id,
-	                                        const std::string& crypto_key,
-	                                        const std::string& cipher);
+	Term *oprs_pb_peer_create(const std::string &host, int port);
+	Term *oprs_pb_peer_create_local(const std::string &host, int send_port, int recv_port);
+	Term *oprs_pb_peer_create_crypto(const std::string &host,
+	                                 int                port,
+	                                 const std::string &crypto_key = "",
+	                                 const std::string &cipher     = "");
+	Term *oprs_pb_peer_create_local_crypto(const std::string &host,
+	                                       int                send_port,
+	                                       int                recv_port,
+	                                       const std::string &crypto_key = "",
+	                                       const std::string &cipher     = "");
+	void  oprs_pb_peer_destroy(long int peer_id);
+	void  oprs_pb_peer_setup_crypto(long int           peer_id,
+	                                const std::string &crypto_key,
+	                                const std::string &cipher);
 
-  bool          oprs_pb_events_pending();
-  void          oprs_pb_process();
+	bool oprs_pb_events_pending();
+	void oprs_pb_process();
 
- private:
-  typedef enum {
-    CT_SERVER, CT_CLIENT, CT_PEER
-  } ClientType;
-  void clips_assert_message(std::pair<std::string, unsigned short> &endpoint,
-			    uint16_t comp_id, uint16_t msg_type,
-			    std::shared_ptr<google::protobuf::Message> &msg,
-			    ClientType ct, unsigned int client_id = 0);
-  void handle_server_client_connected(protobuf_comm::ProtobufStreamServer::ClientID client,
-				      boost::asio::ip::tcp::endpoint &endpoint);
-  void handle_server_client_disconnected(protobuf_comm::ProtobufStreamServer::ClientID client,
-					 const boost::system::error_code &error);
+private:
+	typedef enum { CT_SERVER, CT_CLIENT, CT_PEER } ClientType;
+	void clips_assert_message(std::pair<std::string, unsigned short> &    endpoint,
+	                          uint16_t                                    comp_id,
+	                          uint16_t                                    msg_type,
+	                          std::shared_ptr<google::protobuf::Message> &msg,
+	                          ClientType                                  ct,
+	                          unsigned int                                client_id = 0);
+	void handle_server_client_connected(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                                    boost::asio::ip::tcp::endpoint &              endpoint);
+	void handle_server_client_disconnected(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                                       const boost::system::error_code &             error);
 
-  void handle_server_client_msg(protobuf_comm::ProtobufStreamServer::ClientID client,
-				uint16_t component_id, uint16_t msg_type,
-				std::shared_ptr<google::protobuf::Message> msg);
+	void handle_server_client_msg(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                              uint16_t                                      component_id,
+	                              uint16_t                                      msg_type,
+	                              std::shared_ptr<google::protobuf::Message>    msg);
 
-  void handle_server_client_fail(protobuf_comm::ProtobufStreamServer::ClientID client,
-				 uint16_t component_id, uint16_t msg_type,
-				 std::string msg);
+	void handle_server_client_fail(protobuf_comm::ProtobufStreamServer::ClientID client,
+	                               uint16_t                                      component_id,
+	                               uint16_t                                      msg_type,
+	                               std::string                                   msg);
 
-  void handle_peer_msg(long int peer_id,
-		       boost::asio::ip::udp::endpoint &endpoint,
-		       uint16_t component_id, uint16_t msg_type,
-		       std::shared_ptr<google::protobuf::Message> msg);
-  void handle_peer_recv_error(long int peer_id, boost::asio::ip::udp::endpoint &endpoint, std::string msg);
-  void handle_peer_send_error(long int peer_id, const std::string& msg);
+	void handle_peer_msg(long int                                   peer_id,
+	                     boost::asio::ip::udp::endpoint &           endpoint,
+	                     uint16_t                                   component_id,
+	                     uint16_t                                   msg_type,
+	                     std::shared_ptr<google::protobuf::Message> msg);
+	void handle_peer_recv_error(long int                        peer_id,
+	                            boost::asio::ip::udp::endpoint &endpoint,
+	                            std::string                     msg);
+	void handle_peer_send_error(long int peer_id, const std::string &msg);
 
-  void handle_client_connected(long int client_id);
-  void handle_client_disconnected(long int client_id,
-				    const boost::system::error_code &error);
-  void handle_client_msg(long int client_id,
-                         uint16_t comp_id, uint16_t msg_type,
-                         std::shared_ptr<google::protobuf::Message> msg);
-  void handle_client_receive_fail(long int client_id,
-                                  uint16_t comp_id, uint16_t msg_type,
-                                  const std::string& msg);
-  void oprs_assert_message(std::string &endpoint_host, unsigned short endpoint_port,
-                           uint16_t comp_id, uint16_t msg_type,
-                           std::shared_ptr<google::protobuf::Message> &msg,
-                           OpenPRSProtobuf::ClientType ct,
-                           unsigned int client_id);
-  void oprs_assert_server_client_event(long int client_id,
-				       std::string &host, unsigned short port, bool connect);
-  void oprs_assert_client_event(long int client_id, bool connect);
+	void handle_client_connected(long int client_id);
+	void handle_client_disconnected(long int client_id, const boost::system::error_code &error);
+	void handle_client_msg(long int                                   client_id,
+	                       uint16_t                                   comp_id,
+	                       uint16_t                                   msg_type,
+	                       std::shared_ptr<google::protobuf::Message> msg);
+	void handle_client_receive_fail(long int           client_id,
+	                                uint16_t           comp_id,
+	                                uint16_t           msg_type,
+	                                const std::string &msg);
+	void oprs_assert_message(std::string &                               endpoint_host,
+	                         unsigned short                              endpoint_port,
+	                         uint16_t                                    comp_id,
+	                         uint16_t                                    msg_type,
+	                         std::shared_ptr<google::protobuf::Message> &msg,
+	                         OpenPRSProtobuf::ClientType                 ct,
+	                         unsigned int                                client_id);
+	void oprs_assert_server_client_event(long int       client_id,
+	                                     std::string &  host,
+	                                     unsigned short port,
+	                                     bool           connect);
+	void oprs_assert_client_event(long int client_id, bool connect);
 
- private:
+private:
 	std::shared_ptr<protobuf_comm::MessageRegister> message_register_;
-  protobuf_comm::ProtobufStreamServer *server_;
+	protobuf_comm::ProtobufStreamServer *           server_;
 
-  boost::signals2::signal<void (protobuf_comm::ProtobufStreamServer::ClientID,
-				std::shared_ptr<google::protobuf::Message>)> sig_server_sent_;
-  boost::signals2::signal<void (std::string, unsigned short,
-				std::shared_ptr<google::protobuf::Message>)> sig_client_sent_;
-  boost::signals2::signal<void (long int, std::shared_ptr<google::protobuf::Message>)> sig_peer_sent_;
-  
-  fawkes::Mutex map_mutex_;
-  long int next_client_id_;
+	boost::signals2::signal<void(protobuf_comm::ProtobufStreamServer::ClientID,
+	                             std::shared_ptr<google::protobuf::Message>)>
+	  sig_server_sent_;
+	boost::signals2::signal<
+	  void(std::string, unsigned short, std::shared_ptr<google::protobuf::Message>)>
+	  sig_client_sent_;
+	boost::signals2::signal<void(long int, std::shared_ptr<google::protobuf::Message>)>
+	  sig_peer_sent_;
 
+	fawkes::Mutex map_mutex_;
+	long int      next_client_id_;
 
-  std::map<long int, protobuf_comm::ProtobufStreamServer::ClientID> server_clients_;
-  typedef std::map<protobuf_comm::ProtobufStreamServer::ClientID, long int> RevServerClientMap;
-  RevServerClientMap rev_server_clients_;
-  std::map<long int, protobuf_comm::ProtobufStreamClient *>  clients_;
-  std::map<long int, protobuf_comm::ProtobufBroadcastPeer *> peers_;
+	std::map<long int, protobuf_comm::ProtobufStreamServer::ClientID>         server_clients_;
+	typedef std::map<protobuf_comm::ProtobufStreamServer::ClientID, long int> RevServerClientMap;
+	RevServerClientMap                                                        rev_server_clients_;
+	std::map<long int, protobuf_comm::ProtobufStreamClient *>                 clients_;
+	std::map<long int, protobuf_comm::ProtobufBroadcastPeer *>                peers_;
 
-  std::map<long int, std::pair<std::string, unsigned short>> client_endpoints_;
+	std::map<long int, std::pair<std::string, unsigned short>> client_endpoints_;
 
-  fawkes::LockQueue<
-    std::tuple<std::string, unsigned short, uint16_t, uint16_t,
-               std::shared_ptr<google::protobuf::Message>, ClientType, unsigned int>> q_msgs_;
-  fawkes::LockQueue<std::tuple<long int, std::string, unsigned short, bool>> q_server_client_;
-  fawkes::LockQueue<std::tuple<long int, bool>> q_client_;
+	fawkes::LockQueue<std::tuple<std::string,
+	                             unsigned short,
+	                             uint16_t,
+	                             uint16_t,
+	                             std::shared_ptr<google::protobuf::Message>,
+	                             ClientType,
+	                             unsigned int>>
+	                                                                           q_msgs_;
+	fawkes::LockQueue<std::tuple<long int, std::string, unsigned short, bool>> q_server_client_;
+	fawkes::LockQueue<std::tuple<long int, bool>>                              q_client_;
 };
 
-} // end namespace protobuf_clips
+} // namespace oprs_protobuf
 
 #endif
