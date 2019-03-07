319d44d3c750fa68171ec7f7dbfdf461753a36c0
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:47:25 +0100

    gazsim-localization: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/gazebo/gazsim-localization/gazsim_localization_thread.cpp
+++ src/plugins/gazebo/gazsim-localization/gazsim_localization_thread.cpp
@@ -21,20 +21,19 @@
 
 #include "gazsim_localization_thread.h"
 
-#include <tf/types.h>
-#include <stdio.h>
-#include <math.h>
-#include <utils/math/angle.h>
-#include <core/threading/mutex_locker.h>
-
-#include <interfaces/Position3DInterface.h>
-
-#include <gazebo/transport/Node.hh>
-#include <gazebo/msgs/msgs.hh>
-#include <gazebo/transport/transport.hh>
 #include <aspect/logging.h>
+#include <core/threading/mutex_locker.h>
+#include <interfaces/Position3DInterface.h>
 #include <tf/transform_publisher.h>
+#include <tf/types.h>
+#include <utils/math/angle.h>
 #include <utils/time/clock.h>
+
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/Node.hh>
+#include <gazebo/transport/transport.hh>
+#include <math.h>
+#include <stdio.h>
 
 using namespace fawkes;
 using namespace gazebo;
@@ -46,79 +45,84 @@ using namespace gazebo;
 
 /** Constructor. */
 LocalizationSimThread::LocalizationSimThread()
-  : Thread("LocalizationSimThread", Thread::OPMODE_WAITFORWAKEUP),
-    BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_SENSOR_PROCESS),
-    TransformAspect(TransformAspect::BOTH, "Pose")
+: Thread("LocalizationSimThread", Thread::OPMODE_WAITFORWAKEUP),
+  BlockedTimingAspect(BlockedTimingAspect::WAKEUP_HOOK_SENSOR_PROCESS),
+  TransformAspect(TransformAspect::BOTH, "Pose")
 {
 }
 
-void LocalizationSimThread::init()
+void
+LocalizationSimThread::init()
 {
-  logger->log_debug(name(), "Initializing Simulation of the Localization");
+	logger->log_debug(name(), "Initializing Simulation of the Localization");
 
-  //open interface
-  localization_if_ = blackboard->open_for_writing<Position3DInterface>("Pose");
+	//open interface
+	localization_if_ = blackboard->open_for_writing<Position3DInterface>("Pose");
 
-  //read cofig values
-  gps_topic_ = config->get_string("/gazsim/topics/gps");
-  transform_tolerance_ = config->get_float("/plugins/amcl/transform_tolerance");
-  global_frame_id_ = config->get_string("/plugins/amcl/global_frame_id");
-  odom_frame_id_ = config->get_string("/plugins/amcl/odom_frame_id");
+	//read cofig values
+	gps_topic_           = config->get_string("/gazsim/topics/gps");
+	transform_tolerance_ = config->get_float("/plugins/amcl/transform_tolerance");
+	global_frame_id_     = config->get_string("/plugins/amcl/global_frame_id");
+	odom_frame_id_       = config->get_string("/plugins/amcl/odom_frame_id");
 
-  //subscribing to gazebo publisher
-  localization_sub_ = gazebonode->Subscribe(gps_topic_, &LocalizationSimThread::on_localization_msg, this);
+	//subscribing to gazebo publisher
+	localization_sub_ =
+	  gazebonode->Subscribe(gps_topic_, &LocalizationSimThread::on_localization_msg, this);
 
-  new_data_ = false;
+	new_data_ = false;
 }
 
-void LocalizationSimThread::finalize()
+void
+LocalizationSimThread::finalize()
 {
-  blackboard->close(localization_if_);
+	blackboard->close(localization_if_);
 }
 
-void LocalizationSimThread::loop()
+void
+LocalizationSimThread::loop()
 {
-  if(new_data_)
-  {
-    //write interface
-    localization_if_->set_frame(global_frame_id_.c_str());
-    localization_if_->set_visibility_history(100);
-    localization_if_->set_translation(0, x_);
-    localization_if_->set_translation(1, y_);
-    localization_if_->set_translation(2, z_);
-    localization_if_->set_rotation(0, quat_x_);
-    localization_if_->set_rotation(1, quat_y_);
-    localization_if_->set_rotation(2, quat_z_);
-    localization_if_->set_rotation(3, quat_w_);
-    localization_if_->write();
+	if (new_data_) {
+		//write interface
+		localization_if_->set_frame(global_frame_id_.c_str());
+		localization_if_->set_visibility_history(100);
+		localization_if_->set_translation(0, x_);
+		localization_if_->set_translation(1, y_);
+		localization_if_->set_translation(2, z_);
+		localization_if_->set_rotation(0, quat_x_);
+		localization_if_->set_rotation(1, quat_y_);
+		localization_if_->set_rotation(2, quat_z_);
+		localization_if_->set_rotation(3, quat_w_);
+		localization_if_->write();
 
-    //publish transform (similar as in amcl_thread.cpp)
-    tf::Quaternion rotation = tf::Quaternion(quat_x_, quat_y_, quat_z_, quat_w_);
-    tf::Point position = tf::Point(x_, y_, z_);
-    tf::Transform latest_tf_ = tf::Transform(rotation,position);
-    Time transform_expiration =
-      (clock->now() + transform_tolerance_);
-    tf_publisher->send_transform(latest_tf_, transform_expiration,
-				 global_frame_id_, odom_frame_id_);
+		//publish transform (similar as in amcl_thread.cpp)
+		tf::Quaternion rotation             = tf::Quaternion(quat_x_, quat_y_, quat_z_, quat_w_);
+		tf::Point      position             = tf::Point(x_, y_, z_);
+		tf::Transform  latest_tf_           = tf::Transform(rotation, position);
+		Time           transform_expiration = (clock->now() + transform_tolerance_);
+		tf_publisher->send_transform(latest_tf_,
+		                             transform_expiration,
+		                             global_frame_id_,
+		                             odom_frame_id_);
 
-    new_data_ = false;
-  }
+		new_data_ = false;
+	}
 }
 
-void LocalizationSimThread::on_localization_msg(ConstPosePtr &msg)
+void
+LocalizationSimThread::on_localization_msg(ConstPosePtr &msg)
 {
-  //logger->log_info(name(), "Got new Localization data.\n");
-  
-  MutexLocker lock(loop_mutex);
+	//logger->log_info(name(), "Got new Localization data.\n");
 
-  //read data from message
-  x_ = msg->position().x();
-  y_ = msg->position().y();
-  z_ = msg->position().z();
-  quat_x_ = msg->orientation().x();
-  quat_y_ = msg->orientation().y();
-  quat_z_ = msg->orientation().z();
-  quat_w_ = msg->orientation().w();
-  
-  new_data_ = true;
+	MutexLocker lock(loop_mutex);
+
+	//read data from message
+	x_      = msg->position().x();
+	y_      = msg->position().y();
+	z_      = msg->position().z();
+	quat_x_ = msg->orientation().x();
+	quat_y_ = msg->orientation().y();
+	quat_z_ = msg->orientation().z();
+	quat_w_ = msg->orientation().w();
+
+	new_data_ = true;
 }
