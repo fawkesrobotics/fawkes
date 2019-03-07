6af8483c3dd8cee307764f2615008e812a6b67d4
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:48:41 +0100

    plugin_generator: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/tools/plugin_generator/main.cpp
+++ src/tools/plugin_generator/main.cpp
@@ -19,11 +19,13 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#include <string>
+#include "plugin_generator.h"
+
+#include <utils/system/argparser.h>
+
 #include <cstdio>
 #include <ctime>
-#include <utils/system/argparser.h>
-#include "plugin_generator.h"
+#include <string>
 
 using namespace std;
 using namespace fawkes;
@@ -31,65 +33,70 @@ using namespace fawkes;
 void
 print_usage(const char *program_name)
 {
-  printf("Usage: %s [-h] <author_name> <plugin_name> <description> <directory> \n"
-	 "Example: %s \"John Doe\" robot_mover \"Move the robot a meter forward\" \n"
-	 "                            ~/fawkes/src/plugins/robot_mover/"
-	 "\n"
-         "-h  Print this usage information\n\n"
-         "Generate the necessary files to build a fawkes plugin\n",
-	 program_name, program_name);
+	printf("Usage: %s [-h] <author_name> <plugin_name> <description> <directory> \n"
+	       "Example: %s \"John Doe\" robot_mover \"Move the robot a meter forward\" \n"
+	       "                            ~/fawkes/src/plugins/robot_mover/"
+	       "\n"
+	       "-h  Print this usage information\n\n"
+	       "Generate the necessary files to build a fawkes plugin\n",
+	       program_name,
+	       program_name);
 }
 
 void
-generate_plugin(std::string author_name, std::string plugin_name, std::string description, std::string directory)
+generate_plugin(std::string author_name,
+                std::string plugin_name,
+                std::string description,
+                std::string directory)
 {
-  time_t now = time(0);
-  std::string date = ctime(&now);
-  tm *time_structure = localtime(&now);
-  std::string year = std::to_string(time_structure->tm_year + 1900);
-  //TimeStructure's year is the number of years since 1900
+	time_t      now            = time(0);
+	std::string date           = ctime(&now);
+	tm *        time_structure = localtime(&now);
+	std::string year           = std::to_string(time_structure->tm_year + 1900);
+	//TimeStructure's year is the number of years since 1900
 
-  PluginGenerator *generator = new PluginGenerator(directory, author_name, year , date, plugin_name, description);
-  generator->generate();
+	PluginGenerator *generator =
+	  new PluginGenerator(directory, author_name, year, date, plugin_name, description);
+	generator->generate();
 }
 
 bool
-plugin_name_valid(std::string plugin_name){
-  for (char& c : plugin_name){
-    if (! isalpha(c) && c != '-' && c != '_') {
-      return false;
-    }
-  }
-  return true;
+plugin_name_valid(std::string plugin_name)
+{
+	for (char &c : plugin_name) {
+		if (!isalpha(c) && c != '-' && c != '_') {
+			return false;
+		}
+	}
+	return true;
 }
 
 int
 main(int argc, char **argv)
 {
-  ArgumentParser argp(argc, argv, "h");
+	ArgumentParser argp(argc, argv, "h");
 
-  if ( argp.has_arg("h") ) {
-    print_usage(argv[0]);
-    exit(0);
-  }
+	if (argp.has_arg("h")) {
+		print_usage(argv[0]);
+		exit(0);
+	}
 
-  std::string author_name, plugin_name, description, directory;
-  if (argp.num_items() != 4) {
-    printf("ERROR: Invalid number of arguments\n");
-    print_usage(argv[0]);
-    exit(1);
-  } else if (!plugin_name_valid(argp.items()[1])) {
-    printf("ERROR: Invalid plugin name: Only alphanumerical chars allowed. \n"
-        "To separate multiple words use '-' or '_'\n");
-    exit(2);
-  }
-    else {
-    author_name = argp.items()[0];
-    plugin_name = argp.items()[1];
-    description = argp.items()[2];
-    directory = argp.items()[3];
-    generate_plugin(author_name, plugin_name, description, directory);
-  }
+	std::string author_name, plugin_name, description, directory;
+	if (argp.num_items() != 4) {
+		printf("ERROR: Invalid number of arguments\n");
+		print_usage(argv[0]);
+		exit(1);
+	} else if (!plugin_name_valid(argp.items()[1])) {
+		printf("ERROR: Invalid plugin name: Only alphanumerical chars allowed. \n"
+		       "To separate multiple words use '-' or '_'\n");
+		exit(2);
+	} else {
+		author_name = argp.items()[0];
+		plugin_name = argp.items()[1];
+		description = argp.items()[2];
+		directory   = argp.items()[3];
+		generate_plugin(author_name, plugin_name, description, directory);
+	}
 
-  return 0;
+	return 0;
 }
