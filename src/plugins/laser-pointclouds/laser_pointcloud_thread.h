86f5cc570caad09fef5c27d3bc1071228cf88230
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:48:33 +0100

    laser-pointclouds: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/laser-pointclouds/laser_pointcloud_thread.h
+++ src/plugins/laser-pointclouds/laser_pointcloud_thread.h
@@ -23,82 +23,86 @@
 #define _PLUGINS_LASER_POINTCLOUDS_LASER_POINTCLOUD_THREAD_H_
 
 // must be first for reliable ROS detection
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-
-#include <core/threading/thread.h>
+#include <aspect/blackboard.h>
 #include <aspect/blocked_timing.h>
 #include <aspect/logging.h>
-#include <aspect/blackboard.h>
 #include <aspect/pointcloud.h>
 #include <blackboard/interface_listener.h>
 #include <blackboard/interface_observer.h>
+#include <core/threading/thread.h>
 #include <core/utils/lock_list.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 namespace fawkes {
-  class Interface;
-  class Laser360Interface;
-  class Laser720Interface;
-  class Laser1080Interface;
-}
+class Interface;
+class Laser360Interface;
+class Laser720Interface;
+class Laser1080Interface;
+} // namespace fawkes
 
-class LaserPointCloudThread
-: public fawkes::Thread,
-  public fawkes::LoggingAspect,
-  public fawkes::BlackBoardAspect,
-  public fawkes::BlockedTimingAspect,
-  public fawkes::PointCloudAspect,
-  public fawkes::BlackBoardInterfaceObserver,
-  public fawkes::BlackBoardInterfaceListener
+class LaserPointCloudThread : public fawkes::Thread,
+                              public fawkes::LoggingAspect,
+                              public fawkes::BlackBoardAspect,
+                              public fawkes::BlockedTimingAspect,
+                              public fawkes::PointCloudAspect,
+                              public fawkes::BlackBoardInterfaceObserver,
+                              public fawkes::BlackBoardInterfaceListener
 {
- public:
-  LaserPointCloudThread();
-  virtual ~LaserPointCloudThread();
+public:
+	LaserPointCloudThread();
+	virtual ~LaserPointCloudThread();
 
-  virtual void init();
-  virtual void loop();
-  virtual void finalize();
+	virtual void init();
+	virtual void loop();
+	virtual void finalize();
 
-  // for BlackBoardInterfaceObserver
-  virtual void bb_interface_created(const char *type, const char *id) throw();
+	// for BlackBoardInterfaceObserver
+	virtual void bb_interface_created(const char *type, const char *id) throw();
 
-  // for BlackBoardInterfaceListener
-  virtual void bb_interface_writer_removed(fawkes::Interface *interface,
-                                           unsigned int instance_serial) throw();
-  virtual void bb_interface_reader_removed(fawkes::Interface *interface,
-                                           unsigned int instance_serial) throw();
+	// for BlackBoardInterfaceListener
+	virtual void bb_interface_writer_removed(fawkes::Interface *interface,
+	                                         unsigned int       instance_serial) throw();
+	virtual void bb_interface_reader_removed(fawkes::Interface *interface,
+	                                         unsigned int       instance_serial) throw();
 
- private:
-  void conditional_close(fawkes::Interface *interface) throw();
-  std::string interface_to_pcl_name(const char *interface_id);
+private:
+	void        conditional_close(fawkes::Interface *interface) throw();
+	std::string interface_to_pcl_name(const char *interface_id);
 
- /** Stub to see name in backtrace for easier debugging. @see Thread::run() */
- protected: virtual void run() { Thread::run(); }
+	/** Stub to see name in backtrace for easier debugging. @see Thread::run() */
+protected:
+	virtual void
+	run()
+	{
+		Thread::run();
+	}
 
- private:
-  /// @cond INTERNALS
-  typedef struct {
-    std::string id;
-    unsigned int size;
-    union {
-      fawkes::Laser360Interface *as360;
-      fawkes::Laser720Interface *as720;
-      fawkes::Laser1080Interface *as1080;
-    } interface_typed;
-    fawkes::Interface *interface;
+private:
+	/// @cond INTERNALS
+	typedef struct
+	{
+		std::string  id;
+		unsigned int size;
+		union {
+			fawkes::Laser360Interface * as360;
+			fawkes::Laser720Interface * as720;
+			fawkes::Laser1080Interface *as1080;
+		} interface_typed;
+		fawkes::Interface *interface;
 
-    fawkes::RefPtr<pcl::PointCloud<pcl::PointXYZ> > cloud;
-  } InterfaceCloudMapping;
-  /// @endcond
+		fawkes::RefPtr<pcl::PointCloud<pcl::PointXYZ>> cloud;
+	} InterfaceCloudMapping;
+	/// @endcond
 
-  fawkes::LockList<InterfaceCloudMapping> mappings_;
+	fawkes::LockList<InterfaceCloudMapping> mappings_;
 
-  float sin_angles360[360];
-  float cos_angles360[360];
-  float sin_angles720[720];
-  float cos_angles720[720];
-  float sin_angles1080[1080];
-  float cos_angles1080[1080];
+	float sin_angles360[360];
+	float cos_angles360[360];
+	float sin_angles720[720];
+	float cos_angles720[720];
+	float sin_angles1080[1080];
+	float cos_angles1080[1080];
 };
 
 #endif
