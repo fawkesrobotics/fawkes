e9f4d2cc7da421e7f2fe40d473499d6aa752a252
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 21:21:11 +0100

    buildsys: fix formatting check script for broken clang-format
    Running a recent clang-format (tested with 7.0.1) yields replacements in
    the XML output even though the file is properly formatted and another
    clang-format run would not change it.
    
    Work around by comparing the original and a reformatted file. The effort
    is marginally higher. The old version also ran a full clang-format, but
    could avoid the full diff. But it's still fast.

--- etc/format-scripts/check-all-files.sh
+++ etc/format-scripts/check-all-files.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#  Copyright (c) 2018 Tim Niemueller [www.niemueller.de]
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  Read the full text in the LICENSE.GPL file in the doc directory.
+
 SCRIPT_PATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 ALL_FILES=$(git ls-files *.{h,cpp})
