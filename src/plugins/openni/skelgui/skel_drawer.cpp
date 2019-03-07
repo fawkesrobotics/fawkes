52569c17cc140b6333e5ee3b2439cf20076ef6c1
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:47:38 +0100

    skelgui: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/openni/skelgui/skel_drawer.cpp
+++ src/plugins/openni/skelgui/skel_drawer.cpp
@@ -22,14 +22,12 @@
 
 #include "skel_drawer.h"
 
+#include <GL/glut.h>
+#include <plugins/openni/utils/colors.h>
 #include <utils/math/angle.h>
 
-#include <plugins/openni/utils/colors.h>
-
-
-#include <cstring>
 #include <cstdio>
-#include <GL/glut.h>
+#include <cstring>
 
 using namespace fawkes;
 using namespace fawkes::openni;
@@ -45,134 +43,132 @@ using namespace fawkes::openni;
  * @param hands map of hands shared with interface observer
  */
 SkelGuiSkeletonDrawer::SkelGuiSkeletonDrawer(UserMap &users, HandMap &hands)
-  : users_(users), hands_(hands)
+: users_(users), hands_(hands)
 {
-  print_state_ = PRINT_ID_STATE;
+	print_state_ = PRINT_ID_STATE;
 }
 
 void
 SkelGuiSkeletonDrawer::print_string(void *font, char *str)
 {
-  const int l = strlen(str);
-  for(int i = 0; i < l; ++i)  glutBitmapCharacter(font, *str++);
+	const int l = strlen(str);
+	for (int i = 0; i < l; ++i)
+		glutBitmapCharacter(font, *str++);
 }
 
 void
-SkelGuiSkeletonDrawer::draw_limb(float *proj1, float conf1,
-				 float *proj2, float conf2)
+SkelGuiSkeletonDrawer::draw_limb(float *proj1, float conf1, float *proj2, float conf2)
 {
-  if (conf1 < 0.5 || conf2 < 0.5)  return;
+	if (conf1 < 0.5 || conf2 < 0.5)
+		return;
 
-  glVertex3i(proj1[0], proj1[1], 0);
-  glVertex3i(proj2[0], proj2[1], 0);
+	glVertex3i(proj1[0], proj1[1], 0);
+	glVertex3i(proj2[0], proj2[1], 0);
 }
 
-#define DRAW_LIMB(user, joint1, joint2)					\
-  draw_limb(user.proj_if->proj_##joint1(),				\
-            user.skel_if->pos_##joint1##_confidence(),			\
-	    user.proj_if->proj_##joint2(),				\
-	    user.skel_if->pos_##joint2##_confidence());
+#define DRAW_LIMB(user, joint1, joint2)                \
+	draw_limb(user.proj_if->proj_##joint1(),             \
+	          user.skel_if->pos_##joint1##_confidence(), \
+	          user.proj_if->proj_##joint2(),             \
+	          user.skel_if->pos_##joint2##_confidence());
 
 void
 SkelGuiSkeletonDrawer::draw_user(UserInfo &user)
 {
-  if (user.skel_if->state() != HumanSkeletonInterface::STATE_TRACKING)  return;
+	if (user.skel_if->state() != HumanSkeletonInterface::STATE_TRACKING)
+		return;
 
-  DRAW_LIMB(user, head, neck);
-  
-  DRAW_LIMB(user, neck, left_shoulder);
-  DRAW_LIMB(user, left_shoulder, left_elbow);
-  DRAW_LIMB(user, left_elbow, left_hand);
-  
-  DRAW_LIMB(user, neck, right_shoulder);
-  DRAW_LIMB(user, right_shoulder, right_elbow);
-  DRAW_LIMB(user, right_elbow, right_hand);
+	DRAW_LIMB(user, head, neck);
 
-  DRAW_LIMB(user, left_shoulder, torso);
-  DRAW_LIMB(user, right_shoulder, torso);
+	DRAW_LIMB(user, neck, left_shoulder);
+	DRAW_LIMB(user, left_shoulder, left_elbow);
+	DRAW_LIMB(user, left_elbow, left_hand);
 
-  DRAW_LIMB(user, torso, left_hip);
-  DRAW_LIMB(user, left_hip, left_knee);
-  DRAW_LIMB(user, left_knee, left_foot);
+	DRAW_LIMB(user, neck, right_shoulder);
+	DRAW_LIMB(user, right_shoulder, right_elbow);
+	DRAW_LIMB(user, right_elbow, right_hand);
 
-  DRAW_LIMB(user, torso, right_hip);
-  DRAW_LIMB(user, right_hip, right_knee);
-  DRAW_LIMB(user, right_knee, right_foot);
+	DRAW_LIMB(user, left_shoulder, torso);
+	DRAW_LIMB(user, right_shoulder, torso);
 
-  DRAW_LIMB(user, left_hip, right_hip);
+	DRAW_LIMB(user, torso, left_hip);
+	DRAW_LIMB(user, left_hip, left_knee);
+	DRAW_LIMB(user, left_knee, left_foot);
 
+	DRAW_LIMB(user, torso, right_hip);
+	DRAW_LIMB(user, right_hip, right_knee);
+	DRAW_LIMB(user, right_knee, right_foot);
+
+	DRAW_LIMB(user, left_hip, right_hip);
 }
 
 void
 SkelGuiSkeletonDrawer::draw_circle(unsigned int id, float *proj, float radius)
 {
-  glBegin(GL_LINE_LOOP);
-  glVertex2f(proj[0], proj[1]);
-  glColor4f(1 - USER_COLORS[id % NUM_USER_COLORS][0],
-	    1 - USER_COLORS[id % NUM_USER_COLORS][1],
-	    1 - USER_COLORS[id % NUM_USER_COLORS][2],
-	    1);
-  for (int i=0; i < 360; ++i) {
-    float rad = deg2rad(i);;
-    glVertex2f( proj[0] + cos(rad) * radius, proj[1] + sin(rad) * radius);
-  }
-  glColor4f(1, 1, 1, 1);
-  glEnd();
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(proj[0], proj[1]);
+	glColor4f(1 - USER_COLORS[id % NUM_USER_COLORS][0],
+	          1 - USER_COLORS[id % NUM_USER_COLORS][1],
+	          1 - USER_COLORS[id % NUM_USER_COLORS][2],
+	          1);
+	for (int i = 0; i < 360; ++i) {
+		float rad = deg2rad(i);
+		;
+		glVertex2f(proj[0] + cos(rad) * radius, proj[1] + sin(rad) * radius);
+	}
+	glColor4f(1, 1, 1, 1);
+	glEnd();
 }
-
 
 /** Draw skeletons. */
 void
 SkelGuiSkeletonDrawer::draw()
 {
-  char label[50] = "";
-  for (UserMap::iterator i = users_.begin(); i != users_.end(); ++i) {
-    if (i->second.skel_if->state() != HumanSkeletonInterface::STATE_INVALID) {
-      if (print_state_ != PRINT_NONE) {
-	memset(label, 0, sizeof(label));
-	if (print_state_ == PRINT_ID) {
-	  sprintf(label, "%s", i->first.c_str());
+	char label[50] = "";
+	for (UserMap::iterator i = users_.begin(); i != users_.end(); ++i) {
+		if (i->second.skel_if->state() != HumanSkeletonInterface::STATE_INVALID) {
+			if (print_state_ != PRINT_NONE) {
+				memset(label, 0, sizeof(label));
+				if (print_state_ == PRINT_ID) {
+					sprintf(label, "%s", i->first.c_str());
+				} else if (i->second.skel_if->state() == HumanSkeletonInterface::STATE_TRACKING) {
+					sprintf(label, "%s - Tracking", i->first.c_str());
+				} else if (i->second.skel_if->state() == HumanSkeletonInterface::STATE_CALIBRATING) {
+					sprintf(label, "%s - Calibrating...", i->first.c_str());
+				} else {
+					sprintf(label, "%s - Looking for pose", i->first.c_str());
+				}
+
+				glColor4f(1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][0],
+				          1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][1],
+				          1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][2],
+				          1);
+
+				glRasterPos2i(i->second.proj_if->proj_com(0), i->second.proj_if->proj_com(1));
+				print_string(GLUT_BITMAP_HELVETICA_18, label);
+			}
+
+			glBegin(GL_LINES);
+			glColor4f(1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][0],
+			          1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][1],
+			          1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][2],
+			          1);
+
+			draw_user(i->second);
+			glEnd();
+		}
 	}
-	else if (i->second.skel_if->state() == HumanSkeletonInterface::STATE_TRACKING)
-	{
-	  sprintf(label, "%s - Tracking", i->first.c_str());
-	} else if (i->second.skel_if->state() == HumanSkeletonInterface::STATE_CALIBRATING)
-	{
-	  sprintf(label, "%s - Calibrating...", i->first.c_str());
-	} else {
-	sprintf(label, "%s - Looking for pose", i->first.c_str());
+
+	glEnable(GL_LINE_SMOOTH);
+	glLineWidth(4);
+	for (HandMap::iterator i = hands_.begin(); i != hands_.end(); ++i) {
+		if (i->second.hand_if->is_visible()) {
+			float proj[2] = {i->second.hand_if->world_x(), i->second.hand_if->world_y()};
+			draw_circle(i->second.hand_if->world_z(), proj, 10);
+		}
 	}
-
-	glColor4f(1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][0],
-		  1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][1],
-		  1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][2],
-		  1);
-      
-	glRasterPos2i(i->second.proj_if->proj_com(0), i->second.proj_if->proj_com(1));
-	print_string(GLUT_BITMAP_HELVETICA_18, label);
-      }
-
-      glBegin(GL_LINES);
-      glColor4f(1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][0],
-		1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][1],
-		1 - USER_COLORS[i->second.skel_if->user_id() % NUM_USER_COLORS][2],
-		1);
-
-      draw_user(i->second);
-      glEnd();
-    }
-  }
-
-  glEnable(GL_LINE_SMOOTH);
-  glLineWidth(4);
-  for (HandMap::iterator i = hands_.begin(); i != hands_.end(); ++i) {
-    if (i->second.hand_if->is_visible()) {
-      float proj[2] = {i->second.hand_if->world_x(), i->second.hand_if->world_y()};
-      draw_circle(i->second.hand_if->world_z(), proj, 10);
-    }
-  }
-  glLineWidth(1.);
-  glDisable(GL_LINE_SMOOTH);
+	glLineWidth(1.);
+	glDisable(GL_LINE_SMOOTH);
 }
 
 /** Toggle the printing state.
@@ -182,13 +178,12 @@ SkelGuiSkeletonDrawer::draw()
 void
 SkelGuiSkeletonDrawer::toggle_print_state()
 {
-  switch (print_state_) {
-  case PRINT_NONE:      print_state_ = PRINT_ID_STATE; break;
-  case PRINT_ID_STATE:  print_state_ = PRINT_ID;       break;
-  case PRINT_ID:        print_state_ = PRINT_NONE;     break;
-  }
+	switch (print_state_) {
+	case PRINT_NONE: print_state_ = PRINT_ID_STATE; break;
+	case PRINT_ID_STATE: print_state_ = PRINT_ID; break;
+	case PRINT_ID: print_state_ = PRINT_NONE; break;
+	}
 }
-
 
 /** Set print state.
  * @param state new print state
@@ -196,8 +191,8 @@ SkelGuiSkeletonDrawer::toggle_print_state()
 void
 SkelGuiSkeletonDrawer::set_print_state(SkelGuiSkeletonDrawer::PrintState state)
 {
-  glBegin(GL_LINE_LOOP);
+	glBegin(GL_LINE_LOOP);
 
-  print_state_ = state;
-  glEnd();
+	print_state_ = state;
+	glEnd();
 }
--- src/plugins/openni/skelgui/texture_drawer.cpp
+++ src/plugins/openni/skelgui/texture_drawer.cpp
@@ -22,14 +22,14 @@
 
 #include "texture_drawer.h"
 
+#include <GL/glut.h>
 #include <fvcams/camera.h>
 #include <fvutils/color/colorspaces.h>
 #include <fvutils/color/conversions.h>
 
-#include <cstdlib>
-#include <cstdio>
 #include <algorithm>
-#include <GL/glut.h>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace fawkes;
 using namespace firevision;
@@ -52,15 +52,16 @@ using namespace firevision;
  * @param height height of visible area
  */
 SkelGuiTextureDrawer::SkelGuiTextureDrawer(unsigned int width, unsigned int height)
-  : width_(width), height_(height),
-    texture_width_(get_closest_power_of_two(width_)),
-    texture_height_(get_closest_power_of_two(height_)),
-    texture_raii_(malloc(texture_width_ *texture_height_ * 3))
+: width_(width),
+  height_(height),
+  texture_width_(get_closest_power_of_two(width_)),
+  texture_height_(get_closest_power_of_two(height_)),
+  texture_raii_(malloc(texture_width_ * texture_height_ * 3))
 {
 	texture_ = (unsigned char *)*texture_raii_;
-  memset(texture_, 0, texture_width_ * texture_height_ * 3);
+	memset(texture_, 0, texture_width_ * texture_height_ * 3);
 
-  texture_initialized_ = false;
+	texture_initialized_ = false;
 }
 
 /** Destructor. */
@@ -71,63 +72,73 @@ SkelGuiTextureDrawer::~SkelGuiTextureDrawer()
 unsigned int
 SkelGuiTextureDrawer::get_closest_power_of_two(unsigned int n)
 {
-  unsigned int m = 2;
-  while(m < n) m<<=1;
+	unsigned int m = 2;
+	while (m < n)
+		m <<= 1;
 
-  return m;
+	return m;
 }
 
 void
 SkelGuiTextureDrawer::init_texture()
 {
-  glGenTextures(1, &texture_id_);
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
+	glGenTextures(1, &texture_id_);
+	glBindTexture(GL_TEXTURE_2D, texture_id_);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  memset(texture_coords_, 0, sizeof(texture_coords_));
-  texture_coords_[0] = (float)width_  / texture_width_;
-  texture_coords_[1] = (float)height_ / texture_height_;
-  texture_coords_[2] = (float)width_  / texture_width_;
-  texture_coords_[7] = (float)height_ / texture_height_;
+	memset(texture_coords_, 0, sizeof(texture_coords_));
+	texture_coords_[0] = (float)width_ / texture_width_;
+	texture_coords_[1] = (float)height_ / texture_height_;
+	texture_coords_[2] = (float)width_ / texture_width_;
+	texture_coords_[7] = (float)height_ / texture_height_;
 
-  texture_initialized_ = true;
+	texture_initialized_ = true;
 }
 
-
 void
-SkelGuiTextureDrawer::draw_rectangle(float topLeftX, float topLeftY,
-				   float bottomRightX, float bottomRightY)
+SkelGuiTextureDrawer::draw_rectangle(float topLeftX,
+                                     float topLeftY,
+                                     float bottomRightX,
+                                     float bottomRightY)
 {
-  GLfloat verts[8] = {	topLeftX, topLeftY, topLeftX, bottomRightY,
-			bottomRightX, bottomRightY, bottomRightX, topLeftY };
-  glVertexPointer(2, GL_FLOAT, 0, verts);
-  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-  glFlush();
+	GLfloat verts[8] = {
+	  topLeftX, topLeftY, topLeftX, bottomRightY, bottomRightX, bottomRightY, bottomRightX, topLeftY};
+	glVertexPointer(2, GL_FLOAT, 0, verts);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+	glFlush();
 }
 
 /** Draw texture to screen. */
 void
 SkelGuiTextureDrawer::draw()
 {
-  if (!texture_initialized_) init_texture();
+	if (!texture_initialized_)
+		init_texture();
 
-  fill_texture();
+	fill_texture();
 
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_width_, texture_height_,
-	       0, GL_RGB, GL_UNSIGNED_BYTE, texture_);
+	glBindTexture(GL_TEXTURE_2D, texture_id_);
+	glTexImage2D(GL_TEXTURE_2D,
+	             0,
+	             GL_RGB,
+	             texture_width_,
+	             texture_height_,
+	             0,
+	             GL_RGB,
+	             GL_UNSIGNED_BYTE,
+	             texture_);
 
-  // Display the OpenGL texture map
-  glColor4f(0.75,0.75,0.75,1);
+	// Display the OpenGL texture map
+	glColor4f(0.75, 0.75, 0.75, 1);
 
-  glEnable(GL_TEXTURE_2D);
-  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glTexCoordPointer(2, GL_FLOAT, 0, texture_coords_);
-  draw_rectangle(width_, height_, 0, 0);
-  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-  glDisable(GL_TEXTURE_2D);
+	glEnable(GL_TEXTURE_2D);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glTexCoordPointer(2, GL_FLOAT, 0, texture_coords_);
+	draw_rectangle(width_, height_, 0, 0);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisable(GL_TEXTURE_2D);
 }
 
 /** Copy an RGB buffer to texture.
@@ -137,16 +148,16 @@ SkelGuiTextureDrawer::draw()
 void
 SkelGuiTextureDrawer::copy_rgb_to_texture(const unsigned char *rgb_buf)
 {
-  unsigned char *row = texture_;
-  unsigned char *tex = texture_;
-  const unsigned char *rgb = rgb_buf;
-  for (unsigned int h = 0; h < height_; ++h) {
-    tex = row;
-    for (unsigned int w = 0; w < width_; ++w) {
-      *tex++ = *rgb++;
-      *tex++ = *rgb++;
-      *tex++ = *rgb++;
-    }
-    row += texture_width_ * 3;
-  }
+	unsigned char *      row = texture_;
+	unsigned char *      tex = texture_;
+	const unsigned char *rgb = rgb_buf;
+	for (unsigned int h = 0; h < height_; ++h) {
+		tex = row;
+		for (unsigned int w = 0; w < width_; ++w) {
+			*tex++ = *rgb++;
+			*tex++ = *rgb++;
+			*tex++ = *rgb++;
+		}
+		row += texture_width_ * 3;
+	}
 }
