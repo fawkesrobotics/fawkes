d3b729ed88404d302326ef1c1b2547daaf82d3e0
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-07 23:47:13 +0100

    fountain: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print $1 }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/plugins/perception/fountain/fountain_thread.cpp
+++ src/plugins/perception/fountain/fountain_thread.cpp
@@ -25,8 +25,8 @@
 #include <core/exceptions/software.h>
 #include <fvutils/net/fuse_server.h>
 
-#include <string>
 #include <cstdio>
+#include <string>
 
 using namespace fawkes;
 using namespace firevision;
@@ -37,90 +37,88 @@ using namespace firevision;
  */
 
 /** Constructor. */
-FountainThread::FountainThread()
-  : Thread("FountainThread", OPMODE_WAITFORWAKEUP)
+FountainThread::FountainThread() : Thread("FountainThread", OPMODE_WAITFORWAKEUP)
 {
-  fuse_server_ = NULL;
-  service_ = NULL;
+	fuse_server_ = NULL;
+	service_     = NULL;
 }
-
 
 /** Destructor. */
 FountainThread::~FountainThread()
 {
-  if ( fuse_server_ ) {
-    thread_collector->remove(fuse_server_);
-    delete fuse_server_;
-    fuse_server_ = NULL;
-  }
-  delete service_;
-  service_ = NULL;
+	if (fuse_server_) {
+		thread_collector->remove(fuse_server_);
+		delete fuse_server_;
+		fuse_server_ = NULL;
+	}
+	delete service_;
+	service_ = NULL;
 }
-
 
 void
 FountainThread::init()
 {
-  // Start FUSE server
-  unsigned int port = 0;
-  try {
-    port = config->get_uint("/firevision/fountain/tcp_port");
-    if ( port > 0xFFFF ) {
-      throw OutOfBoundsException("Network port out of bounds", port, 0, 0xFFFF);
-    }
+	// Start FUSE server
+	unsigned int port = 0;
+	try {
+		port = config->get_uint("/firevision/fountain/tcp_port");
+		if (port > 0xFFFF) {
+			throw OutOfBoundsException("Network port out of bounds", port, 0, 0xFFFF);
+		}
 
-    bool enable_ipv4 = true;
-    bool enable_ipv6 = true;
-    std::string listen_ipv4;
-    std::string listen_ipv6;
+		bool        enable_ipv4 = true;
+		bool        enable_ipv6 = true;
+		std::string listen_ipv4;
+		std::string listen_ipv6;
 
-    try {
-	    enable_ipv4 = config->get_bool("/network/ipv4/enable");
-    } catch (Exception &e) {}  // ignore, we stick with the default
-    try {
-	    enable_ipv6 = config->get_bool("/network/ipv6/enable");
-    } catch (Exception &e) {}  // ignore, we stick with the default
+		try {
+			enable_ipv4 = config->get_bool("/network/ipv4/enable");
+		} catch (Exception &e) {
+		} // ignore, we stick with the default
+		try {
+			enable_ipv6 = config->get_bool("/network/ipv6/enable");
+		} catch (Exception &e) {
+		} // ignore, we stick with the default
 
-    try {
-	    listen_ipv4 = config->get_string("/network/ipv4/listen");
-    } catch (Exception &e) {}  // ignore, we stick with the default
-    try {
-	    listen_ipv6 = config->get_string("/network/ipv6/listen");
-    } catch (Exception &e) {}  // ignore, we stick with the default
+		try {
+			listen_ipv4 = config->get_string("/network/ipv4/listen");
+		} catch (Exception &e) {
+		} // ignore, we stick with the default
+		try {
+			listen_ipv6 = config->get_string("/network/ipv6/listen");
+		} catch (Exception &e) {
+		} // ignore, we stick with the default
 
-    fuse_server_ = new FuseServer(enable_ipv4, enable_ipv6,
-                                   listen_ipv4, listen_ipv6,
-                                   port, thread_collector);
-    thread_collector->add(fuse_server_);
-  } catch (Exception &e) {
-    e.print_trace();
-    throw;
-  }
+		fuse_server_ =
+		  new FuseServer(enable_ipv4, enable_ipv6, listen_ipv4, listen_ipv6, port, thread_collector);
+		thread_collector->add(fuse_server_);
+	} catch (Exception &e) {
+		e.print_trace();
+		throw;
+	}
 
-  // Announce service
-  std::string sname = "Fountain on ";
-  sname += nnresolver->short_hostname();
-  service_ = new NetworkService(sname.c_str(), "_fountain._tcp", port);
-  service_publisher->publish_service(service_);
+	// Announce service
+	std::string sname = "Fountain on ";
+	sname += nnresolver->short_hostname();
+	service_ = new NetworkService(sname.c_str(), "_fountain._tcp", port);
+	service_publisher->publish_service(service_);
 }
-
 
 void
 FountainThread::finalize()
 {
-  service_publisher->unpublish_service(service_);
+	service_publisher->unpublish_service(service_);
 
-  thread_collector->remove(fuse_server_);
-  delete fuse_server_;
-  fuse_server_ = NULL;
-  delete service_;
-  service_ = NULL;
+	thread_collector->remove(fuse_server_);
+	delete fuse_server_;
+	fuse_server_ = NULL;
+	delete service_;
+	service_ = NULL;
 }
-
 
 void
 FountainThread::loop()
 {
-  // do nothing, but implement to not exit
-  printf("Sucker Loop\n");
+	// do nothing, but implement to not exit
+	printf("Sucker Loop\n");
 }
