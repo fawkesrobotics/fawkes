a62ff369494721a18f5991aa1297e82d9cc04d90
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2019-03-08 13:54:00 +0100

    buildsys: add local format target
    So far, there was a format target in the root directory, which reformats
    everything. With this change, there is a new local format target, which
    is triggered when running "make format" in a source directory. It will
    format all locally modified and untracked files in the given directory.
    
    This comes very handy to quickly format the code before starting
    committing.

---
+++ etc/buildsys/format.mk
@@ -0,0 +1,44 @@
+
+#*****************************************************************************
+#     Makefile Build System for Fawkes: Local formatting targets
+#                            -------------------
+#   Created on Fri Mar 08 10:08:43 2019 +0100
+#   Copyright (C) 2006-2019 by Tim Niemueller [www.niemueller.de]
+#
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifndef __buildsys_config_mk_
+$(error config.mk must be included before format.mk)
+endif
+
+.PHONY: format-modified-clang
+format-modified-clang:
+	$(SILENT) echo -e "$(INDENT_PRINT)[FMT] Formatting modified files (git)"
+	$(SILENTSYMB)if type -p clang-format >/dev/null; then \
+		pushd $(SRCDIR) >/dev/null; \
+		MODIFIED_FILES=$$(git ls-files -m *.{h,cpp}); \
+		UNTRACKED_FILES=$$(git ls-files --exclude-standard --others *.{h,cpp}); \
+		FILES=$$(echo "$$MODIFIED_FILES" "$$UNTRACKED_FILES" | tr ' ' '\n' | sort -u); \
+		if [ -n "$$FILES" ]; then \
+			if type -p parallel >/dev/null; then \
+				parallel -u --will-cite --bar clang-format -i ::: $$FILES; \
+			else \
+				echo "$$FILES" | xargs -P$$(nproc) -n1 clang-format -i; \
+			fi; \
+		fi; \
+		popd >/dev/null; \
+	else \
+		echo -e "$(INDENT_PRINT)$(TRED)--- Cannot format code$(TNORMAL) (clang-format not found)"; \
+    exit 1; \
+  fi
+
+# The local version only updates modified files by default
+.PHONY: format
+format: format-modified-clang
