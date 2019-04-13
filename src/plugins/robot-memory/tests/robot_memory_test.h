25a7bdabc01de0de1e414cfee1f15ce59b6820d9
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-04-13 18:56:43 +0200

    robot-memory: adapt tests to mongocxx-v3
    Some of the tests fail, but now we can at least build them.

--- src/plugins/robot-memory/tests/robot_memory_test.h
+++ src/plugins/robot-memory/tests/robot_memory_test.h
@@ -27,6 +27,9 @@
 #include <blackboard/blackboard.h>
 #include <gtest/gtest.h>
 
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/document/value.hpp>
+#include <bsoncxx/document/view.hpp>
 #include <stdio.h>
 
 /** Environment for running Tests of the RobotMemory
@@ -78,7 +81,10 @@ protected:
 	fawkes::BlackBoard *blackboard;
 
 protected:
-	::testing::AssertionResult contains_pairs(mongo::BSONObj obj, mongo::BSONObj exp);
+	::testing::AssertionResult contains_pairs(const bsoncxx::document::view_or_value &obj,
+	                                          const bsoncxx::document::view_or_value &exp);
+	::testing::AssertionResult contains_pairs(mongocxx::cursor &                      cursor,
+	                                          const bsoncxx::document::view_or_value &exp);
 };
 
 /**
@@ -99,7 +105,7 @@ public:
      * @param update Trigger update
      */
 	void
-	callback_test(mongo::BSONObj update)
+	callback_test(const bsoncxx::document::view &update)
 	{
 		callback_counter++;
 	}
@@ -120,11 +126,15 @@ public:
      * @param collection Corresponding collection
      * @return Computed docs
      */
-	std::list<mongo::BSONObj>
-	compute(const mongo::BSONObj &query, const std::string &collection)
+	std::list<bsoncxx::document::value>
+	compute(const bsoncxx::document::view &query, const std::string &collection)
 	{
-		std::list<mongo::BSONObj> res;
-		res.push_back(mongo::fromjson("{computed:true, result:'this is computed'}"));
+		std::list<bsoncxx::document::value> res;
+		using namespace bsoncxx::builder;
+		basic::document doc;
+		doc.append(basic::kvp("computed", true));
+		doc.append(basic::kvp("result", "this is computed"));
+		res.push_back(doc.extract());
 		return res;
 	}
 	/**
@@ -133,18 +143,20 @@ public:
      * @param collection Corresponding collection
      * @return Computed docs
      */
-	std::list<mongo::BSONObj>
-	compute_sum(const mongo::BSONObj &query, const std::string &collection)
+	std::list<bsoncxx::document::value>
+	compute_sum(const bsoncxx::document::view &query, const std::string &collection)
 	{
-		std::list<mongo::BSONObj> res;
-		int                       x   = query.getField("x").Int();
-		int                       y   = query.getField("y").Int();
-		int                       sum = x + y;
-		mongo::BSONObjBuilder     b;
-		b << "compute"
-		  << "sum"
-		  << "x" << x << "y" << y << "sum" << sum;
-		res.push_back(b.obj());
+		std::list<bsoncxx::document::value> res;
+		int                                 x   = query["x"].get_int64();
+		int                                 y   = query["y"].get_int64();
+		int                                 sum = x + y;
+		using namespace bsoncxx::builder;
+		basic::document b;
+		b.append(basic::kvp("compute", "sum"));
+		b.append(basic::kvp("x", x));
+		b.append(basic::kvp("y", y));
+		b.append(basic::kvp("sum", sum));
+		res.push_back(b.extract());
 		return res;
 	}
 	/**
@@ -153,13 +165,17 @@ public:
      * @param collection Corresponding collection
      * @return Computed docs
      */
-	std::list<mongo::BSONObj>
-	compute_multiple(const mongo::BSONObj &query, const std::string &collection)
+	std::list<bsoncxx::document::value>
+	compute_multiple(const bsoncxx::document::view &query, const std::string &collection)
 	{
-		std::list<mongo::BSONObj> res;
-		res.push_back(mongo::fromjson("{compute:'multiple', count:1}"));
-		res.push_back(mongo::fromjson("{compute:'multiple', count:2}"));
-		res.push_back(mongo::fromjson("{compute:'multiple', count:3}"));
+		std::list<bsoncxx::document::value> res;
+		using namespace bsoncxx::builder;
+		for (auto i : {1, 2, 3}) {
+			basic::document doc;
+			doc.append(basic::kvp("compute", "multiple"));
+			doc.append(basic::kvp("count", i));
+			res.push_back(doc.extract());
+		}
 		return res;
 	}
 };
