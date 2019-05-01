4722e86209408d0338ad158ff0c346d6b941e305
Christoph Gollok <christoph.gollok@alumni.fh-aachen.de>
2019-05-01 19:07:12 +0200

    realsense: fix set currrent_laser_power before logging

--- src/plugins/realsense/realsense_thread.cpp
+++ src/plugins/realsense/realsense_thread.cpp
@@ -240,10 +240,10 @@ RealsenseThread::set_laser_power(int laser_power){
 
     //double dev_laser_power = rs_get_device_option(rs_device_, RS_OPTION_F200_LASER_POWER, &rs_error_);
     if ( current_laser_power_ != laser_power){
-        current_laser_power_ = laser_power;
         logger->log_info(name(),
                          "set laser power to %i was %i",
                          laser_power, current_laser_power_);
+        current_laser_power_ = laser_power;
         rs_set_device_option(rs_device_, RS_OPTION_F200_LASER_POWER, laser_power_high_, &rs_error_);
         log_error();
     }
