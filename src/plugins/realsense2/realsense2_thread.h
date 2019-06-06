9fac8de80198cb612251f14dbbd0561da5ef375b
Christoph Gollok <christoph.gollok@alumni.fh-aachen.de>
2019-06-06 16:59:56 +0200

    realsense2: reformat code to clang format

--- src/plugins/realsense2/realsense2_thread.h
+++ src/plugins/realsense2/realsense2_thread.h
@@ -21,8 +21,8 @@
  *  Read the full text in the LICENSE.GPL file in the doc directory.
  */
 
-#ifndef _PLUGINS_REALSENSETHREAD_H_
-#define _PLUGINS_REALSENSETHREAD_H_
+#ifndef _PLUGINS_REALSENSE2THREAD_H_
+#define _PLUGINS_REALSENSE2THREAD_H_
 
 #include <aspect/blackboard.h>
 #include <aspect/blocked_timing.h>
@@ -33,14 +33,17 @@
 #include <core/threading/thread.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+
 #include <thread>
 
 #ifdef HAVE_REALSENSE2
+#	include <librealsense2/rsutil.h>
+
 #	include <librealsense2/rs.hpp>
-#   include <librealsense2/rsutil.h>
 #else
-#   include <librealsense2/rs.hpp>
-#   include <librealsense2/rsutil.h>
+#	include <librealsense2/rsutil.h>
+
+#	include <librealsense2/rs.hpp>
 #endif
 
 #include <string>
@@ -50,29 +53,29 @@ class SwitchInterface;
 }
 
 class Realsense2Thread : public fawkes::Thread,
-                        public fawkes::BlockedTimingAspect,
-                        public fawkes::LoggingAspect,
-                        public fawkes::ConfigurableAspect,
-                        public fawkes::BlackBoardAspect,
-                        public fawkes::PointCloudAspect,
-                        public fawkes::ClockAspect
+                         public fawkes::BlockedTimingAspect,
+                         public fawkes::LoggingAspect,
+                         public fawkes::ConfigurableAspect,
+                         public fawkes::BlackBoardAspect,
+                         public fawkes::PointCloudAspect,
+                         public fawkes::ClockAspect
 {
 public:
-    Realsense2Thread();
+	Realsense2Thread();
 
 	virtual void init();
 	virtual void finalize();
 	virtual void loop();
 
 private:
-    bool       start_camera();
-    void       get_camera(rs2::device &dev);
-    void       enable_depth_stream();
-    void       disable_depth_stream();
-	void       log_error();
-	void       log_depths(const uint16_t *image);
-	void       fill_pointcloud();
-	void       stop_camera();
+	bool start_camera();
+	void get_camera(rs2::device &dev);
+	void enable_depth_stream();
+	void disable_depth_stream();
+	void log_error();
+	void log_depths(const uint16_t *image);
+	void fill_pointcloud();
+	void stop_camera();
 
 	/** Stub to see name in backtrace for easier debugging. @see Thread::run() */
 protected:
@@ -98,25 +101,25 @@ private:
 	fawkes::RefPtr<Cloud> realsense_depth_refptr_;
 	CloudPtr              realsense_depth_;
 
-    rs2::pipeline * rs_pipe_;
-    rs2::pipeline_profile rs_pipeline_profile_;
-    rs2::context * rs_context_;
-    rs2::config  rs_config_;
-    rs2::error * rs_error_;
-    rs2::device  rs_device_;
-    rs2::stream_profile stream_profile_;
-    rs2::frameset rs_data_;
-    rs2_intrinsics intrinsics_;
+	rs2::pipeline *       rs_pipe_;
+	rs2::pipeline_profile rs_pipeline_profile_;
+	rs2::context *        rs_context_;
+	rs2::config           rs_config_;
+	rs2::error *          rs_error_;
+	rs2::device           rs_device_;
+	rs2::stream_profile   stream_profile_;
+	rs2::frameset         rs_data_;
+	rs2_intrinsics        intrinsics_;
 
-	float         camera_scale_;
-	std::string   frame_id_;
-	std::string   pcl_id_;
-    bool          camera_running_ = false;
-    bool          enable_camera_  = true;
-    bool          depth_enabled_  = false;
-	int           laser_power_;
-	uint          restart_after_num_errors_;
-	uint          error_counter_ = 0;
+	float       camera_scale_;
+	std::string frame_id_;
+	std::string pcl_id_;
+	bool        camera_running_ = false;
+	bool        enable_camera_  = true;
+	bool        depth_enabled_  = false;
+	int         laser_power_;
+	uint        restart_after_num_errors_;
+	uint        error_counter_ = 0;
 };
 
 #endif
