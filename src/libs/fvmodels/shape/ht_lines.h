c44e5e0cf3eaf108ed33bcc0bb99189cfa4e47e7
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-09-24 10:45:21 +0200

    fvmodels: remove duplicate documentation

--- src/libs/fvmodels/shape/ht_lines.h
+++ src/libs/fvmodels/shape/ht_lines.h
@@ -44,22 +44,6 @@ private:
 	RhtAccumulator         accumulator;
 
 public:
-	/** Creates a new HtLinesModel instance
-   * @param nr_candidates the nr of candidates that is considered per pixel (the hole angle
-   *                      range is devided in this many parts/lines
-   * @param angle_from The angle to start the candidates from, given in rad, 0 is straight up
-   * @param angle_range the angle range the candidates are taken from starting at angle_from,
-   *                    given in rad, can be used for example to only search for horizontal lines
-   * @param r_scale This can be done to reduce the size of the hough space and to map more lines
-   *                to one line
-   * @param min_votes_ratio The minimum ratio num_votes_per_line/total_num_votes that we have to
-   *                        have before a point in the hough space is considered to be a line,
-   *                        this may actually be higher if you use min_votes and set it to a higher
-   *                        number (set min_votes to 0 to only use min_votes_ration)
-   * @param min_votes the minimum number of votes a point in the hough space has to have before it
-   *                  is considered to be a line. The number may actually be higher if min_votes_ratio
-   *                  is set too high (set min_votes_ration to 0 to use only min_votes)
-   */
 	HtLinesModel(unsigned int nr_candidates   = 40,
 	             float        angle_from      = 0,
 	             float        angle_range     = 2 * M_PI,
