cab1a426a8f789ee5deda9386df5f78f2f2bbfa2
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-09-25 17:26:09 +0200

    gologpp: add license headers

--- src/plugins/gologpp/execution_thread.h
+++ src/plugins/gologpp/execution_thread.h
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *  execution_thread.h - Execution thread for Golog++
+ *
+ *  Created: Mon 26 Aug 2019 CEST 15:38
+ *  Copyright  2019  Victor Mataré <matare@fh-aachen.de>
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
 #ifndef FAWKES_GOLOGPP_THREAD_H_
 #define FAWKES_GOLOGPP_THREAD_H_
 
--- src/plugins/gologpp/gologpp_fawkes_backend.cpp
+++ src/plugins/gologpp/gologpp_fawkes_backend.cpp
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *  gologpp_fawkes_backend.cpp - Fawkes backend for Golog++
+ *
+ *  Created: Mon 26 Aug 2019 CEST 15:38
+ *  Copyright  2019  Victor Mataré <matare@fh-aachen.de>
+ *                   Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
 #include "gologpp_fawkes_backend.h"
 
 using namespace gologpp;
