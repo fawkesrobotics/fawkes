52527518926785c4788e80a259cd707fd4b75742
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-12-03 17:43:53 +0100

    gologpp: use `host` instead of `hostname` for remote agent

--- src/plugins/gologpp/gologpp_fawkes_backend.cpp
+++ src/plugins/gologpp/gologpp_fawkes_backend.cpp
@@ -61,7 +61,7 @@ GologppFawkesBackend::GologppFawkesBackend(Configuration *config,
 	     config->get_strings_or_defaults((cfg_prefix + "/agents/names").c_str(), {})) {
 		const std::string  agent_prefix = cfg_prefix + "/agents/" + robot;
 		const std::string &hostname =
-		  config->get_string_or_default((agent_prefix + "/hostname").c_str(), "localhost");
+		  config->get_string_or_default((agent_prefix + "/host").c_str(), "localhost");
 		const unsigned short int &port =
 		  config->get_uint_or_default((agent_prefix + "/port").c_str(), 1911);
 		action_dispatcher_.register_executor(std::make_shared<RemoteSkillerActionExecutor>(
