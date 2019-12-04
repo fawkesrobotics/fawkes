6e36444089ef9e92fefcf8c8c003e1435c43f3ae
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-12-04 21:20:07 +0100

    gologpp: fix default port for remote skiller execution
    Fawkes uses port 1910 by default, not 1911.

--- src/plugins/gologpp/gologpp_fawkes_backend.cpp
+++ src/plugins/gologpp/gologpp_fawkes_backend.cpp
@@ -63,7 +63,7 @@ GologppFawkesBackend::GologppFawkesBackend(Configuration *config,
 		const std::string &hostname =
 		  config->get_string_or_default((agent_prefix + "/host").c_str(), "localhost");
 		const unsigned short int &port =
-		  config->get_uint_or_default((agent_prefix + "/port").c_str(), 1911);
+		  config->get_uint_or_default((agent_prefix + "/port").c_str(), 1910);
 		action_dispatcher_.register_executor(std::make_shared<RemoteSkillerActionExecutor>(
 		  logger, "robot", robot, hostname, port, config, cfg_prefix));
 	}
