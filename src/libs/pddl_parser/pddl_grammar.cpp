3d34b80e5b0d810603c5be8d8da4ab73e6e498fb
Daniel Habering <daniel.habering@rwth-aachen.de>
2019-12-22 22:42:48 +0100

    pddl_parser: update license headers

--- src/libs/pddl_parser/pddl_grammar.cpp
+++ src/libs/pddl_parser/pddl_grammar.cpp
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *  stn.cpp - stn-generator
+ *
+ *  Created: Sun Dec 22 22:41 2019
+ *  Copyright  Nils Adermann <naderman@naderman.de>, Daniel Habering
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
 #include "pddl_grammar.h"
 
 using namespace pddl_parser;
