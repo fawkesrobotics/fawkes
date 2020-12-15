15e8c5af04d97672941939df1e0873e9b38fd457
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2020-12-15 12:52:24 +0100

    utils: add documentation for Uuids

--- src/libs/utils/uuid.cpp
+++ src/libs/utils/uuid.cpp
@@ -25,27 +25,47 @@
 
 namespace fawkes {
 
+/** @class Uuid
+ * A convenience class for universally unique identifiers (UUIDs).
+ * It wraps uuid(3) to allow easier creation and deletion of UUIDs.
+ */
+
+/** Generate a new Uuid. */
 Uuid::Uuid() noexcept
 {
 	uuid_generate(uuid_);
 }
 
+/** Destructor, clears up the occupied storage. */
 Uuid::~Uuid() noexcept
 {
 	uuid_clear(uuid_);
 }
 
+/** Copy constructor.
+ * The newly constructed Uuid is guaranteed to be the same as the source Uuid.
+ * @param other The Uuid to copy from
+ */
 Uuid::Uuid(const Uuid &other) noexcept
 {
 	uuid_copy(uuid_, other.uuid_);
 }
 
+/** Move constructor.
+ * This leaves the other Uuid in an undefined state. The newly constructed Uuid
+ * is guaranteed to be the same as the source Uuid.
+ * @param other The Uuid to move from
+ */
 Uuid::Uuid(Uuid &&other) noexcept
 {
 	uuid_copy(uuid_, other.uuid_);
 	uuid_clear(other.uuid_);
 }
 
+/** Construct a Uuid from a string.
+ * @param string The string represenation of the Uuid, of the form
+ * 1b4e28ba-2fa1-11d2-883f-b9a761bde3fb
+ */
 Uuid::Uuid(const char *string)
 {
 	int res = uuid_parse(string, uuid_);
@@ -54,6 +74,11 @@ Uuid::Uuid(const char *string)
 	}
 }
 
+/** Assignment operator.
+ * After assignment, both Uuids are guaranteed to be the same.
+ * @param other The Uuid to assign from
+ * @return A reference to the assigned Uuid
+ */
 Uuid &
 Uuid::operator=(const Uuid &other) noexcept
 {
@@ -61,6 +86,12 @@ Uuid::operator=(const Uuid &other) noexcept
 	return *this;
 }
 
+/** Move assignment operator.
+ * This leaves the other Uuid in an undefined state. The assigned Uuid
+ * is guaranteed to be the same as the source Uuid.
+ * @param other The Uuid to assign from
+ * @return A reference to the assigned Uuid
+ */
 Uuid &
 Uuid::operator=(Uuid &&other) noexcept
 {
@@ -69,6 +100,9 @@ Uuid::operator=(Uuid &&other) noexcept
 	return *this;
 }
 
+/** Get the string representation of the Uuid.
+ * @return The Uuid as string of the form 1b4e28ba-2fa1-11d2-883f-b9a761bde3fb
+ */
 std::string
 Uuid::get_string() const
 {
@@ -77,18 +111,33 @@ Uuid::get_string() const
 	return std::string(res);
 }
 
+/** Compare two Uuids.
+ * @param uuid The first Uuid to compare
+ * @param other The second Uuid to compare
+ * @return True if the first Uuid is smaller than the second
+ */
 bool
 operator<(const Uuid &uuid, const Uuid &other) noexcept
 {
 	return (uuid_compare(uuid.uuid_, other.uuid_) < 0);
 }
 
+/** Compare two Uuids.
+ * @param uuid The first Uuid to compare
+ * @param other The second Uuid to compare
+ * @return True if the two Uuids are the same
+ */
 bool
 operator==(const Uuid &uuid, const Uuid &other) noexcept
 {
 	return (uuid_compare(uuid.uuid_, other.uuid_) == 0);
 }
 
+/** Compare two Uuids.
+ * @param uuid The first Uuid to compare
+ * @param other The second Uuid to compare
+ * @return True if the two Uuids are not the same
+ */
 bool
 operator!=(const Uuid &uuid, const Uuid &other) noexcept
 {
