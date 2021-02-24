f87830daa80ed14d789073f1c945d0055899028f
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2021-02-24 11:53:46 +0100

    scripts: add gaussian sampler to lookup script

--- etc/scripts/mongodb_skillsim_lookup.py
+++ etc/scripts/mongodb_skillsim_lookup.py
@@ -29,6 +29,12 @@ Extract skill durations from the skiller messages obtained via mongodb blackboar
 import argparse
 import pymongo
 import re
+import textwrap
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as ss
 
 # Accourding to SkillStatusEnum
 S_INACTIVE = 0
@@ -59,6 +65,72 @@ def drop_collection(mongodb_uri, database, collection):
   curr_client = pymongo.MongoClient(mongodb_uri)
   curr_client[database][collection].drop()
 
+class GaussSampler:
+
+  def __init__(
+          self,
+          quantity,
+          dist_weights,
+          gauss_params,
+          upper_bound,
+          lower_bound):
+    self.dist_weights = dist_weights
+    self.lower_bound = lower_bound
+    self.upper_bound = upper_bound
+    if len(self.dist_weights) != len(gauss_params):
+      print("Number of distribution weights do not match number of distributions!")
+      diff= len(gauss_params) - len(dist_weights)
+      if diff < 0:
+          print("Ignoring trailing distribution weights")
+          self.dist_weights = self.dist_weights[:len(dist_weights)+diff]
+      else:
+          print("Assuming default weights of 1")
+          self.dist_weights.extend([1]*diff)
+    # normalize weights
+    self.dist_weights = np.array([float(i)/sum(self.dist_weights) for i in self.dist_weights])
+    # create samples
+    self.samples = []
+    self.gauss_params=gauss_params
+    sample_size = quantity
+    self.sample_min, self.sample_max = [float("inf"),-float("inf")]
+    while(True):
+      i = 0
+      # determine the gaussian to sample from for each sample
+      mixture_idx = np.random.choice(len(self.dist_weights),
+                                     size=sample_size,
+                                     replace=True,
+                                     p=self.dist_weights)
+      # create the samples from the respective gaussian
+      temp = np.fromiter((ss.norm.rvs(*(gauss_params[i]))
+                          for i in mixture_idx), dtype=np.float64)
+      # remember mixed sampled extremas for plotting
+      self.sample_min = min(self.sample_min,temp.min())
+      self.sample_max = max(self.sample_max,temp.max())
+      # add those samples that are within the bounds
+      self.samples = np.concatenate([self.samples,np.fromiter([x for x in temp
+                       if x <= upper_bound and x >= lower_bound],
+                         dtype=np.float64)])
+      sample_size = quantity-len(self.samples)
+      if(sample_size == 0):
+          break
+
+  def display(self, bin_size):
+    xs = np.linspace(self.sample_min, self.sample_max, 2000)
+    ys = np.zeros_like(xs)
+    for (l, s), w in zip(self.gauss_params, self.dist_weights):
+        ys += ss.norm.pdf(xs, loc=l, scale=s) * w
+    plt.plot(xs, ys,color="blue")
+    plt.hist(self.samples, density=True, bins=bin_size,color='palegreen')
+    plt.xlabel("duration")
+    plt.ylabel("density")
+    xmin, xmax, ymin, ymax = plt.axis()
+    if(self.lower_bound > 0):
+      plt.vlines([self.lower_bound],ymin,ymax,color='crimson')
+    if(self.upper_bound < float('inf')):
+      plt.vlines([self.lower_bound],ymin,ymax,color='crimson')
+    plt.show()
+
+
 class MongoInterface:
 
   def __init__(
@@ -71,6 +143,21 @@ class MongoInterface:
     self.dst_mongodb_uri = dst_mongodb_uri
     self.lookup_col = self.client[dst_database][dst_collection]
     self.dry_run = dry_run
+
+  def upload(self, durations, skill_name, skill_args):
+      for dur in durations:
+          doc = {"outcome": 1, "error": "", "name": skill_name, "duration": dur}
+          args_dict  = dict()
+          if skill_args != None:
+             for arg in skill_args:
+                 if(len(arg) == 1):
+                   args_dict[arg[0]]='*'
+                 else:
+                   args_dict[arg[0]]=np.random.choice(arg[1:])
+          doc["args"] = args_dict
+          print(doc)
+          if(not self.dry_run):
+            self.lookup_col.insert_one(doc)
 
   def transform(self, src_mongodb_uri, src_database, src_collection, lower_bound, upper_bound):
     if src_mongodb_uri != self.dst_mongodb_uri:
@@ -161,6 +248,14 @@ def main():
 ###############################################################################
 '''),formatter_class=argparse.RawTextHelpFormatter)
   bb_parser.set_defaults()
+  random_parser = subparsers.add_parser("generate",parents=[common],
+                                    description=textwrap.dedent(header + '''\
+#                                                                             #
+# Selected option 2                                                           #
+#                                                                             #
+###############################################################################
+'''),formatter_class=argparse.RawTextHelpFormatter)
+  random_parser.set_defaults()
   bb_sanity = bb_parser.add_argument_group("Sanity checks to avoid faulty entries")
   bb_sanity.add_argument(
       '--lower-bound', '-l',
@@ -194,6 +289,64 @@ def main():
       help='Delete the skiller blackboard log collection afterwards',
       default=False)
 
+  skill = random_parser.add_argument_group("Skill information")
+  skill.add_argument(
+      '--quantity', '-n',
+      type=int,
+      help='number of entries to generate',
+      required=True)
+  skill.add_argument(
+      '--skill-name', '-s',
+      type=str,
+      help='skill name to generate entries for',
+      required=True)
+  skill.add_argument(
+      '--skill-args', '-a',
+      type=str,
+      nargs='+',
+      action='append',
+      help=textwrap.dedent('''skill arguments. usage -a <arg-name> <val1> <val2> ...
+                              where val<i> are the possible values of the argument that will be chosen from at random
+                              * (placeholder value) if no values are given
+                           '''))
+
+  gauss = random_parser.add_argument_group("Mixed gaussian distribution")
+  gauss.add_argument(
+      '--gauss-params', '-g',
+      type=float,
+      help='mean and standard deviation (in that order)',
+      nargs='+',
+      required=True,
+      action='append')
+  gauss.add_argument(
+      '--dist-weights', '-w',
+      type=float,
+      default=[],
+      help='Weight of each gauss distribution (default 1)',
+      nargs='+')
+  gauss.add_argument(
+      '--lower-bound', '-l',
+      type=float,
+      default=0,
+      help='clip distribution to a lower bound')
+  gauss.add_argument(
+      '--upper-bound', '-u',
+      type=float,
+      default=float('inf'),
+      help='clip distribution to an upper bound')
+
+  visual = random_parser.add_argument_group("Visualization options")
+  visual.add_argument(
+      '--bin-size', '-b',
+      type=int,
+      help='number of bins to display sampled durations',
+      default=1000)
+  visual.add_argument(
+      '--non-interactive', '-y',
+      action='store_true',
+      help='skip drawing the sample range and asking for confirmation')
+  parser.epilog = "--- Arguments common to all sub-parsers ---" \
+      + common.format_help().replace(common.format_usage(), '')
   args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
   # validate inputs
   if args==None:
@@ -208,6 +361,15 @@ def main():
                       args.lower_bound,args.upper_bound)
     if args.drop_src_col:
         drop_collection(args.src_mongodb_uri, args.src_db, args.src_col)
+  elif args.subparser == 'generate':
+      sampler = GaussSampler(args.quantity,
+                             args.dist_weights,
+                             args.gauss_params,
+                             args.upper_bound,
+                             args.lower_bound);
+      if(not args.non_interactive):
+          sampler.display(args.bin_size)
+      mongoIf.upload(sampler.samples, args.skill_name, args.skill_args)
   else:
     print("unrecognized mode")
 
