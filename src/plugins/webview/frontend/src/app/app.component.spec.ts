d7ad788b3874728c488a551356d53c4eb205e262
Ivaylo Doychev <doychev@kbsg.rwth-aachen.de>
2021-03-05 11:10:05 +0100

    webview-frontend: replace deprecated async function

--- src/plugins/webview/frontend/src/app/app.component.spec.ts
+++ src/plugins/webview/frontend/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
 // Copyright  2018  Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
 // License: Apache 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -14,17 +14,17 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'app'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
--- src/plugins/webview/frontend/src/parts/skiller/components/overview.component.spec.ts
+++ src/plugins/webview/frontend/src/parts/skiller/components/overview.component.spec.ts
@@ -2,7 +2,7 @@
 // Copyright  2018  Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
 // License: Apache 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SkillerOverviewComponent } from './overview.component';
 
@@ -10,7 +10,7 @@ describe('SkillerOverviewComponent', () => {
   let component: SkillerOverviewComponent;
   let fixture: ComponentFixture<SkillerOverviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SkillerOverviewComponent ]
     })
