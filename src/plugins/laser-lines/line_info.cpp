e891188a2f28e6ce560da152dfcb999627c0096b
Nicolas Limpert <limpert@fh-aachen.de>
2021-06-24 17:45:48 +0200

    laser-lines: add laserlines transformed to frame
    This adds raw laserlines transformed to a custom frame (i.e. "map") to
    ease accessibility for Fawkes instances connected via bbsync rather than
    having to synchronize all transforms.

--- src/plugins/laser-lines/line_info.cpp
+++ src/plugins/laser-lines/line_info.cpp
@@ -40,6 +40,8 @@ using namespace std;
 TrackedLineInfo::TrackedLineInfo(fawkes::tf::Transformer *tfer,
                                  const string &           input_frame_id,
                                  const string &           tracking_frame_id,
+                                 const string &           fixed_frame_id,
+                                 bool                     transform_to_fixed_frame,
                                  float                    cfg_switch_tolerance,
                                  unsigned int             cfg_moving_avg_len,
                                  fawkes::Logger *         logger,
@@ -48,6 +50,8 @@ TrackedLineInfo::TrackedLineInfo(fawkes::tf::Transformer *tfer,
   visibility_history(0),
   transformer(tfer),
   input_frame_id(input_frame_id),
+  fixed_frame_id(fixed_frame_id),
+  transform_to_fixed_frame(transform_to_fixed_frame),
   tracking_frame_id(tracking_frame_id),
   cfg_switch_tolerance(cfg_switch_tolerance),
   history(cfg_moving_avg_len),
@@ -146,6 +150,15 @@ TrackedLineInfo::update(LineInfo &linfo)
 	this->smooth.line_direction = line_direction_sum / sz;
 	this->smooth.point_on_line  = point_on_line_sum / sz;
 
+
+  if (transform_to_fixed_frame == true)
+  {
+    transform_point_to_frame(linfo.base_point, this->transformed.base_point, input_frame_id, fixed_frame_id);
+    transform_point_to_frame(linfo.line_direction, this->transformed.line_direction, input_frame_id, fixed_frame_id);
+    transform_point_to_frame(linfo.end_point_1, this->transformed.end_point_1, input_frame_id, fixed_frame_id);
+    transform_point_to_frame(linfo.end_point_2, this->transformed.end_point_2, input_frame_id, fixed_frame_id);
+  }
+
 	Eigen::Vector3f x_axis(1, 0, 0);
 
 	Eigen::Vector3f ld_unit    = this->smooth.line_direction / this->smooth.line_direction.norm();
@@ -162,3 +175,34 @@ TrackedLineInfo::update(LineInfo &linfo)
 	if (l_ctr[1] < 0)
 		this->bearing_center = std::abs(this->bearing_center) * -1.;
 }
+
+/** Transform a point with an origin in a particular frame to another frame
+ * @param point the point to be transformed
+ * @param from_frame frame_id of the point's origin
+ * @param to_frame frame_id of the target coordinate system
+ */
+bool
+TrackedLineInfo::transform_point_to_frame(const Eigen::Vector3f& in_point, Eigen::Vector3f& out_point, const std::string from_frame, const std::string to_frame)
+{
+	fawkes::tf::Stamped<fawkes::tf::Point> transformed_point(fawkes::tf::Point(in_point[0],
+	                                                                           in_point[1],
+	                                                                           in_point[2]),
+	                                                         fawkes::Time(0, 0),
+	                                                         from_frame);
+
+	try {
+		transformer->transform_point(fixed_frame_id, transformed_point, transformed_point);
+
+    out_point[0] = transformed_point[0];
+    out_point[1] = transformed_point[1];
+    out_point[2] = transformed_point[2];
+
+	} catch (fawkes::tf::TransformException &e) {
+		logger->log_warn(plugin_name.c_str(),
+		                 "Can't transform to %s.",
+		                 fixed_frame_id.c_str());
+    return false;
+	}
+  return true;
+}
+
