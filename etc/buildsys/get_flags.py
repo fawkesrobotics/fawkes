ac09545a1b77cf489b2dc1e094574fe1ac15a268
Gjorgji Nikolovski <gjorgji.nikolovski@alumni.fh-aachen.de>
2021-11-07 17:05:53 +0100

    ros2: fixed flag aggregation system for compilation (hopefully)

--- etc/buildsys/get_flags.py
+++ etc/buildsys/get_flags.py
@@ -1,34 +1,44 @@
 import subprocess
 import os
 import sys, getopt
+from pathlib import Path
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"hp:vebcl",["package=", "version", "exist", "build", "cflags", "lflags"])
+        opts, args = getopt.getopt(argv,"hw:p:vebcl",["ros2-workspace=", "package=", "version", "exist", "build", "cflags", "lflags"])
     except getopt.GetoptError:
         sys.exit(2)
     package_name = ""
+    ros2_workspace = ""
+    tmp_ws_dir = "/tmp/tmp_ws_for_flags"
     for opt, arg in opts:
         if opt == '-h':
             print('argument description')
             sys.exit()
+        elif opt in ('-w', "--ros2-workspace"):
+            ros2_workspace = arg
         elif opt in ("-p", "--package"):
             package_name = arg
         elif opt in ("-v", "--version"):
             print(1)
-
             sys.exit()
         elif opt in ("-e", "--exist"):
-            if os.path.exists(f"/home/gjorgji/ros2_galactic/build/{package_name}"):
+            if os.path.exists(f"{ros2_workspace}/build/{package_name}") or os.path.exists(f"/tmp/tmp_ws_for_flags/build/{package_name}"):
                 print(1)
             else:
                 print(0)
             sys.exit()
         elif opt in ("-b", "--build"):
-            with open("/home/gjorgji/ros2_galactic/src/dummy_for_flags/CMakeLists.txt", 'w') as cmakelists_txt:
+            
+            if os.path.exists(f"{tmp_ws_dir}/build/dummy_for_{package_name}_flags"):
+                continue
+            dummy_package_path = f"{tmp_ws_dir}/src/dummy_for_{package_name}_flags"
+            Path(f"{dummy_package_path}/src").mkdir(parents=True, exist_ok=True)
+            
+            with open(f"{dummy_package_path}/CMakeLists.txt", 'w') as cmakelists_txt:
                 cmakelists_txt.write(f'''\
 cmake_minimum_required(VERSION 3.8)
-project(dummy_for_flags)
+project(dummy_for_{package_name}_flags)
 
 if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 #add_compile_options()
@@ -39,7 +49,7 @@ endif()
 find_package(ament_cmake REQUIRED)
 find_package({package_name} REQUIRED)
 
-add_executable(${{PROJECT_NAME}} src/dummy_for_flags.cpp)
+add_executable(${{PROJECT_NAME}} src/dummy_for_{package_name}_flags.cpp)
 
 ament_target_dependencies(${{PROJECT_NAME}} {package_name})
 
@@ -71,12 +81,12 @@ DESTINATION lib/${{PROJECT_NAME}}
 ament_package()''')
 
 
-            with open("/home/gjorgji/ros2_galactic/src/dummy_for_flags/package.xml", 'w') as package_xml:
+            with open(f"{dummy_package_path}/package.xml", 'w') as package_xml:
                 package_xml.write(f'''\
 <?xml version="1.0"?>
 <?xml-model href="http://download.ros.org/schema/package_format3.xsd" schematypens="http://www.w3.org/2001/XMLSchema"?>
 <package format="3">
-<name>dummy_for_flags</name>
+<name>dummy_for_{package_name}_flags</name>
 <version>0.0.0</version>
 <description>TODO: Package description</description>
 <maintainer email="max.mustermann@alumni.fh-aachen.de">max</maintainer>
@@ -93,31 +103,38 @@ ament_package()''')
     <build_type>ament_cmake</build_type>
 </export>
 </package>''')
-            subprocess.call("cd /home/gjorgji/ros2_galactic/; rm -rf build/dummy_for_flags/; rm -rf install/dummy_for_flags", shell=True)
-            subprocess.call("cd /home/gjorgji/ros2_galactic/; source /home/gjorgji/ros2_galactic/install/setup.bash > /dev/null 2>&1; colcon build --packages-select dummy_for_flags > /dev/null 2>&1", shell=True)
+
+            with open(f"{dummy_package_path}/src/dummy_for_{package_name}_flags.cpp", 'w') as package_xml:
+                package_xml.write(f'''\
+int main(int argc, char *argv[])
+{{
+    return 0;
+}}''')
+
+            subprocess.call(f"cd {tmp_ws_dir}; rm -rf build/dummy_for_{package_name}_flags/; rm -rf install/dummy_for_{package_name}_flags", shell=True)
+            subprocess.call(f"cd {tmp_ws_dir}; source {ros2_workspace}/install/setup.bash > /dev/null 2>&1; colcon build --packages-select dummy_for_{package_name}_flags > /dev/null 2>&1", shell=True)
         elif opt in ("-c", "--cflags"):
-            subprocess.call("cd /home/gjorgji/ros2_galactic/build/dummy_for_flags/; make clean -s", shell=True)
-            build_object_command = subprocess.check_output("cd /home/gjorgji/ros2_galactic/build/dummy_for_flags/; make  -n VERBOSE=1 | grep ccache", shell=True)
-            build_object_flags= [ "-" + flag.replace(":", " ").replace(",", " ") for flag in build_object_command.decode("utf-8").strip('\n').split(" -") if "dummy_for_flags" not in flag and "ccache" not in flag]
+            '''subprocess.call(f"cd {tmp_ws_dir}/build/dummy_for_{package_name}_flags/; make clean -s", shell=True)
+            build_object_command = subprocess.check_output(f"cd {tmp_ws_dir}/build/dummy_for_{package_name}_flags/; make  -n VERBOSE=1 | grep ccache", shell=True)
+            build_object_flags= [ "-" + flag.replace(":", " ").replace(",", " ") for flag in build_object_command.decode("utf-8").strip('\n').split(" -") if f"dummy_for_{package_name}_flags" not in flag and "ccache" not in flag]
+            print("old:")
             for cflag in build_object_flags:
                 print(cflag + " ", end="")
-            print("")
-            sys.exit()
+            print("\nnew:")'''
+            with open(f"{tmp_ws_dir}/build/dummy_for_{package_name}_flags/CMakeFiles/dummy_for_{package_name}_flags.dir/flags.make", 'r') as cf:
+                compile_flags = ""
+                for line in cf.readlines():
+                    if any(var in line for var in ["CXX_DEFINES", "CXX_INCLUDES", "CXX_FLAGS"]):
+                        compile_flags += ' '.join([flag for flag in line.split("=")[-1].replace('\n', '').split(' ') if f"dummy_for_{package_name}_flags" not in flag])
+                print(compile_flags)
+            sys.exit(0)
         elif opt in ("-l", "--lflags"):
-            link_command = subprocess.check_output("cd /home/gjorgji/ros2_galactic/build/dummy_for_flags/; cat CMakeFiles/dummy_for_flags.dir/link.txt", shell=True)
-            link_flags= [ "-" + flag.replace(":", " ").replace(",", " ") for flag in link_command.decode("utf-8").strip('\n').split(" -") if  "dummy_for_flags" not in flag and "ccache" not in flag and "-Wl" not in flag and "-rpath" not in flag] # 
-            for lflag in link_flags:
-                print(lflag + " ", end="")
-            print("")
-            sys.exit()
-    
-
-#print("Building and linking for test...")
-#subprocess.call(b"cd /home/gjorgji/ros2_galactic/build/dummy_for_flags/; "+build_object_command+b" > /dev/null ", shell=True)
-#subprocess.call(b"cd /home/gjorgji/ros2_galactic/build/dummy_for_flags/; "+link_command+b" > /dev/null", shell=True)
-
-#print("Print calling for test:")
-#os.system("/home/gjorgji/ros2_galactic/build/dummy_for_flags/dummy_for_flags")
+            with open(f"{tmp_ws_dir}/build/dummy_for_{package_name}_flags/CMakeFiles/dummy_for_{package_name}_flags.dir/link.txt", 'r') as lf:
+                link_flags = ""
+                for line in lf.readlines():
+                    link_flags += ' '.join([flag for flag in line.split(',')[-1].replace('\n', '').split(' ')[1:] if f"dummy_for_{package_name}_flags" not in flag])
+            print(link_flags)
+            sys.exit(0)
 
 if __name__=="__main__":
     main(sys.argv[1:])
