00f73714f959d3ee971a25d850a321630ffbc5e6
Nicolas Limpert <limpert@fh-aachen.de>
2022-07-02 11:47:45 +0200

    syncpoint: reformat code with clang-format
    Automatically performed using format target and clang-format.
    
    Automatically committed using:
    for f in $(git ls-files --modified | awk -F/ '{ print  }' | sort -u); do
      cat <<EOM | git commit -F- $f
        THIS MESSAGE
      EOM
    done

--- src/libs/syncpoint/syncpoint.cpp
+++ src/libs/syncpoint/syncpoint.cpp
@@ -245,12 +245,12 @@ SyncPoint::wait(const std::string &component,
 {
 	MutexLocker ml(mutex_);
 
-	std::set<std::string> *        watchers;
-	WaitCondition *                cond;
+	std::set<std::string>         *watchers;
+	WaitCondition                 *cond;
 	CircularBuffer<SyncPointCall> *calls;
-	Mutex *                        mutex_cond;
-	bool *                         timer_running;
-	string *                       timer_owner;
+	Mutex                         *mutex_cond;
+	bool                          *timer_running;
+	string                        *timer_owner;
 	// set watchers, cond and calls depending of the Wakeup type
 	if (type == WAIT_FOR_ONE) {
 		watchers      = &watchers_wait_for_one_;
