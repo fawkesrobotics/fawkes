eeb8480f38bfe0dd7f45840396a4eb6f5f4add20
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-03-19 23:35:39 +0100

    laser-cluster: add cmake build files

--- cmake/lua.cmake
+++ cmake/lua.cmake
@@ -17,7 +17,6 @@ function(depend_on_lua target)
 endfunction()
 
 # TODO tolua 5.1 vs 5.4? TODO link against tolua++?
-
 function(generate_tolua target)
   string(REPLACE ";" " " FILES "${ARGN}")
   add_custom_command(
@@ -27,4 +26,24 @@ function(generate_tolua target)
       "${PROJECT_SOURCE_DIR}/etc/scripts/./tolua_generate.sh ${CMAKE_CURRENT_BINARY_DIR} ${target} ${PROJECT_SOURCE_DIR}/src/lua/fawkes/toluaext.lua ${PROJECT_SOURCE_DIR}/doc/headers/lichead_c.GPL_WRE ${CMAKE_CURRENT_SOURCE_DIR} ${FILES}"
     VERBATIM
     DEPENDS ${ARGN} ${PROJECT_SOURCE_DIR}/etc/scripts/tolua_generate.sh)
+endfunction()
+
+function(generate_interface_from_xml interface)
+  get_filename_component(if_name ${interface} NAME_WLE)
+  add_custom_command(
+    OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${if_name}.tolua
+           ${CMAKE_CURRENT_SOURCE_DIR}/${if_name}.h
+           ${CMAKE_CURRENT_SOURCE_DIR}/${if_name}.cpp
+    COMMAND ffifacegen -d ${CMAKE_CURRENT_SOURCE_DIR}
+            ${CMAKE_CURRENT_SOURCE_DIR}/${interface}
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${interface})
+  include(lua)
+  generate_tolua(${if_name} ${if_name}.tolua)
+  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${FAWKES_INTERFACE_DIR})
+  add_library(${if_name} SHARED ${if_name}_tolua.cpp
+                                ${CMAKE_CURRENT_SOURCE_DIR}/${if_name}.cpp)
+  target_include_directories(${if_name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/../)
+  target_link_libraries(${if_name} fawkescore fawkesutils fawkesinterface)
+
+  # target_link_libraries(fawkesutils_tolua fawkescore fawkesutils)
 endfunction()
--- cmake/pcl.cmake
+++ cmake/pcl.cmake
@@ -1,6 +1,7 @@
 include(ros)
 include(eigen3)
 
+set(PCL_SUFFIX "")
 pkg_check_modules(PCL pcl_common)
 if(NOT PCL_FOUND)
   message("Check for plc_common with explicit version suffix")
@@ -11,11 +12,11 @@ if(NOT PCL_FOUND)
     OUTPUT_STRIP_TRAILING_WHITESPACE
     OUTPUT_VARIABLE PCL_OUTPUT)
   pkg_check_modules(PCL ${PCL_OUTPUT})
+  string(REGEX REPLACE "^pcl_common" "" PCL_SUFFIX ${PCL_OUTPUT})
 endif()
 if(EXISTS "${CMAKE_SYSROOT}/usr/include/vtk/vtkVersion.h")
   set(CFLAGS_VTK -I/usr/include/vtk)
 endif()
-
 function(depend_on_pcl target)
   depend_on_eigen3(${target})
   target_compile_options(${target} PUBLIC -DHAVE_PCL)
@@ -24,6 +25,30 @@ function(depend_on_pcl target)
     PRIVATE ${PCL_CFLAGS} -Wno-unknown-pragmas -Wno-deprecated-declarations
             -Wno-overloaded-virtual -Wno-array-bounds)
   target_link_libraries(${target} ${PCL_LDFLAGS})
+endfunction()
+
+function(check_deps_pcl_extra_libs libs)
+  set(PCL_WARNING)
+  foreach(pcl_lib ${libs})
+    if(NOT PCL_${pcl_lib}_FOUND)
+      pkg_check_modules(PCL_${pcl_lib} pcl_${pcl_lib}${PCL_SUFFIX})
+      if(NOT PCL_${pcl_lib}_FOUND)
+        list(APPEND PCL_WARNING "${pcl_lib} dependency missing")
+      endif()
+    endif()
+  endforeach()
+  set(PCL_WARNING
+      ${PCL_WARNING}
+      PARENT_SCOPE)
+endfunction()
+
+function(depend_on_pcl_extra_libs target libs)
+  foreach(pcl_lib ${libs})
+    if(PCL_${pcl_lib}_FOUND)
+      target_link_libraries(${target} ${PCL_${pcl_lib}_LDFLAGS})
+      target_compile_options(${target} PUBLIC ${PCL_${pcl_lib}_CFLAGS})
+    endif()
+  endforeach()
 endfunction()
 
 # TODO link against ros std_msgs!?
