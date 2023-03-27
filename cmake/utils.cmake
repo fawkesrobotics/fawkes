6b68be6d3aa17b6afdee9ca7a10f802d50c7a45f
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-03-27 22:53:20 +0200

    cmake: set compile definitions properly per target

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,15 +1,15 @@
 function(check_deps_extra_libs libs)
-	set(EXTRA_LIBS_WARNING)
+  set(EXTRA_LIBS_WARNING)
   foreach(lib ${libs})
     if(NOT ${lib}_FOUND)
       pkg_check_modules(${lib} ${lib})
       if(NOT ${lib}_FOUND)
-				list(APPEND EXTRA_LIBS_WARNING "${lib} dependency missing")
+        list(APPEND EXTRA_LIBS_WARNING "${lib} dependency missing")
       endif()
     endif()
   endforeach()
-	set(EXTRA_LIBS_WARNING
-		${EXTRA_LIBS_WARNING}
+  set(EXTRA_LIBS_WARNING
+      ${EXTRA_LIBS_WARNING}
       PARENT_SCOPE)
 endfunction()
 
@@ -20,4 +20,63 @@ function(depend_on_extra_libs target libs)
       target_compile_options(${target} PUBLIC ${${lib}_CFLAGS})
     endif()
   endforeach()
+endfunction()
+
+macro(set_common_properties_of_targets_recursive targets dir)
+  get_property(
+    subdirectories
+    DIRECTORY ${dir}
+    PROPERTY SUBDIRECTORIES)
+  foreach(subdir ${subdirectories})
+    set_common_properties_of_targets_recursive(${targets} ${subdir})
+  endforeach()
+
+  get_property(
+    current_targets
+    DIRECTORY ${dir}
+    PROPERTY BUILDSYSTEM_TARGETS)
+  list(APPEND ${targets} ${current_targets})
+  foreach(target ${current_targets})
+    target_compile_definitions(
+      ${target}
+      PRIVATE SRCDIR=\"${dir}\"
+              BASEDIR=\"${PROJECT_SOURCE_DIR}\"
+              FAWKES_BASEDIR=\"${PROJECT_SOURCE_DIR}\"
+              BINDIR=\"${BINDIR}\"
+              LIBDIR=\"${CMAKE_BINARY_DIR}/src/libs/\"
+              PLUGINDIR=\"${FAWKES_PLUGIN_DIR}\"
+              IFACEDIR=\"${FAWKES_INTERFACE_DIR}\"
+              CONFDIR=\"${PROJECT_SOURCE_DIR}/cfg\"
+              USERDIR=\".fawkes\"
+              LOGDIR=\"${PROJECT_SOURCE_DIR}/log\"
+              RESDIR=\"${PROJECT_SOURCE_DIR}/res\"
+              TMPDIR=\"/tmp\"
+              BUILDTYPE=\"${BUILD_TYPE}\"
+              SOEXT=\"so\")
+  endforeach()
+endmacro()
+
+function(set_common_properties_of_targets var)
+  set(targets)
+  set_common_properties_of_targets_recursive(targets
+                                             ${CMAKE_CURRENT_SOURCE_DIR})
+  set(${var}
+      ${targets}
+      PARENT_SCOPE)
+endfunction()
+
+function(set_output_directory_plugins)
+  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY
+      ${PROJECT_SOURCE_DIR}/plugins
+      PARENT_SCOPE)
+endfunction()
+
+function(set_output_directory_libs)
+  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY
+      ${CMAKE_BINARY_DIR}/src/libs/
+      PARENT_SCOPE)
+endfunction()
+
+function(unset_output_directory)
+  unset(CMAKE_LIBRARY_OUTPUT_DIRECTORY PARENT_SCOPE)
 endfunction()
