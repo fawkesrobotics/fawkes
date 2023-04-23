5aa7b20a3a06109d60d06666aae99f331d723369
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-04-23 13:55:05 +0200

    cmake: search for ros2 packages on central place
    find_package is problematic as its link dependencies are simply imported
    target, hence linking against one library potentially requires that
    another find_package call was already made that imported te right
    target.
    To avoid all the headache simply search for all packages at a common
    place.

--- cmake/ros2.cmake
+++ cmake/ros2.cmake
@@ -23,20 +23,39 @@
 set(BUILD_WITH_ROS_2
     ON
     CACHE BOOL "Build with ROS 2")
-find_package(ament_cmake QUIET)
-remember_dependency(ament_cmake)
-find_package(rclcpp QUIET)
-remember_dependency(rclcpp)
-find_package(rmw QUIET)
-remember_dependency(rmw)
-find_package(rosidl_typesupport_interface QUIET)
-remember_dependency(rosidl_typesupport_interface)
-if(ament_cmake_FOUND
-   AND rclcpp_FOUND
-   AND rmw_FOUND
-   AND rosidl_typesupport_interface_FOUND
-   AND BUILD_WITH_ROS_2)
-  set(ROS_2_FOUND 1)
+if(BUILD_WITH_ROS_2)
+  find_package(ament_cmake QUIET)
+  remember_dependency(ament_cmake)
+  find_package(rclcpp QUIET)
+  remember_dependency(rclcpp)
+  find_package(rmw QUIET)
+  remember_dependency(rmw)
+  find_package(rosidl_typesupport_interface QUIET)
+  remember_dependency(rosidl_typesupport_interface)
+  find_package(rcl_interfaces QUIET)
+  remember_dependency(rcl_interfaces)
+  find_package(std_msgs QUIET)
+  remember_dependency(std_msgs)
+  find_package(geometry_msgs QUIET)
+  remember_dependency(geometry_msgs)
+  find_package(sensor_msgs QUIET)
+  remember_dependency(sensor_msgs)
+  find_package(console_bridge QUIET)
+  remember_dependency(console_bridge)
+  find_package(class_loader QUIET)
+  remember_dependency(class_loader)
+  find_package(tinyxml2 QUIET)
+  remember_dependency(tinyxml2)
+  find_package(unique_identifier_msgs QUIET)
+  remember_dependency(unique_identifier_msgs)
+  find_package(action_msgs QUIET)
+  remember_dependency(action_msgs)
+  if(ament_cmake_FOUND
+     AND rclcpp_FOUND
+     AND rmw_FOUND
+     AND rosidl_typesupport_interface_FOUND)
+    set(ROS_2_FOUND 1)
+  endif()
 endif()
 
 function(depend_on_ros2 target)
