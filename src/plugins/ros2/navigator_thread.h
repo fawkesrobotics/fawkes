5d35274beda58e0d50eeb27ae9abc04c42f1aef4
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-04-27 16:47:29 +0200

    ros2: mirror sending of goal from nav2_behavior_tree
    Applied the goal handling from here:
    https://github.com/ros-planning/navigation2/blob/humble/nav2_behavior_tree/
    include/nav2_behavior_tree/bt_action_node.hpp

--- src/plugins/ros2/navigator_thread.h
+++ src/plugins/ros2/navigator_thread.h
@@ -31,30 +31,31 @@
 #include <core/threading/thread.h>
 #include <interfaces/NavigatorInterface.h>
 #include <interfaces/Position3DInterface.h>
-#include <nav2_msgs/action/navigate_to_pose.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <plugins/ros2/aspect/ros2.h>
+
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav2_msgs/action/navigate_to_pose.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 //#include <tf2/types.h>
 #include <tf/types.h>
 #include <utils/math/angle.h>
-#include <unistd.h>
 
 #include <math.h>
+#include <unistd.h>
 
 namespace fawkes {
 class NavigatorInterface;
 }
 
 class ROS2NavigatorThread : public fawkes::Thread,
-                           public fawkes::ClockAspect,
-//                           public fawkes::BlockedTimingAspect,
-                           public fawkes::LoggingAspect,
-                           public fawkes::BlackBoardAspect,
-                           public fawkes::ConfigurableAspect,
-                           public fawkes::ROS2Aspect,
-                           public fawkes::TransformAspect
+                            public fawkes::ClockAspect,
+                            //                           public fawkes::BlockedTimingAspect,
+                            public fawkes::LoggingAspect,
+                            public fawkes::BlackBoardAspect,
+                            public fawkes::ConfigurableAspect,
+                            public fawkes::ROS2Aspect,
+                            public fawkes::TransformAspect
 {
 public:
 	ROS2NavigatorThread(std::string &cfg_prefix);
@@ -76,44 +77,42 @@ private:
 	void send_goal();
 	void stop_goals();
 	void load_config();
-//	bool set_dynreconf_value(const std::string &path, const float value);
+	//	bool set_dynreconf_value(const std::string &path, const float value);
 
 private:
 	using NavigateToPose = nav2_msgs::action::NavigateToPose;
 	using GoalHandleNav2 = rclcpp_action::ClientGoalHandle<NavigateToPose>;
 	typedef rclcpp_action::Client<NavigateToPose>::SharedPtr Nav2Client;
 
-//	rclcpp_action::Client<NavigateToPose>::SharedPtr client_ptr_;
-
-	void responseCb(rclcpp_action::ClientGoalHandle<NavigateToPose>::SharedPtr future);
-	void feedbackCb(rclcpp_action::ClientGoalHandle<NavigateToPose>::SharedPtr,
-				const std::shared_ptr<const NavigateToPose::Feedback> feedback);
-	void resultCb(const rclcpp_action::ClientGoalHandle<NavigateToPose>::WrappedResult &result);
+	//	rclcpp_action::Client<NavigateToPose>::SharedPtr client_ptr_;
 
 	void transform_to_fixed_frame();
 
-	fawkes::NavigatorInterface *	nav_if_;
-	fawkes::Position3DInterface *	pose_if_;
-	Nav2Client			ac_;
-	NavigateToPose::Goal		goal_;
-	GoalHandleNav2::SharedPtr	goal_handle_;
-	bool				cmd_sent_;
-	bool				connected_history_;
-	rclcpp::Clock			ros_clock_;
+	fawkes::NavigatorInterface                                    *nav_if_;
+	fawkes::Position3DInterface                                   *pose_if_;
+	Nav2Client                                                     ac_;
+	NavigateToPose::Goal                                           goal_;
+	GoalHandleNav2::SharedPtr                                      goal_handle_;
+	std::shared_ptr<std::shared_future<GoalHandleNav2::SharedPtr>> future_goal_handle_;
+	bool                                                           cmd_sent_;
+	bool                                                           goal_result_available_;
+	bool                                                           connected_history_;
+	rclcpp::Clock                                                  ros_clock_;
+	rclcpp_action::ClientGoalHandle<NavigateToPose>::WrappedResult result_;
 
 	fawkes::Time *ac_init_checktime_;
 
 	std::string cfg_prefix_;
 
-//	// ROS2 dynamic reconfigure parts
-//	dynamic_reconfigure::ReconfigureRequest  dynreconf_srv_req;
-//	dynamic_reconfigure::ReconfigureResponse dynreconf_srv_resp;
-//	dynamic_reconfigure::DoubleParameter     dynreconf_double_param;
-//	dynamic_reconfigure::Config              dynreconf_conf;
+	//	// ROS2 dynamic reconfigure parts
+	//	dynamic_reconfigure::ReconfigureRequest  dynreconf_srv_req;
+	//	dynamic_reconfigure::ReconfigureResponse dynreconf_srv_resp;
+	//	dynamic_reconfigure::DoubleParameter     dynreconf_double_param;
+	//	dynamic_reconfigure::Config              dynreconf_conf;
 
-//	std::string cfg_dynreconf_path_;
-//	std::string cfg_dynreconf_trans_vel_name_;
-//	std::string cfg_dynreconf_rot_vel_name_;
+	//	std::string cfg_dynreconf_path_;
+	//	std::string cfg_dynreconf_trans_vel_name_;
+	//	std::string cfg_dynreconf_rot_vel_name_;
 
 	std::string cfg_fixed_frame_;
 	float       cfg_ori_tolerance_;
@@ -123,9 +122,9 @@ private:
 	rclcpp::Parameter param_max_rot;
 
 	geometry_msgs::msg::PoseStamped base_position;
-	float                      goal_position_x;
-	float                      goal_position_y;
-	float                      goal_position_yaw;
+	float                           goal_position_x;
+	float                           goal_position_y;
+	float                           goal_position_yaw;
 
 	float goal_tolerance_trans;
 	float goal_tolerance_yaw;
