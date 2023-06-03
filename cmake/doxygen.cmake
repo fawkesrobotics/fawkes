2795f211238bf133208a346a5e6030fe393187ab
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-06-03 18:41:14 +0200

    cmake: exclude attic from doxygen

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -4,5 +4,5 @@ set(DOXYGEN_WARN_LOGFILE doxywarnings.txt)
 set(DOXYGEN_HAVE_DOT NO)
 set(DOXYGEN_EXCLUDE_PATTERNS
     "${FAWKES_CORE_DIR}/src/plugins/webview/frontend;**/*.pb.*;\
-    **/*Interface.*;**/*.py")
+    **/*Interface.*;**/*.py;**/attic/*;")
 doxygen_add_docs(doxygen ${PROJECT_SOURCE_DIR} COMMENT "Generate man pages")
